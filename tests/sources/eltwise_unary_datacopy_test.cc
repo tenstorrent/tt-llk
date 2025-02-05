aa53dc11d07c58b7773bd65788fdaa57df139b2f
Lazar Djurovic <ldjurovic@tenstorrent.com>
2025-02-05 14:22:26 +0000

    Running ELFs in differenet order not producing hang in Unpacker

--- tests/sources/eltwise_unary_datacopy_test.cc
+++ tests/sources/eltwise_unary_datacopy_test.cc
@@ -21,12 +21,6 @@ const bool unpack_to_dest = true;
 const bool unpack_to_dest = false;
 #endif
 
-#ifdef FORMAT_FLOAT32
-#define UNPACKER_FORMAT (uint32_t)DataFormat::Tf32
-#else
-#define UNPACKER_FORMAT DATA_FORMAT
-#endif
-
 #ifdef LLK_TRISC_UNPACK
 
 #include "llk_unpack_A.h"
@@ -38,9 +32,9 @@ volatile uint32_t* buffer_A = (volatile uint32_t*)0x1a000;
 void run_kernel()
 {
 
-    _llk_unpack_A_init_<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, unpack_to_dest>(0, 0, FACE_R_DIM, 4, DATA_FORMAT,UNPACKER_FORMAT);
-    _llk_unpack_A_hw_configure_<is_fp32_dest_acc_en, StochRndType::None>(DATA_FORMAT,UNPACKER_FORMAT,FACE_R_DIM,0,4);
-    _llk_unpack_A_<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, unpack_to_dest>((((uint32_t)buffer_A)/16)-1, 0, DATA_FORMAT,UNPACKER_FORMAT);
+    _llk_unpack_A_init_<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, unpack_to_dest>(0, 0, FACE_R_DIM, 4, DATA_FORMAT,DATA_FORMAT);
+    _llk_unpack_A_hw_configure_<is_fp32_dest_acc_en, StochRndType::None>(DATA_FORMAT,DATA_FORMAT,FACE_R_DIM,0,4);
+    _llk_unpack_A_<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, unpack_to_dest>((((uint32_t)buffer_A)/16)-1, 0, DATA_FORMAT,DATA_FORMAT);
 
 }
 
@@ -60,12 +54,10 @@ const bool is_int_fpu_en = true;
 #include "params.h"
 
 using namespace ckernel;
-using namespace ckernel::sfpu;
 
 void run_kernel()
 {
     // copy srca to dest
-        // copy srca to dest
     #ifdef ARCH_BLACKHOLE
     _llk_math_eltwise_unary_datacopy_init_<DataCopyType::A2D, BroadcastType::NONE,false, is_fp32_dest_acc_en, is_int_fpu_en>(0, 0, 4, DATA_FORMAT);
     #else
@@ -74,7 +66,7 @@ void run_kernel()
     _llk_math_pack_sync_init_<DstSync::SyncFull,is_fp32_dest_acc_en>();
     _llk_math_hw_configure_<false,false>(DATA_FORMAT, DATA_FORMAT);
     _llk_math_wait_for_dest_available_<DstSync::SyncFull>();
-    _llk_math_eltwise_unary_datacopy_<DataCopyType::A2D, DstSync::SyncFull, BroadcastType::NONE, is_fp32_dest_acc_en, unpack_to_dest>(0, UNPACKER_FORMAT, DATA_FORMAT);
+    _llk_math_eltwise_unary_datacopy_<DataCopyType::A2D, DstSync::SyncFull, BroadcastType::NONE, is_fp32_dest_acc_en, unpack_to_dest>(0, DATA_FORMAT, DATA_FORMAT);
     _llk_math_dest_section_done_<DstSync::SyncFull,is_fp32_dest_acc_en>();
 }
 
