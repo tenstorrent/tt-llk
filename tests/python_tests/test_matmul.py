1a443eb094977fce29bb02dc4b06c299ae2938b9
ldjurovicTT <ldjurovic@tenstoreent.com>
2025-02-06 13:45:21 +0000

    Matmul fidelity sweep

--- tests/python_tests/test_matmul.py
+++ tests/python_tests/test_matmul.py
@@ -3,23 +3,58 @@ import torch
 import os
 from helpers import *
 
-def generate_golden(operand1, operand2, data_format):
+def generate_golden(operand1, operand2, data_format,math_fidelity):
 
     A_untilized = untilize(operand1,data_format)
     B_untilized = untilize(operand2,data_format)
+
+    if( format == "Float16_b"):
+        if math_fidelity == 0: # LoFi
+            for element in operand1:
+                element &= 0xFFF8
+            for element in operand2:
+                element &= 0xFFFE
+        elif math_fidelity == 2: # HiFi2
+            for element in operand2:
+                element &= 0xFFFE
+        elif math_fidelity == 3: # HiFi3
+            pass
+        elif math_fidelity == 4: # HiFi4
+            pass
 
     result = torch.matmul(A_untilized, B_untilized )
     result = tilize(result,data_format)
 
     return result
 
-@pytest.mark.parametrize("format", ["Float16_b", "Float16"])
-@pytest.mark.parametrize("testname", ["matmul_test"])
-@pytest.mark.parametrize("dest_acc", ["","DEST_ACC"])
-def test_all(format, testname, dest_acc):
+# @pytest.mark.parametrize("format", ["Float16_b", "Float16","Bfp8_b"])
+# @pytest.mark.parametrize("testname", ["matmul_test"])
+# @pytest.mark.parametrize("dest_acc", [""])#,"DEST_ACC"])
+
+# This setup allows easier tracking of test cases when run
+param_combinations = [
+    (format, dest_acc, testname, math_fidelity)
+    for format in ["Float16_b", "Float16"]
+    for dest_acc in ["", "DEST_ACC"]
+    for testname in ["matmul_test"]
+    for math_fidelity in [0,2,3,4]
+]
+
+param_ids = [
+    f" format={comb[0]} | dest_acc={comb[1]} | math_fidelity={comb[3]}"
+    for comb in param_combinations
+]
+
+@pytest.mark.parametrize(
+    "format, dest_acc, testname, math_fidelity",
+    param_combinations,
+    ids=param_ids
+)
+
+def test_all(format, testname, dest_acc, math_fidelity):
 
     src_A, src_B = generate_stimuli(format)
-    golden_tensor = generate_golden(src_A, src_B, format)
+    golden_tensor = generate_golden(src_A, src_B, format,math_fidelity)
 
     write_stimuli_to_l1(src_A, src_B, format)
 
@@ -28,6 +63,7 @@ def test_all(format, testname, dest_acc):
         "output_format": format,
         "testname": testname,
         "dest_acc": dest_acc,
+        "math_fidelity" : math_fidelity
     }
 
     make_cmd = generate_make_command(test_config)
