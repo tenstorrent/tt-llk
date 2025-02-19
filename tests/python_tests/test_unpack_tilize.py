5c514d53222e81399a27bf90dbf9aef2dfb87c57
ldjurovicTT <ldjurovic@tenstoreent.com>
2025-02-19 13:06:52 +0000

    Working on unpack tilize test

---
+++ tests/python_tests/test_unpack_tilize.py
@@ -0,0 +1,73 @@
+import pytest
+import torch
+import os
+from helpers import *
+
+def generate_golden(operand1, data_format):
+
+    A_matrix = operand1.reshape(32,32)
+    print("\n\n", type(A_matrix))
+    A_tilized  = tilize(A_matrix,data_format)
+
+    return A_tilized
+
+param_combinations = [
+    (format, testname)
+    for format in ["Float16_b"]
+    for testname in ["unpack_tilize_test"]
+]
+
+param_ids = [
+    f" format={comb[0]} "
+    for comb in param_combinations
+]
+
+@pytest.mark.parametrize(
+    "format,testname",
+    param_combinations,
+    ids=param_ids
+)
+
+def test_all(format, testname):
+
+    src_A, src_B = generate_stimuli(format, tile_cnt = 1, sfpu = False, const_face = True, const_value_A=4, const_value_B=3)
+    golden_tensor = generate_golden(src_A, format)
+
+    write_stimuli_to_l1(src_A, src_B, format)
+
+    test_config = {
+        "input_format": format,
+        "output_format": format,
+        "testname": testname,
+    }
+
+    make_cmd = generate_make_command(test_config)
+    os.system(f"cd .. && {make_cmd} >/dev/null")
+
+    run_elf_files(testname)
+
+    res_from_L1 = collect_results(format)
+
+    os.system("cd .. && make clean")
+
+    assert len(res_from_L1) == len(golden_tensor)
+
+    # Mailbox checks
+    assert read_words_from_device("0,0", 0x19FF4, word_count=1)[0].to_bytes(4, 'big') == b'\x00\x00\x00\x01'
+    assert read_words_from_device("0,0", 0x19FF8, word_count=1)[0].to_bytes(4, 'big') == b'\x00\x00\x00\x01'
+    assert read_words_from_device("0,0", 0x19FFC, word_count=1)[0].to_bytes(4, 'big') == b'\x00\x00\x00\x01'
+
+    res_tensor = torch.tensor(res_from_L1, dtype=format_dict[format] if format in ["Float16", "Float16_b"] else torch.bfloat16)
+
+    if(format == "Float16_b" or format == "Float16"):
+        atol = 0.1
+        rtol = 0.05
+    elif(format == "Bfp8_b"):
+        atol = 0.1
+        rtol = 0.2
+
+    for i in range(len(golden_tensor)):
+        assert torch.isclose(golden_tensor[i],res_tensor[i], rtol = rtol, atol = atol), f"Failed at index {i} with values {golden_tensor[i]} and {res_from_L1[i]}"
+
+    _ , pcc = comp_pcc(golden_tensor, res_tensor, pcc=0.99) 
+    assert pcc > 0.98
---
+++ tests/sources/unpack_tilize_test.cc
@@ -0,0 +1,95 @@
+#include <cstdint>
+#include <cstdio>
+
+#include "llk_defs.h"
+#include "ckernel.h"
+
+// Globals
+uint32_t unp_cfg_context = 0;
+uint32_t pack_sync_tile_dst_ptr = 0;
+volatile uint32_t tt_l1_ptr l1_buffer[16] __attribute__ ((section (".text#"))) __attribute__ ((aligned (16)));
+
+#ifdef DEST_ACC
+const bool is_fp32_dest_acc_en = true;
+#else
+const bool is_fp32_dest_acc_en = false;
+#endif
+
+#ifdef LLK_TRISC_UNPACK
+
+#include "llk_unpack_tilize.h"
+#include "llk_unpack_common.h"
+#include "params.h"
+
+volatile uint32_t* buffer_A = (volatile uint32_t*)0x1a000;
+
+void run_kernel()
+{
+    _llk_unpack_tilize_hw_configure_<false,StochRndType::None>(DATA_FORMAT, DATA_FORMAT, FACE_R_DIM, 0, 4);
+    _llk_unpack_tilize_init_(DATA_FORMAT, DATA_FORMAT, 0, FACE_R_DIM, false);
+    _llk_unpack_tilize_((std::uint32_t)buffer_A/16-1,0,DATA_FORMAT,0,FACE_R_DIM,4,false);
+}
+
+#endif
+
+
+#ifdef LLK_TRISC_MATH
+
+const bool is_int_fpu_en = false;
+
+#include "llk_math_eltwise_unary_datacopy.h"
+#include "llk_math_common.h"
+#include "params.h"
+
+using namespace ckernel;
+
+void run_kernel()
+{
+    // copy srca to dest
+    #ifdef ARCH_BLACKHOLE
+    _llk_math_eltwise_unary_datacopy_init_<DataCopyType::A2D, BroadcastType::NONE,false, is_fp32_dest_acc_en, is_int_fpu_en>(0, 0, 4, DATA_FORMAT);
+    #else
+    _llk_math_eltwise_unary_datacopy_init_<DataCopyType::A2D, BroadcastType::NONE, is_fp32_dest_acc_en, is_int_fpu_en>(0, 0, 4, DATA_FORMAT);
+    #endif
+    _llk_math_pack_sync_init_<DstSync::SyncFull,is_fp32_dest_acc_en>();
+    _llk_math_hw_configure_<false,false>(DATA_FORMAT, DATA_FORMAT);
+    _llk_math_wait_for_dest_available_<DstSync::SyncFull>();
+    _llk_math_eltwise_unary_datacopy_<DataCopyType::A2D, DstSync::SyncFull, BroadcastType::NONE, is_fp32_dest_acc_en, false>(0, DATA_FORMAT, DATA_FORMAT);
+    _llk_math_dest_section_done_<DstSync::SyncFull,is_fp32_dest_acc_en>();
+}
+
+#endif 
+
+#ifdef LLK_TRISC_PACK
+
+#include "llk_pack.h"
+#include "llk_pack_common.h"
+#include "params.h"
+
+volatile uint32_t* buffer_Dest = (volatile uint32_t*)0x1c000;
+void run_kernel()
+{
+    for(int i = 0; i < 16*16*4; i++)
+    {
+        buffer_Dest[i] = 0xdeadbeef;
+    }
+    #ifdef ARCH_BLACKHOLE
+    _llk_pack_hw_configure_<false, is_fp32_dest_acc_en, false>(DATA_FORMAT, DATA_FORMAT, 16*16*4);
+    #else
+    _llk_pack_hw_configure_<false, is_fp32_dest_acc_en>(DATA_FORMAT, DATA_FORMAT, 16*16*4);
+    #endif
+
+    _llk_pack_init_<false, false, DstTileFaceLayout::RowMajor, false>(DATA_FORMAT);
+    
+    #ifdef ARCH_BLACKHOLE
+    _llk_pack_dest_init_<DstSync::SyncFull,DstTileFaceLayout::RowMajor,is_fp32_dest_acc_en>();
+    #else
+    _llk_pack_dest_init_<DstSync::SyncFull, DstTileFaceLayout::RowMajor, false, false>();
+    #endif
+
+    _llk_packer_wait_for_math_done_();
+    _llk_pack_<DstSync::SyncFull,false, is_fp32_dest_acc_en>(0, (std::uint32_t)buffer_Dest/16-1);
+    _llk_pack_dest_section_done_<DstSync::SyncFull,is_fp32_dest_acc_en>();
+}
+
+#endif
