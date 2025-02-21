7d07a4604383e721e8ab8da8ad5520749e1b6244
Lazar Djurovic <ldjurovic@tenstorrent.com>
2025-02-21 15:50:06 +0000

    const + reinterpret_cast

--- tests/sources/eltwise_unary_sfpu_test.cc
+++ tests/sources/eltwise_unary_sfpu_test.cc
@@ -24,13 +24,13 @@ const bool is_fp32_dest_acc_en = false;
 #include "llk_unpack_common.h"
 #include "params.h"
 
-volatile uint32_t* buffer_A = (volatile uint32_t*)0x1a000;
+volatile uint32_t* const buffer_A = reinterpret_cast<volatile uint32_t*>(0x1a000);
 
 void run_kernel()
 {
     _llk_unpack_A_hw_configure_<is_fp32_dest_acc_en, StochRndType::None>(DATA_FORMAT,DATA_FORMAT,FACE_R_DIM,0,4);
     _llk_unpack_A_init_<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, unpack_to_dest>(0, 0, FACE_R_DIM, 4, DATA_FORMAT,DATA_FORMAT);
-    _llk_unpack_A_<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, unpack_to_dest>((((uint32_t)buffer_A)/16)-1, 0, DATA_FORMAT,DATA_FORMAT);
+    _llk_unpack_A_<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, unpack_to_dest>((reinterpret_cast<uint32_t>(buffer_A)/16)-1, 0, DATA_FORMAT,DATA_FORMAT);
 }
 
 #endif
@@ -93,7 +93,7 @@ void run_kernel()
 // #define PACK_DEST_FORMAT DATA_FORMAT
 
 
-volatile uint32_t* buffer_Dest = (volatile uint32_t*)0x1c000;
+volatile uint32_t* const buffer_Dest = reinterpret_cast<volatile uint32_t*>(0x1c000);
 void run_kernel()
 {
 
@@ -114,7 +114,7 @@ void run_kernel()
     #endif
 
     _llk_packer_wait_for_math_done_();
-    _llk_pack_<DstSync::SyncFull,false, is_fp32_dest_acc_en>(0, (std::uint32_t)buffer_Dest/16-1);
+    _llk_pack_<DstSync::SyncFull,false, is_fp32_dest_acc_en>(0, reinterpret_cast<std::uint32_t>(buffer_Dest)/16-1);
     _llk_pack_dest_section_done_<DstSync::SyncFull,is_fp32_dest_acc_en>();
 }
 
