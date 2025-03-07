bcf638af74e37455cd18377908929e0cba4f3995
JushBJJ <JushPacis@gmail.com>
2025-03-07 21:29:33 +1000

    Cleanup ckernel_sfpu_*.h headers for Wormhole

--- tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_dropout.h
+++ tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_dropout.h
@@ -4,11 +4,11 @@
 
 #pragma once
 
-#include "ckernel_defs.h"
-#include "ckernel.h"
-#include "noc_nonblocking_api.h"
+#include <cstdint>
 
 #include "sfpi.h"
+#include "ckernel_ops.h"
+#include "debug/fw_debug.h"
 
 using namespace sfpi;
 
