5cc926f3dec11f505893a0c5003a9cfd063a5195
JushBJJ <JushPacis@gmail.com>
2025-03-07 19:28:41 +1000

    Cleanup ckernel_sfpu_*.h files for Wormhole

--- tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_exp.h
+++ tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_exp.h
@@ -182,6 +182,12 @@ void _calculate_exponential_(const int iterations, uint16_t exp_base_scale_facto
     {
         // Unroll 8 best for approx, unroll 0 for precise, compiler figures this out
         for (int d = 0; d < iterations; d++)
+    {
+        vFloat val = dst_reg[0];
+        if constexpr(SCALE_EN){
+            val = val * s2vFloat16a(exp_base_scale_factor);
+        }
+        if constexpr (APPROXIMATION_MODE)
         {
             vFloat val = dst_reg[0];
             if constexpr(SCALE_EN){
@@ -201,31 +207,31 @@ void _calculate_exponential_(const int iterations, uint16_t exp_base_scale_facto
                     vInt adj_exp          = vConstIntPrgm2;
                     val                   = val * vConstLn2Recip + c23_73;
 
-                    // Remove Exponent of 7 and bias the Mantissa to 127.
-                    vInt val_short = adj_exp + reinterpret<vInt>(val);
+                // Remove Exponent of 7 and bias the Mantissa to 127.
+                vInt val_short = adj_exp + reinterpret<vInt>(val);
 
-                    // SHL to move integer bits to exponent
-                    val_short <<= 10 - p_exp::FRAC_BITS;
-                    dst_reg[0] = reinterpret<vFloat>(val_short);
-                }
-                v_endif;
+                // SHL to move integer bits to exponent
+                val_short <<= 10 - p_exp::FRAC_BITS;
+                dst_reg[0] = reinterpret<vFloat>(val_short);
             }
-            else
-            {
-                // Force sign to 0 (make number positive)
+            v_endif;
+        }
+        else
+        {
+            // Force sign to 0 (make number positive)
                 vFloat result = _sfpu_exp_(setsgn(val, 0));
 
                 v_if (val < 0)
                 {
                     result = _sfpu_reciprocal_(result);
-                }
-                v_endif;
+            }
+            v_endif;
 
                 dst_reg[0] = result;
-            }
-
-            dst_reg++;
         }
+
+        dst_reg++;
+    }
     }
 }
 
