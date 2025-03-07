0e4c7d4e28e164ab06d33667794949f64ec35438
JushBJJ <JushPacis@gmail.com>
2025-03-07 19:28:41 +1000

    Cleanup ckernel_sfpu_*.h files for Wormhole

--- tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_max_int32.h
+++ tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_max_int32.h
@@ -4,12 +4,7 @@
 
 #pragma once
 
-#include "ckernel.h"
-#include "ckernel_defs.h"
-#include "noc_nonblocking_api.h"
-#include "sfpi.h"
-
-using namespace sfpi;
+#include "ckernel_ops.h"
 
 namespace ckernel
 {
