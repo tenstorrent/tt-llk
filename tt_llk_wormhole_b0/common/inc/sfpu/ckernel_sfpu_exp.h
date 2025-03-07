e1cfcef9486f104a8c2b5bc4fd9eeaf1120d3041
JushBJJ <JushPacis@gmail.com>
2025-03-07 21:29:33 +1000

    Cleanup ckernel_sfpu_*.h headers for Wormhole

--- tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_exp.h
+++ tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_exp.h
@@ -4,14 +4,10 @@
 
 #pragma once
 
-#include "ckernel_defs.h"
-#include "ckernel.h"
-#include "noc_nonblocking_api.h"
-#include "ckernel_sfpu_recip.h"
-
 #include "sfpi.h"
-
-using namespace sfpi;
+#include "sfpi_fp16.h"
+#include "ckernel_sfpu_recip.h"
+#include "ckernel_sfpu_exp.h"
 
 namespace ckernel
 {
