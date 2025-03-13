1bc7521a4f2ea7a778fd75e9c4cf7c2e6ea69ca7
Filip Vranic <fvranic@tenstorrent.com>
2025-03-13 15:05:50 +0000

    fixup! fix: reset only on failure

--- tests/python_tests/conftest.py
+++ tests/python_tests/conftest.py
@@ -6,23 +6,10 @@ from helpers import HardwareController
 
 
 @pytest.fixture(scope="function", autouse=True)
-def hardware_controller(request):
+def hardware_controller():
     # Setup: initialize the hardware controller
     controller = HardwareController()
 
     yield controller
 
-    # Teardown: reset the hardware only if the test failed
-    if request.node.result.failed:
-        controller.reset()
-
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    # Execute all other hooks to obtain the report object
-    outcome = yield
-    result = outcome.get_result()
-
-    # Attach the test result to the item
-    if result.when == "call":
-        item.result = result
+    controller.reset()
--- tests/python_tests/helpers/hardware_controller.py
+++ tests/python_tests/helpers/hardware_controller.py
@@ -1,20 +1,19 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+
 from .utils import get_chip_architecture, run_shell_command
+
 
 class HardwareController:
     def __init__(self):
-        # Initialize the hardware controller
-        self.chip_architecture = None
-        self.initialize_hardware()
-
-    def initialize_hardware(self):
         self.chip_architecture = get_chip_architecture()
 
     def reset(self):
         if self.chip_architecture == "blackhole":
-            print("Resetting card")
+            print("Resetting BH card")
             run_shell_command("/home/software/syseng/bh/tt-smi -r 0")
         elif self.chip_architecture == "wormhole":
-            print("Resetting card")
+            print("Resetting WH card")
             run_shell_command("/home/software/syseng/wh/tt-smi -wr 0")
         else:
             raise ValueError("Unknown chip architecture")
