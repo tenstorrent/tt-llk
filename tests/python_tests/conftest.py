fcdcbd5387e6a52ab3761e1b667edbdecb71c1be
Filip Vranic <fvranic@tenstorrent.com>
2025-03-25 17:56:26 +0100

    chore: don't reset chip on each test run

--- tests/python_tests/conftest.py
+++ tests/python_tests/conftest.py
@@ -10,8 +10,8 @@ from helpers import HardwareController
 @pytest.fixture(scope="function", autouse=True)
 def manage_hardware_controller():
     # Setup: initialize the hardware controller
-    controller = HardwareController()
-    controller.reset_card()
+    # controller = HardwareController()
+    # controller.reset_card()
     yield controller
 
 
