f799b8181c4c51c5363fbd1f603c0840dfe4dc37
Filip Vranic <fvranic@tenstorrent.com>
2025-03-28 07:55:37 +0000

    fixup! fix: remove using namespace declarations from blackhole

--- tt_llk_blackhole/llk_lib/llk_unpack_reduce.h
+++ tt_llk_blackhole/llk_lib/llk_unpack_reduce.h
@@ -19,16 +19,16 @@ inline void _llk_unpack_reduce_mop_config_()
 #if SKIP_UNP == 1
     static constexpr uint unpack_srca = TT_OP_NOP;
 #else
-    static constexpr uint unpack_srca = TT_OP_UNPACR(SrcA, 0b1, 0, 0, 0, 1, 1, ckernel::p_unpacr::RAREFYB_DISABLE, 0, 0, 0, 0, 1);
+    static constexpr uint unpack_srca = TT_OP_UNPACR(ckernel::SrcA, 0b1, 0, 0, 0, 1, 1, ckernel::p_unpacr::RAREFYB_DISABLE, 0, 0, 0, 0, 1);
 #endif
     static constexpr uint unpack_zerosrca =
         TT_OP_UNPACR_NOP(ckernel::p_unpacr_nop::UNP0, 0, 0, 0, 0, 0, 0, ckernel::p_unpacr_nop::CLR_SRC_0, ckernel::p_unpacr_nop::CLR_SRC);
 #if SKIP_UNP == 1
     static constexpr uint unpack_srcb = TT_OP_NOP;
 #else
-    static constexpr uint unpack_srcb = TT_OP_UNPACR(SrcB, 0b0, 0, 0, 0, 1, 1, ckernel::p_unpacr::RAREFYB_DISABLE, 0, 0, 0, 0, 1);
+    static constexpr uint unpack_srcb = TT_OP_UNPACR(ckernel::SrcB, 0b0, 0, 0, 0, 1, 1, ckernel::p_unpacr::RAREFYB_DISABLE, 0, 0, 0, 0, 1);
 #endif
-    ckernel_unpack_template tmp = ckernel_unpack_template(
+    ckernel::ckernel_unpack_template tmp = ckernel::ckernel_unpack_template(
         true, // src B
         true, // halo - just used for 4 unpacks
         unpack_zerosrca,
@@ -38,7 +38,7 @@ inline void _llk_unpack_reduce_mop_config_()
         0,
         unpack_srcb,
         0);
-    tmp.program(instrn_buffer);
+    tmp.program(ckernel::instrn_buffer);
 }
 
 template <bool is_fp32_dest_acc_en = false, ckernel::StochRndType stoch_rnd_mode = ckernel::StochRndType::None>
@@ -47,8 +47,8 @@ inline void _llk_unpack_reduce_hw_configure_(
     const std::uint32_t unpB_src_format,
     const std::uint32_t unpA_dst_format,
     const std::uint32_t unpB_dst_format,
-    const std::uint32_t unpA_face_r_dim             = FACE_R_DIM,
-    const std::uint32_t unpB_face_r_dim             = FACE_R_DIM,
+    const std::uint32_t unpA_face_r_dim             = ckernel::FACE_R_DIM,
+    const std::uint32_t unpB_face_r_dim             = ckernel::FACE_R_DIM,
     const std::uint32_t within_face_16x16_transpose = 0,
     const std::uint32_t unpA_num_faces              = 4,
     const std::uint32_t unpB_num_faces              = 4)
@@ -58,7 +58,7 @@ inline void _llk_unpack_reduce_hw_configure_(
     constexpr bool fpu_srnd_en  = stoch_rnd_en || (stoch_rnd_mode == ckernel::StochRndType::Fpu);
     constexpr bool pack_srnd_en = stoch_rnd_en || (stoch_rnd_mode == ckernel::StochRndType::Pack);
 
-    configure_unpack_AB<is_row_pool, is_fp32_dest_acc_en, fpu_srnd_en, pack_srnd_en>(
+    ckernel::unpacker::configure_unpack_AB<is_row_pool, is_fp32_dest_acc_en, fpu_srnd_en, pack_srnd_en>(
         unpA_src_format,
         unpB_src_format,
         unpA_dst_format,
@@ -75,9 +75,10 @@ inline void _llk_unpack_reduce_init_(const std::uint32_t within_face_16x16_trans
 {
     // REDUCE_ROW requires transpose itself; additionaly, within_face_16x16_transpose flag could require transpose;
     // if we have the flag set with REDUCE_ROW, we don't need to do anything
-    cfg_reg_rmw_tensix<THCON_SEC0_REG2_Haloize_mode_RMW>(ckernel::ReduceDim::REDUCE_ROW == dim ? !within_face_16x16_transpose : within_face_16x16_transpose);
+    ckernel::cfg_reg_rmw_tensix<THCON_SEC0_REG2_Haloize_mode_RMW>(
+        ckernel::ReduceDim::REDUCE_ROW == dim ? !within_face_16x16_transpose : within_face_16x16_transpose);
 
-    TTI_SETADCXX(0b11, FACE_R_DIM * FACE_C_DIM - 1, 0x0);
+    TTI_SETADCXX(0b11, ckernel::FACE_R_DIM * ckernel::FACE_C_DIM - 1, 0x0);
 
     _llk_unpack_reduce_mop_config_<type, dim>();
 }
@@ -89,7 +90,7 @@ inline void _llk_unpack_reduce_(const std::uint32_t address)
     TTI_SETADCZW(0b011, 0, 0, 0, 0, 0b1111);
 
     // Program srcA and srcB base addresses
-    volatile uint tt_reg_ptr *cfg = get_cfg_pointer(); // get pointer to registers for current state ID
+    volatile uint tt_reg_ptr *cfg = ckernel::get_cfg_pointer(); // get pointer to registers for current state ID
 
     // Wait for free context
     ckernel::unpacker::wait_for_next_context(2);
@@ -105,13 +106,13 @@ inline void _llk_unpack_reduce_(const std::uint32_t address)
     }
 
     // Trisc::SEMPOST for context acquire
-    semaphore_post(semaphore::UNPACK_SYNC);
+    ckernel::semaphore_post(ckernel::semaphore::UNPACK_SYNC);
 
     // Load only 16 datums into srcB
-    TTI_SETADCXX(p_setadc::UNP1, FACE_C_DIM - 1, 0x0);
+    TTI_SETADCXX(ckernel::p_setadc::UNP1, ckernel::FACE_C_DIM - 1, 0x0);
 
     // Stall unpacker until pending CFG writes from Trisc have completed
-    TTI_STALLWAIT(p_stall::STALL_UNPACK, p_stall::TRISC_CFG);
+    TTI_STALLWAIT(ckernel::p_stall::STALL_UNPACK, ckernel::p_stall::TRISC_CFG);
 
     // Run MOP
     mop_run(0, 4);
@@ -120,10 +121,10 @@ inline void _llk_unpack_reduce_(const std::uint32_t address)
     TTI_SETADCXX(p_setadc::UNP1, FACE_R_DIM * FACE_C_DIM - 1, 0x0);
 
     // T6::SEMGET for context release
-    t6_semaphore_get(semaphore::UNPACK_SYNC);
+    ckernel::t6_semaphore_get(ckernel::semaphore::UNPACK_SYNC);
 
     // Switch unpacker config context
-    switch_config_context(unp_cfg_context);
+    ckernel::unpacker::switch_config_context(unp_cfg_context);
 
 #ifdef PERF_DUMP
     first_unpack_recorded = true;
