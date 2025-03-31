c31e390ffdb7274aced91c98913e20b6125c6ce5
Stefan Krsmanovic <skrsmanovic@tenstorrent.com>
2025-03-31 08:58:56 +0000

    #0: Fix unpack tilize code to support BH tilize perf measurement

--- tt_llk_blackhole/llk_lib/llk_unpack_tilize.h
+++ tt_llk_blackhole/llk_lib/llk_unpack_tilize.h
@@ -24,7 +24,7 @@ inline void _llk_unpack_tilize_mop_config_(const bool narrow_tile=false) {
             static constexpr uint unpack_srcb_set_dvalid = TT_OP_UNPACR_NOP(SrcB, 0, 0, p_unpacr_nop::SET_DVALID, 0, 0, 0, 0, p_unpacr_nop::UNP_ZEROSRC);
         #else
             static constexpr uint unpack_srca = TT_OP_UNPACR(SrcA, 0b1 /*Z inc*/, 0, 0, 0, 1 /* Set OvrdThreadId*/, 0 /*Set Dvalid*/, p_unpacr::RAREFYB_DISABLE, 0, 0, 0, 0, 1);
-            static constexpr uint unpack_srcb_set_dvalid = TT_OP_UNPACR_NOP(SrcB, 0, 0, p_unpacr_nop::SET_DVALID, 0, 0, 0, 0, p_unpacr_nop::UNP_ZEROSRC);
+            static constexpr uint unpack_srcb_set_dvalid = TT_OP_UNPACR_NOP(SrcB, 0, 0, 0, 0, 0, 0, 0, p_unpacr_nop::UNP_ZEROSRC);
         #endif
     #endif
 
