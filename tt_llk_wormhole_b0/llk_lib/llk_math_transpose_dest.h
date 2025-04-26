b26b8499fa3c41a2340d718cd1395c00e618b512
Jason Davies <jason@jasondavies.com>
2025-04-26 15:00:49 +0000

    Add support for 32-bit types to transpose_dest.
    As suggested by @corsix, this performs a transpose on a 16x16 face of 32-bit
    values by splitting into two faces of 16-bit values (the low 16 bits and high
    16 bits), transposing each face separately using `TRNSPSRCB`, and then
    recombining into 32-bit values.
    
    A neat way to achieve this (also suggested by @corsix!) is to use the
    `dest_32b_lo` instruction mode flag on `MOVD2B`/`MOVB2D`.  This allows the low
    bits to be copied to SrCB, transposed, and copied back.  Followed by the same
    operation on the high bits.
    
    Further optimisation would involve using the full 32 slots in the replay
    buffer, rather than overwriting slots 16 to 32 as I'm doing currently.
    
    This was just the most straightforward way to get a working proof of concept.

--- tt_llk_wormhole_b0/llk_lib/llk_math_transpose_dest.h
+++ tt_llk_wormhole_b0/llk_lib/llk_math_transpose_dest.h
@@ -17,7 +17,10 @@ using namespace ckernel;
 
 // local function declarations
 inline void transpose_dest_configure_addrmod();
+inline void transpose_dest_configure_mop_hi();
+inline void transpose_dest_configure_mop();
 
+template<bool is_32bit = false>
 inline void _llk_math_transpose_dest_(const std::uint32_t dst_index)
 {
     math::set_dst_write_addr<DstTileLayout::Default, DstTileShape::Tile32x32>(dst_index);
@@ -29,9 +32,21 @@ inline void _llk_math_transpose_dest_(const std::uint32_t dst_index)
     TTI_REPLAY(20, 5, 0, 0);
     TTI_REPLAY(26, 4, 0, 0);
 
-    TTI_SETRWC(p_setrwc::CLR_AB, 0, 0, 0, 0, p_setrwc::SET_AB);
-
     math::clear_dst_reg_addr();
+
+    if constexpr (is_32bit)
+    {
+        transpose_dest_configure_mop_hi();
+        ckernel_template::run(instrn_buffer);
+
+        TTI_REPLAY(20, 5, 0, 0);
+        TTI_REPLAY(26, 4, 0, 0);
+
+        math::clear_dst_reg_addr();
+        transpose_dest_configure_mop(); // TODO use length 32 replay buffer
+    }
+
+    TTI_SETRWC(p_setrwc::CLR_AB, 0, 0, 0, 0, p_setrwc::SET_AB);
 }
 
 inline void transpose_dest_configure_addrmod()
@@ -56,6 +71,51 @@ inline void transpose_dest_configure_addrmod()
         .dest = {.incr = -16},
     }
         .set(ADDR_MOD_2);
+}
+
+inline void transpose_dest_configure_mop_hi()
+{
+    TTI_REPLAY(16, 16, 0, 1);
+
+    // A
+    TTI_MOVD2B(1, p_movd2b::SRC_ZERO_OFFSET + 0, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 0 - 16);
+    TTI_MOVD2B(1, p_movd2b::SRC_ZERO_OFFSET + 4, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 4 - 16);
+    TTI_MOVD2B(1, p_movd2b::SRC_ZERO_OFFSET + 8, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 8 - 16);
+    TTI_MOVD2B(1, p_movd2b::SRC_ZERO_OFFSET + 12, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 12 - 16);
+
+    // B
+    TTI_MOVD2B(1, p_movd2b::SRC_ROW16_OFFSET + 0, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 0);
+    TTI_MOVD2B(1, p_movd2b::SRC_ROW16_OFFSET + 4, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 4);
+    TTI_MOVD2B(1, p_movd2b::SRC_ROW16_OFFSET + 8, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 8);
+    TTI_MOVD2B(1, p_movd2b::SRC_ROW16_OFFSET + 12, ADDR_MOD_1, p_movd2b::MOV_4_ROWS, 12);
+
+    // C
+    TTI_TRNSPSRCB;
+
+    // D
+    TTI_MOVD2B(1, p_movd2b::SRC_ZERO_OFFSET + 32, ADDR_MOD_2, p_movd2b::MOV_1_ROW, 0); // throwaway to decrement dst
+
+    // E
+    TTI_MOVB2D(1, p_movd2b::SRC_ROW16_OFFSET + 0, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 0);
+    TTI_MOVB2D(1, p_movd2b::SRC_ROW16_OFFSET + 4, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 4);
+    TTI_MOVB2D(1, p_movd2b::SRC_ROW16_OFFSET + 8, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 8);
+    TTI_MOVB2D(1, p_movd2b::SRC_ROW16_OFFSET + 12, ADDR_MOD_0, p_movb2d::MOV_4_ROWS, 12);
+
+    // F
+    TTI_MOVB2D(1, p_movd2b::SRC_ZERO_OFFSET + 0, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 0);
+    TTI_MOVB2D(1, p_movd2b::SRC_ZERO_OFFSET + 4, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 4);
+
+    uint AF = TT_OP_REPLAY(16, 16, 0, 0);
+    uint BC = TT_OP_REPLAY(20, 5, 0, 0);
+    uint E  = TT_OP_REPLAY(26, 4, 0, 0);
+    uint X  = TT_OP_MOVB2D(1, p_movd2b::SRC_ZERO_OFFSET + 8, ADDR_MOD_1, p_movb2d::MOV_4_ROWS, 8);
+    uint Y  = TT_OP_MOVB2D(1, p_movd2b::SRC_ZERO_OFFSET + 12, ADDR_MOD_0, p_movb2d::MOV_4_ROWS, 12);
+
+    ckernel_template tmp(1, 2, E, BC);
+    tmp.set_start_op(BC);
+    tmp.set_last_outer_loop_instr(AF);
+    tmp.set_end_ops(X, Y);
+    tmp.program(instrn_buffer);
 }
 
 inline void transpose_dest_configure_mop()
