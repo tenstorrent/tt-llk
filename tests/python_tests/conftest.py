b077bd31c1c6d1a40417fa09bbf88ab410ab9a4d
skotarac <skotarac@tenstorrent.com>
2025-05-05 14:29:40 +0000

    fixed format logging when dest accumulation is turned on, avoids touching __builtins

--- tests/python_tests/conftest.py
+++ tests/python_tests/conftest.py
@@ -89,9 +89,12 @@ def pytest_runtest_protocol(item, nextitem):
 
 
 _format_log = []
+
+
 def add_to_format_log(input_fmt, output_fmt):
     global _format_log
     _format_log.append((input_fmt, output_fmt))
+
 
 def pytest_sessionfinish(session, exitstatus):
     BOLD = "\033[1m"
