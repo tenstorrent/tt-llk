4b0f9bf23f4fbc9cf3b3aebcd18ad47828033b44
Aswin Zayasankaran <156493059+Aswinmcw@users.noreply.github.com>
2025-05-10 12:57:53 +0530

    WH ins mod update

--- tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_binary_bitwise.h
+++ tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_binary_bitwise.h
@@ -19,7 +19,7 @@ enum class BinaryBitwiseOp : uint8_t
     XOR = 2,
 };
 
-template <bool APPROXIMATION_MODE, BinaryBitwiseOp BITWISE_OP, int ITERATIONS = 8>
+template <bool APPROXIMATION_MODE, BinaryBitwiseOp BITWISE_OP, uint DTYPE = INT32, int ITERATIONS = 8>
 inline void _calculate_sfpu_binary_bitwise_(const uint dst_offset)
 {
     // SFPU microcode
@@ -27,8 +27,8 @@ inline void _calculate_sfpu_binary_bitwise_(const uint dst_offset)
     {
         constexpr uint dst_tile_size = 64;
 
-        TTI_SFPLOAD(0, 4, 3, 0);
-        TT_SFPLOAD(1, 4, 3, dst_offset * dst_tile_size);
+        TTI_SFPLOAD(0, DTYPE, 3, 0);
+        TT_SFPLOAD(1, DTYPE, 3, dst_offset * dst_tile_size);
 
         if constexpr (BITWISE_OP == BinaryBitwiseOp::AND)
         {
@@ -43,7 +43,7 @@ inline void _calculate_sfpu_binary_bitwise_(const uint dst_offset)
             TTI_SFPXOR(0, 1, 0, 0);
         }
 
-        TTI_SFPSTORE(0, 4, 3, 0);
+        TTI_SFPSTORE(0, DTYPE, 3, 0);
         sfpi::dst_reg++;
     }
 }
