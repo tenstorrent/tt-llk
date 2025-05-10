0f11b3ab1a1e0c3c65a0a5fd42fe230e5bb8bc07
Aswin Zayasankaran <156493059+Aswinmcw@users.noreply.github.com>
2025-05-10 14:44:53 +0530

    Add enum

--- tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_binary_bitwise.h
+++ tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_binary_bitwise.h
@@ -19,16 +19,33 @@ enum class BinaryBitwiseOp : uint8_t
     XOR = 2,
 };
 
-template <bool APPROXIMATION_MODE, BinaryBitwiseOp BITWISE_OP, uint DTYPE = 4, int ITERATIONS = 8>
+enum class DataType : uint8_t
+{
+    DEFAULT    = 0,
+    FP16A      = 1,
+    FP16B      = 2,
+    FP32       = 3,
+    INT32      = 4,
+    INT32_COMP = 5,
+    INT8       = 6,
+    INT8_COMP  = 7,
+    LO16       = 8,
+    LO16_ONLY  = 9,
+    HI16       = 10,
+    HI16_ONLY  = 11,
+};
+
+template <bool APPROXIMATION_MODE, BinaryBitwiseOp BITWISE_OP, DataType DTYPE = INT32, int ITERATIONS = 8>
 inline void _calculate_sfpu_binary_bitwise_(const uint dst_offset)
 {
+    constexpr uint8_t dtype = static_cast<uint8_t>(DTYPE);
     // SFPU microcode
     for (int d = 0; d < ITERATIONS; d++)
     {
         constexpr uint dst_tile_size = 64;
 
-        TTI_SFPLOAD(0, DTYPE, 3, 0);
-        TT_SFPLOAD(1, DTYPE, 3, dst_offset * dst_tile_size);
+        TTI_SFPLOAD(0, dtype, 3, 0);
+        TT_SFPLOAD(1, dtype, 3, dst_offset * dst_tile_size);
 
         if constexpr (BITWISE_OP == BinaryBitwiseOp::AND)
         {
@@ -43,7 +60,7 @@ inline void _calculate_sfpu_binary_bitwise_(const uint dst_offset)
             TTI_SFPXOR(0, 1, 0, 0);
         }
 
-        TTI_SFPSTORE(0, DTYPE, 3, 0);
+        TTI_SFPSTORE(0, dtype, 3, 0);
         sfpi::dst_reg++;
     }
 }
