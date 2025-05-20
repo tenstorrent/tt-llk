823f6f8961bcb80befe6334b15a5aed49931f084
Lazar Djurovic <ldjurovic@tenstorrent.com>
2025-05-20 09:34:39 +0000

    Moved transpose bit to 0

--- tests/sources/reduce_test.cpp
+++ tests/sources/reduce_test.cpp
@@ -16,8 +16,10 @@ uint32_t math_sync_tile_dst_index = 0;
 
 #ifdef REDUCE_ROW_OPERATION
 const std::uint32_t within_face_16x16_transpose = 1;
+const bool row_pool = true;
 #else
 const std::uint32_t within_face_16x16_transpose = 0;
+const bool row_pool = false;
 #endif
 
 #ifdef LLK_TRISC_UNPACK
@@ -33,7 +35,7 @@ void run_kernel()
 
     _llk_unpack_AB_hw_configure_<is_fp32_dest_acc_en, StochRndType::None>(
         UNPACK_A_IN, UNPACK_B_IN, UNPACK_A_OUT, UNPACK_B_OUT, FACE_R_DIM, within_face_16x16_transpose);
-    _llk_unpack_AB_init_<>(FACE_C_DIM, 4, false, within_face_16x16_transpose, 0);
+    _llk_unpack_AB_init_<>(FACE_C_DIM, 4, false, 0, 0);
     _llk_unpack_AB_<>(L1_ADDRESS(buffer_A), L1_ADDRESS(buffer_B), within_face_16x16_transpose);
 }
 
@@ -45,11 +47,9 @@ void run_kernel()
 #include "llk_math_reduce.h"
 #include "params.h"
 
-const bool row_pool = false;
-
 void run_kernel()
 {
-    const std::uint32_t math_fid = 4;
+    const std::uint32_t math_fid = 0;
     const bool is_int_fpu_en     = false;
 
     _llk_math_pack_sync_init_<DstSync::SyncFull, is_fp32_dest_acc_en>();
