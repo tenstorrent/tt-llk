7a16d0e267b7b8f8e815c62dbd69a4bdf9500f5d
Jason Davies <jason@jasondavies.com>
2025-05-27 17:35:38 +0100

    Fix.

--- tt_llk_blackhole/llk_lib/llk_unpack_tilize.h
+++ tt_llk_blackhole/llk_lib/llk_unpack_tilize.h
@@ -318,7 +318,7 @@ inline void _llk_unpack_tilizeA_B_(
         Face 2: address = base_address + block_ct_dim * TILE_C_DIM * face_r_dim (address for the bottom 2 faces of tiles)
         Face 3: address = base_address + block_ct_dim * TILE_C_DIM * face_r_dim + 1x16 row of datums
         */
-        std::uint32_t address_face_a = (n % 2 == 0) ? address_a : (address_a + (SCALE_DATUM_SIZE(unpack_src_format[operandA_id], FACE_C_DIM) >> 4));
+        std::uint32_t address_face_a = (n % 2 == 0) ? address_a : (address_a + (SCALE_DATUM_SIZE(unpA_src_format, FACE_C_DIM) >> 4));
         address_face_a += (n >= 2) ? ((SCALE_DATUM_SIZE(unpack_src_format[operandA_id], block_c_dim)) >> 4) : 0;
 
         // Wait for free context
