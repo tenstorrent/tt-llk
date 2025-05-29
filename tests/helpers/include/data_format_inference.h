1a96d573fbd71ba590852f528050e6696aa8a640
skotarac <skotarac@tenstorrent.com>
2025-05-29 07:42:43 +0000

    fixing data format inference, was ignoring specific case in wh due to if-else placement

--- tests/helpers/include/data_format_inference.h
+++ tests/helpers/include/data_format_inference.h
@@ -64,11 +64,6 @@ constexpr FormatConfig get_data_formats(uint32_t input, uint32_t output, bool is
     {
         pack_in = static_cast<uint32_t>(DataFormat::Bfp8);
     }
-    else if (is_wormhole && is_fp32_dest_acc_en && output == (uint32_t)DataFormat::Float16)
-    {
-        pack_in = static_cast<uint32_t>(DataFormat::Float32); // Gasket in wormhole cannot convert fp32 to fp16, and since dest accumulation turns on for
-                                                              // outlier cases we have fp32 in dest, so gasket cannot convert it to fp16, packer must do that
-    }
     else if (is_format_combination_outlier(input, output, is_fp32_dest_acc_en))
     {
         pack_in = (is_wormhole) ? (uint32_t)DataFormat::Float32 : output;
@@ -76,6 +71,12 @@ constexpr FormatConfig get_data_formats(uint32_t input, uint32_t output, bool is
     else
     {
         pack_in = is_fp32_dest_acc_en ? output : input;
+    }
+
+    if (is_wormhole && is_fp32_dest_acc_en && output == (uint32_t)DataFormat::Float16)
+    {
+        pack_in = static_cast<uint32_t>(DataFormat::Float32); // Gasket in wormhole cannot convert fp32 to fp16, and since dest accumulation turns on for
+                                                              // outlier cases we have fp32 in dest, so gasket cannot convert it to fp16, packer must do that
     }
 
     return {unpack_in, unpack_out, pack_in, pack_out};
