e948e8e2cb82fdfd381a05c5b8ca101172107d65
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-05-29 15:23:46 +0000

    foreshadow load_replay_buf api change

--- tt_llk_blackhole/common/inc/ckernel.h
+++ tt_llk_blackhole/common/inc/ckernel.h
@@ -10,6 +10,12 @@
 // MT: This should be dissolved and moved to the appropriate place
 #include "tensix.h"
 
+// This header is include on non-trisc builds, for reasons
+// unknown. lltt is only available on trisc
+#if defined(COMPILE_FOR_TRISC)
+#include <utility>
+#endif
+
 // Compiler hint that a branch is unlikely to be taken
 #define UNLIKELY(condition) __builtin_expect(static_cast<bool>(condition), 0)
 #define UNROLL_LOOP(factor) GCC unroll factor
@@ -683,28 +689,40 @@ inline void enable_gathering()
             : "t1");
 }
 
-// Pass a lambda function (or a regular function pointer) that takes void,
-// returns void, and issues the instructions you want to load into the
-// replay buffer. start, len, and exec_while_loading have the same meaning
-// as they do for the REPLAY instruction, as descired in assembly.yaml.
+#if defined(COMPILE_FOR_TRISC)
+// Place instructions into the replay buffer. EXEC is true to execute
+// when loading (default is false). START is where to place in the
+// replay buffer, and LEN is the number of instructions to record
+// (should match the expansion of CALLABLE). CALLABLE is a callable,
+// to which ARGS are forwarded.
+template <bool Exec = false, typename Callable, typename... Args>
+[[gnu::always_inline, gnu::flatten]] inline auto load_replay_buf(uint start, uint len, Callable callable, Args&&... args)
+{
+    // Workaround for tt-metal#16439, making sure gathering is disabled
+    // WE DON'T UNDERSTAND WHY ENABLING GATHERING DOESN'T WORK
+    struct protect {
+        protect() {
+            // disable_gathering();
+        }
+        ~protect() {
+            // enable_gathering();
+        }
+    } _;
+
+    // Issue instruction to load replay buffer
+    TTI_REPLAY(start, len, Exec, 1);
+
+    // Send in the user's desired instructions
+    return callable(std::forward<Args>(args)...);
+}
+
+// Deprecated entry points, will be deleted.
 template <uint start, uint len, bool exec_while_loading = false, typename F>
 inline void load_replay_buf(F fn)
 {
-    // disable_gathering();
-
-    // Issue instruction to load replay buffer
-    TTI_REPLAY(start, len, exec_while_loading, 1);
-
-    // Send in the user's desired instructions
-    fn();
-
-    // Workaround for tt-metal#16439, making sure gathering is disabled
-    // WE DON'T UNDERSTAND WHY ENABLING GATHERING DOESN'T WORK
-    // enable_gathering();
+    load_replay_buf<exec_while_loading>(start, len, fn);
 }
 
-// Same as above, but used if start/len/exec_while_loading are not known
-// at compiletime.
 template <typename F>
 inline void load_replay_buf(uint start, uint len, bool exec_while_loading, F fn)
 {
@@ -720,6 +738,7 @@ inline void load_replay_buf(uint start, uint len, bool exec_while_loading, F fn)
     // WE DON'T UNDERSTAND WHY ENABLING GATHERING DOESN'T WORK
     // enable_gathering();
 }
+#endif
 
 enum class CSR : uint16_t
 {
