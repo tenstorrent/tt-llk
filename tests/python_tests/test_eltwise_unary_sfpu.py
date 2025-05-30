284f9ebfa05b6963bdd2dacfd81cc36c02472ea5
Jason Davies <jason@jasondavies.com>
2025-05-30 19:59:56 +0100

    Re-enable Cos test.

--- tests/python_tests/test_eltwise_unary_sfpu.py
+++ tests/python_tests/test_eltwise_unary_sfpu.py
@@ -78,7 +78,7 @@ all_params = generate_params(
     approx_mode=[ApproximationMode.No, ApproximationMode.Yes],
     mathop=[
         MathOperation.Abs,
-        # MathOperation.Cos,
+        MathOperation.Cos,
         MathOperation.Log,
         MathOperation.Reciprocal,
         MathOperation.Rsqrt,
