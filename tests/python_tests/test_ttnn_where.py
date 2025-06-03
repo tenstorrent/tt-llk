078c7b89d744ca3a5334dcd3451822cf10116d1c
Lazar Djurovic <ldjurovic@tenstorrent.com>
2025-06-03 09:35:20 +0000

    Prototype implementation of ttnn.where

---
+++ tests/python_tests/test_ttnn_where.py
@@ -0,0 +1,171 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+# SPDX-License-Identifier: Apache-2.0
+
+import math
+
+import pytest
+import torch
+
+from helpers.chip_architecture import ChipArchitecture, get_chip_architecture
+from helpers.device import (
+    collect_results,
+    run_elf_files,
+    wait_for_tensix_operations_finished,
+    write_stimuli_to_l1,
+)
+from helpers.format_arg_mapping import (
+    ApproximationMode,
+    DestAccumulation,
+    MathOperation,
+    format_dict,
+)
+from helpers.format_config import DataFormat
+from helpers.param_config import (
+    clean_params,
+    generate_param_ids,
+    generate_params,
+    input_output_formats,
+)
+from helpers.stimuli_generator import generate_stimuli
+from helpers.test_config import generate_make_command
+from helpers.utils import compare_pcc, run_shell_command
+
+
+def generate_golden(operation, operand1, data_format):
+    tensor1_float = (
+        operand1.clone()
+        .detach()
+        .to(format_dict[data_format] if data_format != "Bfp8_b" else torch.bfloat16)
+    )
+    ops = {
+        MathOperation.Abs: lambda x: abs(x),
+        MathOperation.Cos: lambda x: math.cos(x),
+        MathOperation.Log: lambda x: math.log(x) if x != 0 else float("nan"),
+        MathOperation.Reciprocal: lambda x: 1 / x if x != 0 else float("nan"),
+        MathOperation.Sin: lambda x: math.sin(x),
+        MathOperation.Sqrt: lambda x: math.sqrt(x),
+        MathOperation.Square: lambda x: x * x,
+    }
+    if operation not in ops:
+        raise ValueError("Unsupported operation!")
+    return [ops[operation](num) for num in tensor1_float.tolist()][:256]
+
+
+# SUPPORTED FORMATS FOR TEST
+supported_formats = [DataFormat.Float16_b]
+
+#   INPUT-OUTPUT FORMAT SWEEP
+#   input_output_formats(supported_formats)
+
+#   FULL FORMAT SWEEP
+#   format_combination_sweep(formats=supported_formats, all_same=False, same_src_reg_format=True)
+
+#   SPECIFIC FORMAT COMBINATION
+#   generate_combination(
+#       [(DataFormat.Float16_b,  # index 0 is for unpack_A_src
+#         DataFormat.Float16_b,  # index 1 is for unpack_A_dst
+#         DataFormat.Float16_b,  # index 2 is for pack_src (if src registers have same formats)
+#         DataFormat.Bfp8_b,  # index 3 is for pack_dst
+#         DataFormat.Float16_b,  # index 4 is for math format)])
+
+#   SPECIFIC INPUT-OUTPUT COMBINATION
+#   [InputOutputFormat(DataFormat.Float16, DataFormat.Float32)]
+
+test_formats = input_output_formats(supported_formats)
+all_params = generate_params(
+    ["ttnn_where_test"],
+    test_formats,
+    dest_acc=[DestAccumulation.No],
+    approx_mode=[ApproximationMode.No],
+    mathop=[
+        MathOperation.Abs,
+    ],
+)
+param_ids = generate_param_ids(all_params)
+
+
+@pytest.mark.parametrize(
+    "testname, formats, dest_acc, approx_mode, mathop",
+    clean_params(all_params),
+    ids=param_ids,
+)
+def test_eltwise_unary_sfpu(testname, formats, dest_acc, approx_mode, mathop):
+    arch = get_chip_architecture()
+    if (
+        formats.input_format in [DataFormat.Float32, DataFormat.Int32]
+        and dest_acc != DestAccumulation.Yes
+    ):
+        pytest.skip(
+            reason="Skipping test for 32 bit wide data without 32 bit accumulation in Dest"
+        )
+
+    if formats.input_format == DataFormat.Float16 and (
+        dest_acc == DestAccumulation.No and arch == ChipArchitecture.BLACKHOLE
+    ):
+        pytest.skip(reason="This combination is not fully implemented in testing")
+
+
+    src_A = torch.tensor([[1.0 if i % 2 == 0 else 0.0 for _ in range(32)] for i in range(32)]).flatten()
+    src_B = torch.zeros(1024)
+
+    golden = generate_golden(mathop, src_A, formats.output_format)
+    write_stimuli_to_l1(src_A, src_B, formats.input_format, formats.input_format)
+
+    test_config = {
+        "formats": formats,
+        "testname": testname,
+        "dest_acc": dest_acc,
+        "mathop": mathop,
+        "approx_mode": approx_mode,
+    }
+
+    make_cmd = generate_make_command(test_config)
+    run_shell_command(f"cd .. && {make_cmd}")
+    run_elf_files(testname)
+
+    wait_for_tensix_operations_finished()
+    res_from_L1 = collect_results(
+        formats, tensor_size=len(src_A)
+    )  # Bug patchup in (unpack.py): passing formats struct to check unpack_src with pack_dst and distinguish when input and output formats have different exponent widths then reading from L1 changes
+    res_from_L1 = res_from_L1[
+        :256
+    ]  # this will be removed once we implement to read bytes from L1 according to data format (size of datum) which will be added in next PR
+    assert len(res_from_L1) == len(golden)
+
+    golden_tensor = torch.tensor(
+        golden,
+        dtype=(
+            format_dict[formats.output_format]
+            if formats.output_format in [DataFormat.Float16, DataFormat.Float16_b]
+            else torch.bfloat16
+        ),
+    )
+    res_tensor = torch.tensor(
+        res_from_L1,
+        dtype=(
+            format_dict[formats.output_format]
+            if formats.output_format in [DataFormat.Float16, DataFormat.Float16_b]
+            else torch.bfloat16
+        ),
+    )
+
+    if formats.output_format in [
+        DataFormat.Float16_b,
+        DataFormat.Float16,
+        DataFormat.Float32,
+    ]:
+        atol = 0.05
+        rtol = 0.1
+    elif formats.output_format == DataFormat.Bfp8_b:
+        atol = 0.05
+        rtol = 0.1
+
+    print(res_tensor.view(32,32))
+
+    for i in range(len(golden)):
+        assert torch.isclose(
+            golden_tensor[i], res_tensor[i], rtol=rtol, atol=atol
+        ), f"Failed at index {i} with values {golden[i]} and {res_from_L1[i]}"
+
+    _, pcc = compare_pcc(golden_tensor, res_tensor, pcc=0.99)
+    assert pcc > 0.99
---
+++ tests/sources/ttnn_where_test.cpp
@@ -0,0 +1,151 @@
+#include <algorithm>
+#include <cstdint>
+#include <cstdio>
+#include <type_traits>
+
+#include "ckernel.h"
+#include "llk_defs.h"
+// Globals
+uint32_t unp_cfg_context          = 0;
+uint32_t pack_sync_tile_dst_ptr   = 0;
+uint32_t math_sync_tile_dst_index = 0;
+
+template <ckernel::ThreadId thread_id>
+inline void dbg_thread_halt()
+{
+    static_assert(
+        (thread_id == ckernel::ThreadId::MathThreadId) || (thread_id == ckernel::ThreadId::UnpackThreadId) || (thread_id == ckernel::ThreadId::PackThreadId),
+        "Invalid thread id set in dbg_wait_for_thread_idle(...)");
+
+    if constexpr (thread_id == ckernel::ThreadId::UnpackThreadId)
+    {
+        // Wait for all instructions on the running thread to complete
+        ckernel::tensix_sync();
+        // Notify math thread that unpack thread is idle
+        ckernel::mailbox_write(ckernel::ThreadId::MathThreadId, 1);
+        // Wait for math thread to complete debug dump
+        volatile uint32_t temp = ckernel::mailbox_read(ckernel::ThreadId::MathThreadId);
+    }
+    else if constexpr (thread_id == ckernel::ThreadId::MathThreadId)
+    {
+        // Wait for all instructions on the running thread to complete
+        ckernel::tensix_sync();
+        // Wait for unpack thread to complete
+        volatile uint32_t temp = ckernel::mailbox_read(ckernel::ThreadId::UnpackThreadId);
+        // Wait for previous packs to finish
+        while (ckernel::semaphore_read(ckernel::semaphore::MATH_PACK) > 0)
+        {
+        };
+    }
+}
+
+void dbg_halt() {
+    dbg_thread_halt<ckernel::MathThreadId>();
+}
+
+
+
+
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+
+#ifdef LLK_TRISC_UNPACK
+
+#include "llk_unpack_A.h"
+#include "llk_unpack_common.h"
+#include "params.h"
+
+void run_kernel()
+{
+    volatile uint32_t* const buffer_A = reinterpret_cast<volatile uint32_t*>(0x1a000);
+
+    _llk_unpack_A_hw_configure_<is_fp32_dest_acc_en, StochRndType::None>(UNPACK_A_IN, UNPACK_A_OUT, FACE_R_DIM, 0, 4);
+    _llk_unpack_A_init_<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, unpack_to_dest>(0, 0, FACE_R_DIM, 4, UNPACK_A_IN, UNPACK_A_OUT);
+    _llk_unpack_A_<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, unpack_to_dest>(L1_ADDRESS(buffer_A), 0, UNPACK_A_IN, UNPACK_A_OUT);
+}
+
+#endif
+
+#ifdef LLK_TRISC_MATH
+
+#include "ckernel_sfpu.h"
+#include "llk_math_common.h"
+#include "llk_math_eltwise_unary_datacopy.h"
+#include "llk_math_eltwise_unary_sfpu.h"
+#include "params.h"
+#include "ckernel_sfpu_where.h"
+
+using namespace ckernel;
+using namespace ckernel::sfpu;
+
+void run_kernel()
+{
+// copy srca to dest
+#ifdef ARCH_BLACKHOLE
+    _llk_math_eltwise_unary_datacopy_init_<DataCopyType::A2D, BroadcastType::NONE, false, is_fp32_dest_acc_en, false>(0, 0, 4, MATH_FORMAT);
+#else
+    _llk_math_eltwise_unary_datacopy_init_<DataCopyType::A2D, BroadcastType::NONE, is_fp32_dest_acc_en, false>(0, 0, 4, MATH_FORMAT);
+#endif
+    _llk_math_pack_sync_init_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+    _llk_math_hw_configure_<false, false>(MATH_FORMAT, MATH_FORMAT);
+    _llk_math_wait_for_dest_available_<DstSync::SyncHalf>();
+    _llk_math_eltwise_unary_datacopy_<DataCopyType::A2D, DstSync::SyncHalf, BroadcastType::NONE, is_fp32_dest_acc_en, unpack_to_dest>(
+        0, MATH_FORMAT, MATH_FORMAT);
+
+    // calculation of sfpu operation on dest
+    _llk_math_eltwise_unary_sfpu_init_<SFPU_OPERATION>();
+    _llk_math_eltwise_unary_sfpu_start_<DstSync::SyncHalf>(0);
+    // calling sfpu function from ckernel
+    // this part is where parametrization of operation takes part
+    //math::clear_dst_reg_addr();
+    _calculate_where_<32>(0,2,3);
+    dbg_halt();
+
+    _llk_math_eltwise_unary_sfpu_done_();
+    _llk_math_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+}
+
+#endif
+
+#ifdef LLK_TRISC_PACK
+
+#include "llk_pack.h"
+#include "llk_pack_common.h"
+#include "params.h"
+
+void run_kernel()
+{
+    // If data foramt is Bfp8 it is calculated correctly in Dest but packer cannot pack just that one face
+    // TODO: make it so It can
+    // So for now It is packed as Float16_b
+
+#ifdef PACK_DST_BFP8_B
+    constexpr auto PACK_OUT = static_cast<std::underlying_type_t<DataFormat>>(DataFormat::Float16_b);
+#endif
+
+    volatile uint32_t* const buffer_Dest = reinterpret_cast<volatile uint32_t*>(0x1c000);
+
+    std::fill(buffer_Dest, buffer_Dest + 16 * 16 * 4, 0xdeadbeef);
+
+#ifdef ARCH_BLACKHOLE
+    _llk_pack_hw_configure_<false, is_fp32_dest_acc_en, false>(PACK_IN, PACK_OUT, 16 * 16 * 4);
+#else
+    _llk_pack_hw_configure_<false, is_fp32_dest_acc_en>(PACK_IN, PACK_OUT, 16 * 16 * 4);
+#endif
+
+    _llk_pack_init_<false, false, DstTileFaceLayout::RowMajor, false>(PACK_OUT);
+
+#ifdef ARCH_BLACKHOLE
+    _llk_pack_dest_init_<DstSync::SyncHalf, DstTileFaceLayout::RowMajor, is_fp32_dest_acc_en>();
+#else
+    _llk_pack_dest_init_<DstSync::SyncHalf, DstTileFaceLayout::RowMajor, false, false>();
+#endif
+
+    _llk_packer_wait_for_math_done_();
+    _llk_pack_<DstSync::SyncHalf, false, is_fp32_dest_acc_en>(0, L1_ADDRESS(buffer_Dest));
+    _llk_pack_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+}
+
+#endif
