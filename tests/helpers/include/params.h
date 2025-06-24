4f393de660ccb86725293b91ee3576740bfb05f8
Aswinmcw <azayasankaran@tenstorrent.com>
2025-06-24 11:19:55 +0000

    feat: Add negative op support

--- tests/helpers/include/params.h
+++ tests/helpers/include/params.h
@@ -90,6 +90,9 @@ constexpr auto SFPU_BINARY_OPERATION = ckernel::BinaryOp::RSHFT;
 #ifdef SFPU_OP_LSHFT
 constexpr auto SFPU_BINARY_OPERATION = ckernel::BinaryOp::LSHFT;
 #endif
+#ifdef SFPU_OP_NEG
+constexpr auto SFPU_OPERATION = SfpuType::neg;
+#endif
 
 #ifdef SFPU_OP_SQRT
 constexpr auto SFPU_OPERATION = SfpuType::sqrt;
