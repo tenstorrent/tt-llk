3d7b00b31a797b0668e448a5a7f7baba5e456efe
Filip Vranic <fvranic@tenstorrent.com>
2025-06-26 21:24:17 +0200

    Update install-exalens.sh

--- .github/scripts/install-exalens.sh
+++ .github/scripts/install-exalens.sh
@@ -5,10 +5,10 @@
 
 set -e
 
-EXALENS_VERSION="0.1.250617+dev.32e94cb-cp310-cp310-linux_x86_64"
+EXALENS_VERSION="0.1.250626+dev.7538f60-cp310-cp310-linux_x86_64"
 EXALENS_WHEEL="ttexalens-${EXALENS_VERSION}.whl"
 
 wget -O ${EXALENS_WHEEL} \
-    https://github.com/tenstorrent/tt-exalens/releases/download/0.1.250617/${EXALENS_WHEEL}
+    https://github.com/tenstorrent/tt-exalens/releases/download/0.1.250626/${EXALENS_WHEEL}
 pip install --no-cache-dir ${EXALENS_WHEEL}
 rm ${EXALENS_WHEEL}
