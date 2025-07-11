7e21ff186ecffc0fffb1f9b01048c294c17d4ee6
VirdhatchaniKN <virdhatchani.narayanamoorthy@multicorewareinc.com>
2025-07-11 10:50:43 +0000

    #0: Exp axurate

--- tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_exp.h
+++ tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_exp.h
@@ -4,15 +4,37 @@
 
 #pragma once
 
-#include "ckernel_sfpu_exp.h"
 #include "ckernel_sfpu_recip.h"
 #include "sfpi.h"
 #include "sfpi_fp16.h"
+#include "ckernel_sfpu_rounding_ops.h"
 
 namespace ckernel
 {
 namespace sfpu
 {
+
+sfpi_inline sfpi::vFloat _sfpu_exp_21f_(sfpi::vFloat val) {
+    sfpi::vFloat y = 0.0f;
+    v_if(val > -88.0) {
+        sfpi::vInt z = sfpu::_float_to_int32_(val * sfpi::vFloat(0x00b8aa3b) + sfpi::vFloat(0x3f800000));
+        sfpi::vInt zii = z & 0x7f800000;
+        sfpi::vInt zif = z & sfpi::vInt(0x007fffff);  // extra mantissa
+
+        sfpi::vFloat d1 = sfpi::vFloat(0.40196114e-7);
+        sfpi::vFloat d2 = sfpi::int32_to_float(sfpi::vInt(0xf94ee7) + zif);
+        sfpi::vFloat d3 = sfpi::int32_to_float(sfpi::vInt(0x560) + zif);
+        d2 = d1 * d2;
+        zif = sfpu::_float_to_int32_(d2 * d3);
+
+        zii |= zif;  // restore exponent
+
+        y = sfpi::reinterpret<sfpi::vFloat>(zii);
+    }
+    v_endif;
+    return y;
+}
+
 
 sfpi_inline sfpi::vFloat _sfpu_exp_(sfpi::vFloat val)
 {
