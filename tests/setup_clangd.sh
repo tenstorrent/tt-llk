ffe0166d71aa39e9e91b1565a5fb38360d98fe85
Filip Vranic <fvranic@tenstorrent.com>
2025-07-18 12:04:23 +0000

    fix: correct clangd discovery

--- tests/setup_clangd.sh
+++ tests/setup_clangd.sh
@@ -24,21 +24,29 @@ case "$ARCH" in
         ;;
 esac
 
+ROOT_DIR=$(git rev-parse --show-toplevel)
+
 cat > compile_flags.txt <<EOF
 -D$ARCH_DEFINE
+-DTENSIX_FIRMWARE
+-std=c++17
 
 -DLLK_TRISC_UNPACK
 -DLLK_TRISC_MATH
 -DLLK_TRISC_PACK
 
--I../$ARCH_LLK_ROOT/llk_lib
--I../$ARCH_LLK_ROOT/common/inc
--I../$ARCH_LLK_ROOT/common/inc/sfpu
--Ihw_specific/inc
--Ifirmware/riscv/common
--Ifirmware/riscv/$CHIP_ARCH
--Isfpi/include
--Ihelpers/include
+-I$ROOT_DIR/tests/sfpi/compiler/riscv32-tt-elf/include
+-I$ROOT_DIR/tests/sfpi/compiler/riscv32-tt-elf/include/c++/12.4.0
+-I$ROOT_DIR/tests/sfpi/compiler/riscv32-tt-elf/include/c++/12.4.0/riscv32-tt-elf
+-I$ROOT_DIR/firmware/riscv/common
+-I$ROOT_DIR/firmware/riscv/$CHIP_ARCH
+-I$ROOT_DIR/tests/hw_specific/$CHIP_ARCH/inc
+-I$ROOT_DIR/$ARCH_LLK_ROOT/common/inc
+-I$ROOT_DIR/$ARCH_LLK_ROOT/common/inc/sfpu
+-I$ROOT_DIR/$ARCH_LLK_ROOT/llk_lib
+-I$ROOT_DIR/tests/sfpi/include
+-I$ROOT_DIR/tests/helpers/include
+-I$ROOT_DIR/tests/firmware/riscv/common
 EOF
 
 (pkill clangd && clangd >/dev/null 2>&1 &) || true  # restart clang if it's running
