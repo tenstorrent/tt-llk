c5d31c59f581b8baff615d4901bf4a507de2196c
KalaivaniMCW <azayasankaran@tenstorrent.com>
2025-07-28 14:22:52 +0000

    remove unused header

--- tt_llk_blackhole/llk_lib/llk_math_eltwise_ternary_sfpu_params.h
+++ tt_llk_blackhole/llk_lib/llk_math_eltwise_ternary_sfpu_params.h
@@ -7,7 +7,6 @@
 
 #include "llk_math_eltwise_ternary_sfpu.h"
 #include "llk_sfpu_types.h"
-#include "sfpu/ckernel_sfpu_where.h"
 
 template <bool APPROXIMATE, typename Callable, typename... Args>
 inline void _llk_math_eltwise_ternary_sfpu_params_(
