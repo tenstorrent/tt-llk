637a557546cf1d42ddaac1bf4d64283cf619faef
Jason Davies <jason@jasondavies.com>
2025-07-30 19:01:27 +0000

    Add layernorm_compat template param to rsqrt.

--- tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_rsqrt.h
+++ tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_rsqrt.h
@@ -13,16 +13,30 @@ namespace ckernel
 namespace sfpu
 {
 
-template <bool APPROXIMATION_MODE, int ITERATIONS>
+template <bool APPROXIMATION_MODE, int ITERATIONS, bool layernorm_compat>
 inline void _calculate_rsqrt_(int iterations)
 {
-    return _calculate_sqrt_internal_<APPROXIMATION_MODE, ITERATIONS, true>(iterations);
+    if constexpr (layernorm_compat)
+    {
+        return _calculate_sqrt_internal_<APPROXIMATION_MODE, ITERATIONS, true>(iterations);
+    }
+    else
+    {
+        return _calculate_rsqrt_compat_<APPROXIMATION_MODE, ITERATIONS>(iterations);
+    }
 }
 
-template <bool APPROXIMATION_MODE>
+template <bool APPROXIMATION_MODE, bool layernorm_compat>
 inline void _init_rsqrt_()
 {
-    _init_sqrt_<APPROXIMATION_MODE>();
+    if constexpr (layernorm_compat)
+    {
+        _init_sqrt_compat_<APPROXIMATION_MODE>();
+    }
+    else
+    {
+        _init_sqrt_<APPROXIMATION_MODE>();
+    }
 }
 
 } // namespace sfpu
