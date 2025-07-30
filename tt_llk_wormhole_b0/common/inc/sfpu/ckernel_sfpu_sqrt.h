6ff0c0377d5ae6aa092704cdfb0e356c6500f34b
Jason Davies <jason@jasondavies.com>
2025-07-30 13:06:48 +0100

    Remove unused variable.

--- tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_sqrt.h
+++ tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_sqrt.h
@@ -70,7 +70,6 @@ sfpi_inline sfpi::vFloat _calculate_sqrt_body_(const sfpi::vFloat x)
         y                          = y * (sfpi::vConstFloatPrgm1 + c * (sfpi::vConstFloatPrgm2 + c));
         xy                         = x * y;
         negative_y                 = -y;
-        sfpi::vFloat negative_xyy  = negative_y * xy;
         sfpi::vFloat one_minus_xyy = sfpi::vConst1 + (negative_y * xy);
 
         if constexpr (RECIPROCAL)
