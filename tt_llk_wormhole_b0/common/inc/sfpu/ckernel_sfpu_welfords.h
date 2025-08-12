9ee42964b1364b10199500c9a00908df8ee3b2f1
vsureshTT <vsuresh@tenstorrent.com>
2025-08-12 20:52:48 +0000

    LLK seems to work now on WH!

--- tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_welfords.h
+++ tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_welfords.h
@@ -16,31 +16,6 @@ using namespace sfpi;
 namespace ckernel {
 namespace sfpu {
 
-//Notes we use macros since the code crashes when we use inline functions.
-#define WELFORDS(row, past_mean, mean, past_var, var, N) { \
-                mean = row; \
-                mean -= past_mean; \
-                float inv_n_plus_1 = 1.0/((float)N+1.0); \
-                var = inv_n_plus_1; \
-                mean *= var; \
-                mean += past_mean; \
-                past_mean = -past_mean; \
-                past_mean += row; \
-                mean = -mean; \
-                mean += row; \
-                past_mean *= mean; \
-                past_mean -= past_var; \
-                past_mean *= var; \
-                past_mean += past_var; \
-                past_var = past_mean; \
-                past_mean = -mean; \
-                N++; \
-            }
-sfpi_inline void transpose (vFloat &f0,  vFloat &f1, vFloat &f2, vFloat &f3, vFloat &f4, vFloat &f5, vFloat &f6, vFloat &f7) {
-   asm volatile ("sfptransp" : "+Q0"(f1.get()), "+Q1"(f1.get()), "+Q2"(f2.get()), "+Q3"(f3.get()),
-                "+Q4"(f4.get()), "+Q5"(f5.get()), "+Q6"(f6.get()), "+Q7"(f7.get()));
-}
-
 //calculates one iteration of welfords
 void Welfords_Math(uint InputLREG, uint32_t N){
     //mean calculation start
@@ -82,39 +57,35 @@ void Welfords_Math(uint InputLREG, uint32_t N){
     //var_{N+1} = var_{N} + ...
     //...(1/N+1) * (((x_{N+1} - mean_{N}) * (x_{N+1} - mean_{N+1})) - var_{N})
 
-    /*mean_{N} = -1 * ((-1) *mean_{N})*/
+    /*mean_{N} = (Input_LREG - mean_{N})*/
     TTI_SFPMAD(p_sfpu::LREG11, p_sfpu::LREG4, InputLREG, p_sfpu::LREG4, 0);
-
     //Next cycle cannot read from LREG4 See tt-isa-documentation
 
-    /*mean_{N+1} = -1 * ((-1) *mean_{N+1})*/
-    TTI_SFPMULI(negative_one_BF16, p_sfpu::LREG6, 0);
-    //Next cycle cannot read from LREG6 See tt-isa-documentation
-
-    /*mean_{N}temp = 1 * (InputLREG + (-mean_{N}))*/
-    TTI_SFPADD(p_sfpu::LCONST_1/*LREG10 = <1>*/, InputLREG, p_sfpu::LREG4, p_sfpu::LREG4,0);
-    //Next cycle cannot read from LREG4 See tt-isa-documentation
-
-
-    /*inputLREG temp = 1 * (InputLREG + (-mean_{N}))*/
-    TTI_SFPADD(p_sfpu::LCONST_1/*LREG10 = <1>*/, InputLREG, p_sfpu::LREG6, InputLREG,0);
+    /*inputLREG temp = (InputLREG + (-mean_{N+1}))*/
+    TTI_SFPMAD(p_sfpu::LREG11/*LREG11 = <-1>*/,p_sfpu::LREG6, InputLREG, InputLREG,0);
     //Next cycle cannot read from InputLREG See tt-isa-documentation
 
-    //Calls NOP Macro since InputLREG is read next
+    //var_N = -var_N
+    TTI_SFPMULI(negative_one_BF16, p_sfpu::LREG5, 0);
+    //Next cycle cannot read from LREG5 See tt-isa-documentation
     TTI_SFPNOP;
 
     /*InputLREG temp= (mean_{N} = (InputLREG-mean_{N}) * InputLREG =(InputLREG - mean_{N+1})) + var_{N}*/
     TTI_SFPMAD(p_sfpu::LREG4, InputLREG, p_sfpu::LREG5, InputLREG, 0);
     //Next cycle cannot read from InputLREG See tt-isa-documentation
+
+    //var_N = -(-var_N)
+    TTI_SFPMULI(negative_one_BF16, p_sfpu::LREG5, 0);
     TTI_SFPNOP;
 
-    /*var_{N+1} = (InputLREG = (see above) * var_{N+1} = (1/N+1)) + var_{N} = ()*/
+    //updates the var
+    /*var_{N} = (InputLREG = (see above) * (1/N+1)) + var_{N} = ()*/
     TTI_SFPMAD(InputLREG, p_sfpu::LREG7, p_sfpu::LREG5, p_sfpu::LREG5, 0);
     //Next cycle cannot read from LREG5 See tt-isa-documentation
 
     //keep it simple please change
     /*mean_{N} = -1 * ((-1) *mean_{N+1})*/
-    TTI_SFPMUL(11, p_sfpu::LREG6, p_sfpu::LCONST_0, p_sfpu::LREG4, 0);
+    TTI_SFPMUL(p_sfpu::LCONST_1 /*LREG11 = <-1>*/, p_sfpu::LREG6, p_sfpu::LCONST_0, p_sfpu::LREG4, 0);
     //Next cycle cannot read from LREG4 See tt-isa-documentation
     TTI_SFPNOP;
 
@@ -122,7 +93,8 @@ void Welfords_Math(uint InputLREG, uint32_t N){
     //Now past_mean (LREG4) is population
     //Now past_var (LREG5) is population
 }
-void _welfords_(uint32_t N, uint32_t endN, uint32_t last_run) {
+
+void Welfords_Main(uint32_t &N, uint32_t &endN, uint32_t &last_run) {
 
     // dst0 = input
     // dst1 = past_mean
@@ -152,36 +124,65 @@ void _welfords_(uint32_t N, uint32_t endN, uint32_t last_run) {
             TTI_SFPTRANSP(0, 0, 0, 0);
             if(N == 0){
                 //Needed since LREGS can maintain state between calls/maybe kernels? So setting them to zero is needed
-                TT_SFPLOADI(p_sfpu::LREG4, 0, 0);
-                TT_SFPLOADI(p_sfpu::LREG5, 0, 0);
-                TT_SFPLOADI(p_sfpu::LREG6, 0, 0);
-                TT_SFPLOADI(p_sfpu::LREG7, 0, 0);
+                TTI_SFPLOADI(p_sfpu::LREG4, 0, 0);
+                TTI_SFPLOADI(p_sfpu::LREG5, 0, 0);
+                TTI_SFPLOADI(p_sfpu::LREG6, 0, 0);
+                TTI_SFPLOADI(p_sfpu::LREG7, 0, 0);
             }
             else{
             /*past_mean = dst1*/TTI_SFPLOAD(p_sfpu::LREG4, 0, ADDR_MOD_3, 64);
             /*past_var = dst2*/TTI_SFPLOAD(p_sfpu::LREG5, 0, ADDR_MOD_3, 128);
             //wiping LREG 6 and 7 since they may be filled with garbage data
-            TT_SFPLOADI(p_sfpu::LREG6, 0, 0);
-            TT_SFPLOADI(p_sfpu::LREG7, 0, 0);
+            TTI_SFPLOADI(p_sfpu::LREG6, 0, 0);
+            TTI_SFPLOADI(p_sfpu::LREG7, 0, 0);
             }
             //LOAD was success has been tested for L0 and L1
 
+            if(N == endN) return;
             Welfords_Math(p_sfpu::LREG0, N);
             N++;
+            if(N == endN) return;
             Welfords_Math(p_sfpu::LREG1, N);
             N++;
+            if(N == endN) return;
             Welfords_Math(p_sfpu::LREG2, N);
             N++;
+            if(N == endN) return;
             Welfords_Math(p_sfpu::LREG3, N);
             N++;
-
             //storing since SFPTRANSP will shuffle these LREGS
-            TT_SFPSTORE(p_sfpu::LREG4, 0, ADDR_MOD_3, 64);
-            TT_SFPSTORE(p_sfpu::LREG5, 0, ADDR_MOD_3, 128);
+            TTI_SFPSTORE(p_sfpu::LREG4, 0, ADDR_MOD_3, 64);
+            TTI_SFPSTORE(p_sfpu::LREG5, 0, ADDR_MOD_3, 128);
         }
     }
     return;
 }
+void _welfords_(uint32_t N, uint32_t endN, uint32_t last_run){
+    Welfords_Main(N, endN, last_run);
+    if(N == endN){
+        //This subroutine allows us to save the row of mean vals to the dstreg 1 and row of variance vals to dstreg 2
+        TTI_SFPADD(p_sfpu::LCONST_1/*LREG10 = <1>*/, p_sfpu::LCONST_0, p_sfpu::LREG4, p_sfpu::LREG0,0);
+        TTI_SFPLOADI(p_sfpu::LREG1, 0, 0);
+        TTI_SFPLOADI(p_sfpu::LREG2, 0, 0);
+        TTI_SFPLOADI(p_sfpu::LREG3, 0, 0);
 
+        TTI_SFPADD(p_sfpu::LCONST_1/*LREG10 = <1>*/, p_sfpu::LCONST_0, p_sfpu::LREG5, p_sfpu::LREG4,0);
+        TTI_SFPLOADI(p_sfpu::LREG5, 0, 0);
+        TTI_SFPLOADI(p_sfpu::LREG6, 0, 0);
+        TTI_SFPLOADI(p_sfpu::LREG7, 0, 0);
+
+        TTI_SFPTRANSP(0, 0, 0, 0);
+
+        TTI_SFPSTORE(p_sfpu::LREG0, 0, ADDR_MOD_3, 64);
+        TTI_SFPSTORE(p_sfpu::LREG1, 0, ADDR_MOD_3, 64 + 2);
+        TTI_SFPSTORE(p_sfpu::LREG2, 0, ADDR_MOD_3, 64 + 16);
+        TTI_SFPSTORE(p_sfpu::LREG3, 0, ADDR_MOD_3, 64 + 18);
+
+        TTI_SFPSTORE(p_sfpu::LREG4, 0, ADDR_MOD_3, 128);
+        TTI_SFPSTORE(p_sfpu::LREG5, 0, ADDR_MOD_3, 128 + 2);
+        TTI_SFPSTORE(p_sfpu::LREG6, 0, ADDR_MOD_3, 128 + 16);
+        TTI_SFPSTORE(p_sfpu::LREG7, 0, ADDR_MOD_3, 128 + 18);
+    }
+}
 }
 }
