fd8b61c42d5090e7cd26e48ed6868efac740a22b
Aswinmcw <azayasankaran@tenstorrent.com>
2025-08-12 11:23:45 +0000

    improve code

--- tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_threshold.h
+++ tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_threshold.h
@@ -14,33 +14,32 @@
 namespace ckernel::sfpu
 {
 
+template <typename T>
+constexpr bool is_supported_threshold_type_v = std::is_same_v<T, float> || std::is_same_v<T, uint32_t>;
+
 template <bool APPROXIMATION_MODE, int ITERATIONS, typename T>
 inline void _calculate_threshold_(T param0, T param1)
 {
+    static_assert(is_supported_threshold_type_v<T>, "Type T must be either float or uint32_t");
+
+    sfpi::vFloat t;
+    sfpi::vFloat v;
+    if constexpr (std::is_same_v<T, float>)
+    {
+        t = param0;
+        v = param1;
+    }
+    else if constexpr (std::is_same_v<T, uint32_t>)
+    {
+        t = Converter::as_float(param0);
+        v = Converter::as_float(param1);
+    }
 #pragma GCC unroll 8
     for (int d = 0; d < ITERATIONS; d++)
     {
-        sfpi::vFloat in = sfpi::dst_reg[0];
-
-        sfpi::vFloat t;
-        sfpi::vFloat v;
-
-        if constexpr (std::is_same_v<T, float>)
-        {
-            t = param0;
-            v = param1;
-        }
-        else if constexpr (std::is_same_v<T, uint32_t>)
-        {
-            t = Converter::as_float(param0);
-            v = Converter::as_float(param1);
-        }
-        else
-        {
-            static_assert(!sizeof(T*), "Unsupported type for _calculate_threshold_");
-        }
-
+        sfpi::vFloat in     = sfpi::dst_reg[0];
         sfpi::vFloat result = v;
+
         v_if (in > t)
         {
             result = in;
