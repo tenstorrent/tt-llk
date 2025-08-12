2b3b41ce1bf0ca530f8592a16d1f5c365fa5a8d3
skotarac <skotarac@tenstorrent.com>
2025-08-12 08:54:04 +0000

    found some bugs :)
    minor fix
    
    minor fix

--- tests/sources/unpack_matmul_test.cpp
+++ tests/sources/unpack_matmul_test.cpp
@@ -103,10 +103,9 @@ void run_kernel()
     _llk_pack_init_<false, false, DstTileFaceLayout::RowMajor, false>(formats.pack_dst);
     _llk_pack_dest_init_<DstSync::SyncHalf, is_fp32_dest_acc_en, DstTileFaceLayout::RowMajor, false>();
 #endif
-
+    _llk_packer_wait_for_math_done_();
     for (int i = 0; i < TILE_CNT; i++)
     {
-        _llk_packer_wait_for_math_done_();
         _llk_pack_<DstSync::SyncHalf, is_fp32_dest_acc_en, false>(i, L1_ADDRESS(buffer_Res[i]));
     }
     _llk_pack_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
