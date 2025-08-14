77f8c563e1fe6d3e11878ccfea83ed101bff9a31
Ana Mokan <amokan@tenstorrent.com>
2025-08-14 13:41:33 +0000

    Remove previously added workaround as they should not be needed there. Make separate int and float tests.

--- tt_llk_blackhole/llk_lib/llk_math_transpose_dest.h
+++ tt_llk_blackhole/llk_lib/llk_math_transpose_dest.h
@@ -43,14 +43,14 @@ inline void _llk_math_transpose_dest_(const std::uint32_t dst_index)
     // for SrcB[MatrixUnit.SrcBBank].AllowedClient == SrcClient::MatrixUnit.
     TTI_STALLWAIT(p_stall::STALL_MATH, p_stall::WAIT_SFPU | p_stall::SRCA_VLD | p_stall::SRCB_VLD);
 
-    if constexpr (is_fp32_dest_acc_en)
-    {
-        // Needs to be disabled for MOVD2B/B2D on BH (Issue ##449)
-        cfg_reg_rmw_tensix<ALU_ACC_CTRL_Fp32_enabled_RMW>(0);
-    }
-
     if constexpr (is_32bit)
     {
+        if constexpr (is_fp32_dest_acc_en)
+        {
+            // Needs to be disabled for MOVD2B/B2D on BH (Issue ##449)
+            cfg_reg_rmw_tensix<ALU_ACC_CTRL_Fp32_enabled_RMW>(0);
+        }
+
         if constexpr (transpose_of_faces)
         {
             // 4x 32b face transpositions followed by 8x middle-face row swaps.
@@ -60,6 +60,11 @@ inline void _llk_math_transpose_dest_(const std::uint32_t dst_index)
         {
             // 4x 32b face transpositions.
             ckernel_unpack_template::run(4, 0);
+        }
+
+        if constexpr (is_fp32_dest_acc_en)
+        {
+            cfg_reg_rmw_tensix<ALU_ACC_CTRL_Fp32_enabled_RMW>(1);
         }
     }
     else
