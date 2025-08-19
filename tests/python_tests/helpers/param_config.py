acce5137700e0ee402a08e18a09605bd7359fdfe
Ana Mokan <amokan@tenstorrent.com>
2025-08-19 11:16:43 +0000

    Add helper function description

--- tests/python_tests/helpers/param_config.py
+++ tests/python_tests/helpers/param_config.py
@@ -8,7 +8,7 @@ import pytest
 from helpers.chip_architecture import ChipArchitecture, get_chip_architecture
 from helpers.log_utils import add_to_format_log
 
-from .format_arg_mapping import DestAccumulation, DestSync, Tilize
+from .format_arg_mapping import DestAccumulation, DestSync, Tilize, Transpose
 from .format_config import (
     DataFormat,
     FormatConfig,
