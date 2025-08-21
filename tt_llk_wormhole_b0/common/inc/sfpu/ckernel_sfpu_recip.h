422f9bb90eb55f37d84f52243a6568af85314ba7
Jason Davies <jason@jasondavies.com>
2025-08-21 11:30:55 +0000

    Reciprocal: 2 cycles faster on WH for float32.

--- tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_recip.h
+++ tt_llk_wormhole_b0/common/inc/sfpu/ckernel_sfpu_recip.h
@@ -12,45 +12,60 @@ namespace ckernel
 namespace sfpu
 {
 
-// See: Cezary J. Walczyk, Leonid V. Moroz, Volodymyr Samotyy, and Jan L. Cieśliński.
-// Optimal Approximation of the 1/x Function using Chebyshev Polynomials and Magic Constants.
-// https://doi.org/10.1145/3708472
-
 // Computes the reciprocal of a floating point value x.
-// Returns 0 if abs(x) > 0x1.6a09e6p+126, or if x is NaN.
-template <bool APPROXIMATE = false>
-sfpi_inline sfpi::vFloat _sfpu_reciprocal_(const sfpi::vFloat x)
+// For max_iter = 2, this should take 20 cycles.
+// For max_iter = 1, this should take 18 cycles.
+template <int max_iter = 2>
+sfpi_inline sfpi::vFloat _sfpu_reciprocal_(const sfpi::vFloat in)
 {
-    sfpi::vFloat abs_x      = sfpi::abs(x);
-    sfpi::vFloat negative_x = -x;
-    sfpi::vInt y0_bits      = sfpi::vConstIntPrgm0 - sfpi::reinterpret<sfpi::vInt>(abs_x);
-    sfpi::vFloat y;
-    v_if (y0_bits >= 0)
-    {
-        y              = sfpi::setsgn(sfpi::reinterpret<sfpi::vFloat>(y0_bits), x);
-        sfpi::vFloat t = y * negative_x + sfpi::vConstFloatPrgm2;
-        y              = y * sfpi::vConstFloatPrgm1;
-        y              = y * t;
+    // Combines the sign and exponent of -1.0 with the mantissa of `in`.
+    // Scale the input value to the range [1.0, 2.0), and make it negative.
+    // If in ≠ ±0 and in ≠ ±inf, then x = in * 2**(127-in.Exp).
+    // If in = ±0 or in = ±inf, then x = ±1.
+    // Then negative_x = -x.
+    sfpi::vFloat negative_x = sfpi::setman(sfpi::vConstNeg1, sfpi::reinterpret<sfpi::vInt>(in));
 
-        if constexpr (!APPROXIMATE)
-        {
-            // 2nd iteration of Newton-Raphson
-            t = y * negative_x + sfpi::vConst1;
-            y = y * t + y;
-        }
-    }
-    v_else
-    {
-        // This occurs for a small portion of very large floats, infinity, and NaN.
-        y = sfpi::vConst0;
-    }
-    v_endif;
+    // Quadratic initial estimate: y = 140/33 - 64/11 * x + 256/99 x**2.
+    sfpi::vFloat y = sfpi::vConstFloatPrgm1 + sfpi::vConstFloatPrgm0 * negative_x;
+    // Hide SFPNOP.
+    sfpi::vFloat two = sfpi::s2vFloat16a(2.0f);
+    // Continue with quadratic estimate.
+    y = sfpi::vConstFloatPrgm2 + y * negative_x;
 
-    v_if (x == 0)
+    // Scale factor: we want 1/in = 1/x * scale.
+    // For x ≠ ±0 and x ≠ ±inf, in = x * 2**-(127-in.Exp), so 1/in = 1/x * 2**(127-in.Exp).
+    // Add float32 bias: scale.Exp = 127+127-in.Exp = 254-in.Exp.
+    // For efficiency and handling of x = ±0 and x = ±inf, we set scale.Exp = 255-in.Exp = ~in.Exp.
+    // This is efficiently computed with a single SFPNOT, followed by SFPSETMAN to clear the mantissa at the next opportunity.
+    // The sign doesn't matter as we set the output sign to match the input at the end.
+    // Not only is 255-in.Exp more efficient via SFPNOT, but it also ensures
+    // that in.Exp == 0 results in ±inf, and in.Exp == 255 results in ±0.
+    // See the scale factor adjustment via scale*0.5 below for further details.
+    sfpi::vInt scale_bits = ~sfpi::reinterpret<sfpi::vUInt>(in);
+
+    // First iteration of Newton-Raphson: t = 2.0 - xy.
+    sfpi::vFloat t = two + negative_x * y;
+    // Scale factor: set mantissa to zero.
+    sfpi::vFloat scale = sfpi::setman(sfpi::reinterpret<sfpi::vFloat>(scale_bits), 0);
+    // Continue Newton-Raphson: y = yt.
+    y = y * t;
+    // Scale factor adjustment: scale = scale*0.5.
+    // Note that scale = ±0 and scale = ±inf will be preserved.
+    // If scale.Exp == 0, then scale = ±inf, so scale*0.5 == ±inf.
+    // If scale.Exp == 255, then scale = ±0, so scale*0.5 == ±0.
+    // Otherwise, scale.Exp = scale.Exp-1 = 255-in.Exp-1 = 254-in.Exp.
+    scale *= 0.5f;
+
+    if constexpr (max_iter > 1)
     {
-        y = sfpi::s2vFloat16b(std::numeric_limits<float>::infinity());
+        // Second iteration of Newton-Raphson: t = 2.0 - xy, y = yt.
+        t = two + negative_x * y;
+        y = y * t;
     }
-    v_endif;
+
+    // Apply scaling factor, and set sign to match input.
+    y = y * scale;
+    y = sfpi::setsgn(y, in);
 
     return y;
 }
@@ -61,15 +76,23 @@ inline void _calculate_reciprocal_(const int iterations)
 #pragma GCC unroll 8
     for (int d = 0; d < iterations; d++)
     {
-        sfpi::vFloat out = _sfpu_reciprocal_<APPROXIMATION_MODE>(sfpi::dst_reg[0]);
+        sfpi::vFloat in = sfpi::dst_reg[0];
 
-        if constexpr (is_fp32_dest_acc_en || APPROXIMATION_MODE)
+        if constexpr (APPROXIMATION_MODE)
         {
-            sfpi::dst_reg[0] = out;
+            sfpi::dst_reg[0] = _sfpu_reciprocal_<0>(in);
         }
         else
         {
-            sfpi::dst_reg[0] = sfpi::reinterpret<sfpi::vFloat>(float_to_fp16b(out, 0));
+            if constexpr (is_fp32_dest_acc_en)
+            {
+                sfpi::dst_reg[0] = _sfpu_reciprocal_<2>(in);
+            }
+            else
+            {
+                sfpi::vFloat out = _sfpu_reciprocal_<1>(in);
+                sfpi::dst_reg[0] = sfpi::reinterpret<sfpi::vFloat>(float_to_fp16b(out, 0));
+            }
         }
 
         sfpi::dst_reg++;
@@ -79,9 +102,9 @@ inline void _calculate_reciprocal_(const int iterations)
 template <bool APPROXIMATION_MODE>
 inline void _init_reciprocal_()
 {
-    sfpi::vConstIntPrgm0   = 0x7eb504f3;
-    sfpi::vConstFloatPrgm1 = 1.94090888923f;
-    sfpi::vConstFloatPrgm2 = 1.43566017178f;
+    sfpi::vConstFloatPrgm0 = 32.0f / 99.0f;
+    sfpi::vConstFloatPrgm1 = 16.0f / 11.0f;
+    sfpi::vConstFloatPrgm2 = 70.0f / 33.0f;
 }
 
 } // namespace sfpu
