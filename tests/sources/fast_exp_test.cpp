42a02f66088b8b281d27d2e2a65b397340b69435
ldjurovicTT <ldjurovic@tenstorrent.com>
2025-08-25 12:47:57 +0000

    Starting over

---
+++ tests/sources/fast_exp_test.cpp
@@ -0,0 +1,140 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <algorithm>
+#include <cstdint>
+#include <cstdio>
+#include <type_traits>
+
+#include "ckernel.h"
+#include "llk_defs.h"
+
+// Globals
+uint32_t unp_cfg_context          = 0;
+uint32_t pack_sync_tile_dst_ptr   = 0;
+uint32_t math_sync_tile_dst_index = 0;
+
+#ifdef LLK_TRISC_UNPACK
+
+#include "llk_unpack_A.h"
+#include "llk_unpack_common.h"
+#include "params.h"
+
+void run_kernel()
+{
+    _llk_unpack_A_hw_configure_<is_fp32_dest_acc_en, StochRndType::None>(formats.unpack_src, formats.unpack_dst, FACE_R_DIM, 0, 4);
+    _llk_unpack_A_init_<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, unpack_to_dest>(
+        0, 0, FACE_R_DIM, 4, formats.unpack_src, formats.unpack_dst);
+
+    for (int i = 0; i < TILE_CNT; ++i)
+    {
+        _llk_unpack_A_<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, unpack_to_dest>(
+            L1_ADDRESS(buffer_A[i]), 0, formats.unpack_src, formats.unpack_dst);
+    }
+}
+
+#endif
+
+#ifdef LLK_TRISC_MATH
+
+#include "ckernel_sfpu.h"
+#include "llk_math_common.h"
+#include "llk_math_eltwise_unary_datacopy.h"
+#include "llk_math_eltwise_unary_sfpu.h"
+#include "llk_math_eltwise_unary_sfpu_params.h"
+#include "params.h"
+
+using namespace ckernel;
+using namespace ckernel::sfpu;
+
+const int iterations = 32;
+
+namespace ckernel
+{
+namespace sfpu
+{
+template <bool APPROXIMATION_MODE, bool FAST_APPROX, bool SCALE_EN = false, int ITERATIONS = 8, bool SKIP_POSITIVE_CHECK = false>
+void calculate_exponential(const uint exp_base_scale_factor = p_sfpu::kCONST_1_FP16B)
+{
+    _calculate_exponential_<APPROXIMATION_MODE, SCALE_EN, ITERATIONS, FAST_APPROX, SKIP_POSITIVE_CHECK>(exp_base_scale_factor);
+}
+
+template <bool APPROXIMATION_MODE, bool FAST_APPROX, uint32_t scale = 0x3F800000>
+void exp_init()
+{
+    _init_exponential_<APPROXIMATION_MODE, FAST_APPROX, scale>();
+}
+
+} // namespace sfpu
+} // namespace ckernel
+
+void run_kernel()
+{
+// copy srca to dest
+#ifdef ARCH_BLACKHOLE
+    _llk_math_eltwise_unary_datacopy_init_<DataCopyType::A2D, is_fp32_dest_acc_en, BroadcastType::NONE, false, false>(0, 0, 4, formats.math);
+#else
+    _llk_math_eltwise_unary_datacopy_init_<DataCopyType::A2D, is_fp32_dest_acc_en, BroadcastType::NONE, false>(0, 0, 4, formats.math);
+#endif
+    _llk_math_pack_sync_init_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+    _llk_math_hw_configure_<false, false>(formats.math, formats.math);
+
+    ckernel::sfpu::exp_init<true, true>();
+
+    for (int i = 0; i < TILE_CNT; ++i)
+    {
+        _llk_math_wait_for_dest_available_<DstSync::SyncHalf>();
+        _llk_math_eltwise_unary_datacopy_<DataCopyType::A2D, DstSync::SyncHalf, is_fp32_dest_acc_en, BroadcastType::NONE, unpack_to_dest>(
+            i, formats.math, formats.math);
+
+        // calculation of sfpu operation on dest
+        // _llk_math_eltwise_unary_sfpu_init_<SFPU_UNARY_OPERATION>();
+
+        // _llk_math_eltwise_unary_sfpu_start_<DstSync::SyncHalf>(i);
+        // // calling sfpu function from ckernel
+        // // this part is where parametrization of operation takes part
+        // call_sfpu_operation(SFPU_UNARY_OPERATION, formats.math);
+
+        _llk_math_eltwise_unary_sfpu_params_<true>(
+            ckernel::sfpu::calculate_exponential<true, true, false, 32, false>, i, VectorMode::RC, p_sfpu::kCONST_1_FP16B /* exp_base_scale_factor*/);
+
+        // _llk_math_eltwise_unary_sfpu_done_();
+    }
+
+    _llk_math_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+}
+
+#endif
+
+#ifdef LLK_TRISC_PACK
+
+#include "llk_pack.h"
+#include "llk_pack_common.h"
+#include "params.h"
+
+void run_kernel()
+{
+#ifdef ARCH_BLACKHOLE
+    _llk_pack_hw_configure_<is_fp32_dest_acc_en, false, false>(formats.pack_src, formats.pack_dst, 16 * 16 * 4);
+#else
+    _llk_pack_hw_configure_<is_fp32_dest_acc_en, false>(formats.pack_src, formats.pack_dst, 16 * 16 * 4);
+#endif
+
+    _llk_pack_init_<false, false, DstTileFaceLayout::RowMajor, false>(formats.pack_dst);
+
+#ifdef ARCH_BLACKHOLE
+    _llk_pack_dest_init_<DstSync::SyncHalf, is_fp32_dest_acc_en, DstTileFaceLayout::RowMajor>();
+#else
+    _llk_pack_dest_init_<DstSync::SyncHalf, false, DstTileFaceLayout::RowMajor, false>();
+#endif
+
+    _llk_packer_wait_for_math_done_();
+    for (int i = 0; i < TILE_CNT; ++i)
+    {
+        _llk_pack_<DstSync::SyncHalf, is_fp32_dest_acc_en, false>(i, L1_ADDRESS(buffer_Res[i]));
+    }
+    _llk_pack_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+}
+
+#endif
