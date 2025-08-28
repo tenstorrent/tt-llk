e323379b76a481c36bdd3c6c9681343034bf043e
Radomir Djogo <rdjogo@tenstorrent.com>
2025-08-28 20:06:59 +0000

    Address comments

--- tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_max_pool_indices.h
+++ tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_max_pool_indices.h
@@ -13,13 +13,27 @@ namespace ckernel
 namespace sfpu
 {
 
+/**
+ * @brief Calculates column-wise MaxPool of a tile, placing output values into the first row.
+ *        Also places the index of the max value into the first row of the indices tile.
+ *        Supports {FP32, FP16_B} for values, and {UINT16, INT32, UINT32} for indices, implied from the Dest mode used.
+ * @tparam APPROXIMATION_MODE Whether to use the approximation mode (unused).
+ * @tparam is_fp32_dest_acc_en Whether Dest is in 32bit mode (true) or 16bit mode (false).
+ * @tparam num_rows The number of rows in the tile, must be one of: {9}
+ * @tparam ITERATIONS The number of iterations to use for the MaxPool operation (unused).
+ * @param indices_tile_idx The index of the tile in the Dest register containing the indices of the data.
+ */
 template <bool APPROXIMATION_MODE, bool is_fp32_dest_acc_en, int num_rows, int ITERATIONS>
-inline void _calculate_max_pool_with_indices_(const uint idx_addr)
+inline void _calculate_max_pool_with_indices_(const uint indices_tile_idx)
 {
-    static_assert(num_rows == 9, "num_rows must be one of: {9}");
-    const uint idx_tile_offset        = idx_addr * 64;
-    constexpr uint8_t instr_mod_index = is_fp32_dest_acc_en ? InstrModLoadStore::INT32 : InstrModLoadStore::LO16;
+    static_assert(num_rows == 9, "num_rows must be one of: {9}"); // add others as support is added
+
+    // size of each tile in Dest is 64 rows
+    constexpr uint dst_tile_size = 64;
+    // each face is 16 rows
     constexpr uint face_offset        = 16;
+    const uint idx_tile_offset        = indices_tile_idx * dst_tile_size;
+    constexpr uint8_t instr_mod_index = is_fp32_dest_acc_en ? InstrModLoadStore::INT32 : InstrModLoadStore::LO16;
 
     // F0
     // data
@@ -84,7 +98,9 @@ inline void _calculate_max_pool_with_indices_(const uint idx_addr)
 
 inline void _init_max_pool_with_indices_()
 {
-    // Set bit [2] of the SFPU_CONTROL_REG to enable index tracking mode
+    // Set bit [2] of the SFPU_CONTROL_REG to enable Destination Index Tracking Mode:
+    // LREGs 4-7 will be treated as indices corresponding to the values in LREGs 0-3,
+    // and LREGs 4-7 will mirror the movement of the values in LREGs 0-3;
     _sfpu_load_config32_(0xF, 0x0, 0x4);
 
     // Program replay buffer
@@ -93,15 +109,16 @@ inline void _init_max_pool_with_indices_()
         7,
         []
         {
-            // transpose to put Dest rows into separate LREGS
+            // Values have been loaded such that 4 rows of Dest occupy the 4 lanes of each LREG
+            // To sort those 4 rows, we transpose the SFPU LREGs to put the 4 rows into separate LREGs
             TTI_SFPTRANSP(0, 0, 0, 0);
 
-            // put max into LREG0 (index into LREG4)
+            // Sort the 4 rows of Dest, placing the max value into LREG0 (index into LREG4)
             TTI_SFPSWAP(0, p_sfpu::LREG0, p_sfpu::LREG1, p_sfpswap::ALL_ROWS_MAX);
             TTI_SFPSWAP(0, p_sfpu::LREG2, p_sfpu::LREG3, p_sfpswap::ALL_ROWS_MAX);
             TTI_SFPSWAP(0, p_sfpu::LREG0, p_sfpu::LREG2, p_sfpswap::ALL_ROWS_MAX);
 
-            // transpose back
+            // Transpose the LREGs back
             TTI_SFPTRANSP(0, 0, 0, 0);
 
             TTI_SFPSWAP(0, p_sfpu::LREG0, p_sfpu::LREG1, p_sfpswap::ALL_ROWS_MAX);
