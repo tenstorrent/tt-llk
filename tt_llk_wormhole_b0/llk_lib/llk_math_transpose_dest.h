d1f5ec431f4317a86e63e6e674b3c92c2eae93e9
Ana Mokan <amokan@tenstorrent.com>
2025-09-02 06:46:00 +0000

    Add fix for large Int32 values

--- tt_llk_wormhole_b0/llk_lib/llk_math_transpose_dest.h
+++ tt_llk_wormhole_b0/llk_lib/llk_math_transpose_dest.h
@@ -46,6 +46,7 @@ inline void _llk_math_transpose_dest_(const std::uint32_t dst_index)
 
     if constexpr (is_32bit)
     {
+        cfg_reg_rmw_tensix<ALU_ACC_CTRL_Zero_Flag_disabled_src_RMW>(1);
         if constexpr (transpose_of_faces)
         {
             // 4x 32b face transpositions followed by 8x middle-face row swaps.
@@ -56,6 +57,7 @@ inline void _llk_math_transpose_dest_(const std::uint32_t dst_index)
             // 4x 32b face transpositions.
             ckernel_unpack_template::run(4, 0);
         }
+        cfg_reg_rmw_tensix<ALU_ACC_CTRL_Zero_Flag_disabled_src_RMW>(0);
     }
     else
     {
