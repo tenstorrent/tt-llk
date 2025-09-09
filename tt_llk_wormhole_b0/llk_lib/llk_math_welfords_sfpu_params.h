9d09676ae4e66146a295fc02535655473c7e3d84
Vasisht Suresh <vsuresh@tenstorrent.com>
2025-09-09 05:55:08 -0700

    Added proper includes

--- tt_llk_wormhole_b0/llk_lib/llk_math_welfords_sfpu_params.h
+++ tt_llk_wormhole_b0/llk_lib/llk_math_welfords_sfpu_params.h
@@ -5,6 +5,8 @@
 #pragma once
 #include "llk_math_eltwise_ternary_sfpu.h"
 #include "llk_sfpu_types.h"
+#include <algorithm>
+#include <utility> 
 
 template <typename Callable, typename... ARGS>
 inline void _llk_math_welfords_sfpu_params_(Callable&& sfpu_func, uint dst_index0, uint dst_index1, uint dst_index2, ARGS&&... args)
