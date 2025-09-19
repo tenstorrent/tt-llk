57c44f6bdad5b02268d5596224fb83946f14cf9e
Filip Vranic <fvranic@tenstorrent.com>
2025-09-19 11:39:22 +0200

    fix: remove unused union and functions

--- tests/firmware/riscv/common/risc_attribs.h
+++ tests/firmware/riscv/common/risc_attribs.h
@@ -2,52 +2,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-#ifndef _RISC_ATTRIBS_H_
-#define _RISC_ATTRIBS_H_
-
-#include <stdint.h>
-
-struct tt_uint64_t
-{
-    union
-    {
-        uint64_t v;
-
-        struct
-        {
-            uint32_t hi;
-            uint32_t lo;
-        } parts;
-    };
-};
+#pragma once
 
 #define tt_l1_ptr  __attribute__((rvtt_l1_ptr))
 #define tt_reg_ptr __attribute__((rvtt_reg_ptr))
-
-inline __attribute__((always_inline)) uint64_t tt_l1_load(tt_uint64_t tt_l1_ptr *p)
-{
-    tt_uint64_t v;
-
-    v.parts.hi = p->parts.hi;
-    v.parts.lo = p->parts.lo;
-    return v.v;
-}
-
-inline __attribute__((always_inline)) uint64_t tt_l1_load(volatile tt_uint64_t tt_l1_ptr *p)
-{
-    tt_uint64_t v;
-
-    v.parts.hi = p->parts.hi;
-    v.parts.lo = p->parts.lo;
-    return v.v;
-}
-
-// In certain cases enabling watcher can cause the code size to be too large. Give an option to
-// disable inlining if we need to.
-#if defined(WATCHER_NOINLINE)
-#define FORCE_INLINE
-#else
-#define FORCE_INLINE inline __attribute__((always_inline))
-#endif
-
-#endif // _RISC_ATTRIBS_H_
