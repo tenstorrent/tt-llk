9ac235a197300934b3812b2c6a3bc81a1ad61bcb
Lazar Premovic <lpremovic@tenstorrent.com>
2025-09-19 10:56:02 +0000

    Remove risc_attribs.h

--- tests/firmware/riscv/common/risc_attribs.h
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: © 2023 Tenstorrent AI ULC
-//
-// SPDX-License-Identifier: Apache-2.0
-
-#pragma once
-
-#define tt_l1_ptr  __attribute__((rvtt_l1_ptr))
-#define tt_reg_ptr __attribute__((rvtt_reg_ptr))
