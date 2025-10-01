7cf38813d009b1716626aec03c667c989ed4c380
ldjurovicTT <ldjurovic@tenstorrent.com>
2025-10-01 14:59:44 +0000

    Fixing perf test

--- tests/python_tests/perf_col_tile_sdpa.py
+++ tests/python_tests/perf_col_tile_sdpa.py
@@ -26,29 +26,46 @@ from helpers.perf import (
 @parametrize(
     test_name="col_tile_sdpa_perf",
     formats=input_output_formats([DataFormat.Float16_b]),
-    mathop=[MathOperation.Elwsub],
+    mathop=[MathOperation.Elwsub, MathOperation.Elwadd, MathOperation.Elwmul],
+    dest_acc=[DestAccumulation.No],
+    reuse_a_times=[2, 4, 8],
     math_fidelity=[
         MathFidelity.LoFi,
     ],
-    tile_count=8,
-    dest_acc=[DestAccumulation.No],
+    input_dimensions=[
+        [128, 32],
+        [32, 128],
+        [64, 128],
+    ],
 )
 def test_perf_col_tile_sdpa(
-    perf_report, test_name, formats, mathop, math_fidelity, dest_acc, tile_count
+    perf_report,
+    test_name,
+    formats,
+    mathop,
+    dest_acc,
+    math_fidelity,
+    input_dimensions,
+    reuse_a_times,
 ):
 
     # MathFidelity is only used for Elwmul
     if mathop != MathOperation.Elwmul and math_fidelity != MathFidelity.LoFi:
         pytest.skip("Fidelity does not affect Elwadd and Elwsub operations")
 
+    tile_cnt = input_dimensions[0] * input_dimensions[1] // 1024
+
     test_config = {
-        "testname": test_name,
-        "mathop": mathop,
         "formats": formats,
-        "math_fidelity": math_fidelity,
-        "tile_cnt": tile_count,
+        "testname": test_name,
         "dest_acc": dest_acc,
+        "input_A_dimensions": input_dimensions,
+        "input_B_dimensions": input_dimensions,
+        "mathop": mathop,
+        "math_fidelity": math_fidelity,
+        "tile_cnt": tile_cnt,
+        "reuse_a_times": reuse_a_times,
     }
 
-    results = perf_benchmark(test_config, [PerfRunType.L1_TO_L1], 100)
+    results = perf_benchmark(test_config, [PerfRunType.L1_TO_L1], 20)
     update_report(perf_report, test_config, results)
