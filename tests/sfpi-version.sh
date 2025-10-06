105efa4f4632b8e4b0d06eedd64f1cecc726a0bd
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-10-06 16:54:49 +0000

    sfpi build 364

--- tests/sfpi-version.sh
+++ tests/sfpi-version.sh
@@ -1,13 +1,51 @@
-# set SFPI release version information
+#! /bin/bash
 
+# Set SFPI release version information.
+# This is the source of truth as to how we determine arch and distro names.
+# Canonical location is in tenstorrent/tt-sfpi project's script directory.
+
+# source $FILE -- generate sfpi variables for use by shell
+# $FILE CMAKE -- emit cmake script to set variables
+# $FILE *.md5 -- emit variable initializations from md5 file(s)
+
+case "${1-}" in
+    *.md5)
+	# convert md5 files into release variables
+	sed 's/^\([0-9a-f]*\) \*sfpi_\([-.0-9a-zA-Z]*\)_\([a-z0-9_A-Z]*\)\.\([a-z]*\)$/sfpi_version=\2'"\n"'sfpi_\3_\4_md5=\1/' "$@" \
+	    | sort -u
+	exit 0
+	;;
+esac
+
+# define host system
+sfpi_dist=unknown
+if [[ -r /etc/os-release ]] ; then
+    # some distros are sufficiently similar for common handling
+    sfpi_dist=$(eval $(grep '^ID=' /etc/os-release) ; \
+		case "$ID" in \
+		    debian|ubuntu|redhat|centos|rhel|almalinux) ID=linux ;; \
+		esac ; \
+		echo "$ID")
+fi
+sfpi_arch=$(uname -m)
+
+# download root location
 sfpi_url=https://github.com/tenstorrent/sfpi/releases/download
 
-# convert md5 file into these variables
-# sed 's/^\([0-9a-f]*\) \*sfpi_\([-.0-9a-zA-Z]*\)_\([a-z0-9_A-Z]*\)\.\([a-z]*\)$/sfpi_version=\2'"\n"'sfpi_\3_\4_md5=\1/' *-build/sfpi_*.md5 | sort -u
-sfpi_aarch64_deb_md5=bbe267282dc8c36554bc91354e47fc7d
-sfpi_aarch64_rpm_md5=f146cc686ffd08f5d5bf55055f2010f2
-sfpi_aarch64_txz_md5=d03da69379ea08386ce88dfb2bcdb88c
-sfpi_version=7.3.0
-sfpi_x86_64_deb_md5=100e9834fe6877a186440b241f69da65
-sfpi_x86_64_rpm_md5=38def21cfa7cfebef8e9def61019f7cc
-sfpi_x86_64_txz_md5=17970c8034b039f269d9a9db8e841e45
+# define release
+sfpi_version=7.3.0-ext-29186
+sfpi_x86_64_linux_deb_md5=4d9fb7ae2cb0ac4d374d8146009cb9fe
+sfpi_x86_64_linux_rpm_md5=0c490e2fca4a26543f41e49a4741fb9a
+sfpi_x86_64_linux_txz_md5=bb02d56a92012767bc33ceeb10246c07
+
+case "${1-}" in
+    CMAKE)
+	# emit as cmake script
+	for var in $(set -o posix ; set | grep '^sfpi_')
+	do
+	    # relies on no inserted quoting for meta-characters
+	    name=${var%%=*}
+	    echo "set(SFPI${name#sfpi} \"${var#*=}\")"
+	done
+	;;
+esac
