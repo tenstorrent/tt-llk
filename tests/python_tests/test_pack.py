e0c0619d876e021ed4a7212bdf4b0f09bdbcc1f0
Ana Mokan <amokan@tenstorrent.com>
2025-10-08 14:40:26 +0000

    llk_pack cpp and py test

---
+++ tests/python_tests/test_pack.py
@@ -0,0 +1,127 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+
+from helpers.device import (
+    collect_results,
+    write_stimuli_to_l1,
+)
+from helpers.format_arg_mapping import DestAccumulation, format_dict
+from helpers.format_config import DataFormat
+from helpers.golden_generators import DataCopyGolden, get_golden_generator
+from helpers.param_config import (
+    input_output_formats,
+    parametrize,
+)
+from helpers.stimuli_generator import generate_stimuli
+from helpers.test_config import run_test
+from helpers.utils import passed_test
+
+
+def generate_pack_combinations():
+    # r_dim_list = [1, 2, 4, 8, 16, 32]
+    # c_dim_list = [8, 16, 32, 64]
+    tile_dim_list = [
+        (1, 32)
+    ]  # , (2, 32), (4, 32), (8, 32),(16, 32), (32, 32), (32, 16), (32, 8), (16, 16), (16, 8), (8, 8)]
+    # num_faces_list = [1, 2, 4]
+    # partial_face_list = [False, True]
+
+    combinations = []
+
+    for tile_dim in tile_dim_list:
+        if tile_dim[0] == 32 and tile_dim[1] == 32:
+            num_faces = 4
+            partial_face = False
+        elif tile_dim[0] <= 16 and tile_dim[1] <= 16:
+            num_faces = 1
+            partial_face = False if tile_dim[0] == tile_dim[1] else True
+        else:
+            num_faces = 2
+            partial_face = (
+                False if tile_dim[0] % 16 == 0 and tile_dim[1] % 16 == 0 else True
+            )
+
+        combinations.append((tile_dim, num_faces, partial_face))
+
+    return combinations
+
+
+@parametrize(
+    test_name="pack_test",
+    formats=input_output_formats(
+        [
+            # DataFormat.Float16_b,
+            DataFormat.Float16,
+            # DataFormat.Float32,
+            # DataFormat.Bfp8_b,
+        ]
+    ),
+    dest_acc=[DestAccumulation.No],  # , DestAccumulation.Yes],
+    tile_dim_combinations=generate_pack_combinations(),
+)
+def test_pack(test_name, formats, dest_acc, tile_dim_combinations):
+
+    input_dimensions = [32, 32]
+    tile_dim = tile_dim_combinations[0]
+    in0_tile_r_dim = tile_dim[0]
+    in0_tile_c_dim = tile_dim[1]
+    num_faces = tile_dim_combinations[1]
+    partial_face = tile_dim_combinations[2]
+
+    src_A, src_B, tile_cnt = generate_stimuli(
+        formats.input_format,
+        formats.input_format,
+        input_dimensions=input_dimensions,
+    )
+    tile_cnt = 1
+
+    generate_golden = get_golden_generator(DataCopyGolden)
+    golden_tensor = generate_golden(
+        src_A, formats.output_format, num_faces, input_dimensions
+    )
+
+    unpack_to_dest = (
+        formats.input_format.is_32_bit() and dest_acc == DestAccumulation.Yes
+    )
+
+    print(golden_tensor)
+
+    test_config = {
+        "formats": formats,
+        "testname": test_name,
+        "dest_acc": dest_acc,
+        "input_A_dimensions": input_dimensions,
+        "input_B_dimensions": input_dimensions,
+        "unpack_to_dest": unpack_to_dest,
+        "tile_cnt": tile_cnt,
+        "num_faces": num_faces,
+        "partial_face_A": partial_face,
+        "in0_tile_r_dim": in0_tile_r_dim,
+        "in0_tile_c_dim": in0_tile_c_dim,
+    }
+
+    res_address = write_stimuli_to_l1(
+        test_config,
+        src_A,
+        src_B,
+        formats.input_format,
+        formats.input_format,
+        tile_count_A=tile_cnt,
+        tile_count_B=tile_cnt,
+        num_faces=num_faces,
+    )
+
+    run_test(test_config)
+
+    res_from_L1 = collect_results(
+        formats, tile_count=tile_cnt, address=res_address, num_faces=num_faces
+    )
+
+    assert len(res_from_L1) == len(golden_tensor)
+
+    torch_format = format_dict[formats.output_format]
+    res_tensor = torch.tensor(res_from_L1, dtype=torch_format)
+
+    assert passed_test(golden_tensor, res_tensor, formats.output_format)
---
+++ tests/sources/pack_test.cpp
@@ -0,0 +1,124 @@
+
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#include <algorithm>
+#include <cstdint>
+#include <cstdio>
+
+#include "ckernel.h"
+#include "llk_defs.h"
+
+// Globals
+uint32_t unp_cfg_context          = 0;
+uint32_t pack_sync_tile_dst_ptr   = 0;
+uint32_t math_sync_tile_dst_index = 0;
+
+#ifdef LLK_TRISC_UNPACK
+
+#include "llk_unpack_A.h"
+#include "llk_unpack_common.h"
+// #include "llk_unpack_tilize.h"
+#include "params.h"
+
+void run_kernel()
+{
+    // if constexpr (!tilize_en)
+    //{
+    _llk_unpack_A_init_<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, unpack_to_dest>(
+        0, 0, FACE_R_DIM, 4, formats.unpack_src, formats.unpack_dst);
+    _llk_unpack_A_hw_configure_<is_fp32_dest_acc_en, StochRndType::None>(formats.unpack_src, formats.unpack_dst, FACE_R_DIM, 0, 4);
+
+    for (int i = 0; i < TILE_CNT; ++i)
+    {
+        _llk_unpack_A_<BroadcastType::NONE, false, EltwiseBinaryReuseDestType::NONE, unpack_to_dest>(
+            L1_ADDRESS(buffer_A[i]), 0, formats.unpack_src, formats.unpack_dst);
+    }
+    //}
+    /*else
+    {
+        _llk_unpack_tilize_hw_configure_<is_fp32_dest_acc_en, StochRndType::None>(formats.unpack_src, formats.unpack_dst, FACE_R_DIM, 0, 4);
+        _llk_unpack_tilize_init_(formats.unpack_src, formats.unpack_dst, BLOCK_CT_DIM, FACE_R_DIM, false);
+
+        uint32_t read_offset = 0;
+
+        for (uint32_t i = 0; i < BLOCK_RT_DIM; i++)
+        {
+            for (uint32_t j = 0; j < BLOCK_CT_DIM; j++)
+            {
+                _llk_unpack_tilize_(L1_ADDRESS(buffer_A[read_offset]), j, formats.unpack_src, BLOCK_CT_DIM, FACE_R_DIM, 4, false);
+            }
+            read_offset += BLOCK_RT_DIM;
+        }
+    }*/
+}
+
+#endif
+
+#ifdef LLK_TRISC_MATH
+
+#ifdef FORMAT_INT32
+const bool is_int_fpu_en = true;
+#else
+const bool is_int_fpu_en = false;
+#endif
+
+#include "llk_math_common.h"
+#include "llk_math_eltwise_unary_datacopy.h"
+#include "params.h"
+
+using namespace ckernel;
+
+void run_kernel()
+{
+// copy srca to dest
+#ifdef ARCH_BLACKHOLE
+    _llk_math_eltwise_unary_datacopy_init_<DataCopyType::A2D, is_fp32_dest_acc_en, BroadcastType::NONE, false, is_int_fpu_en>(0, 0, 4, formats.math);
+#else
+    _llk_math_eltwise_unary_datacopy_init_<DataCopyType::A2D, is_fp32_dest_acc_en, BroadcastType::NONE, is_int_fpu_en>(0, 0, 4, formats.math);
+#endif
+    _llk_math_pack_sync_init_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+    _llk_math_hw_configure_<false, false>(formats.math, formats.math);
+    _llk_math_wait_for_dest_available_<DstSync::SyncHalf>();
+    for (int i = 0; i < TILE_CNT; ++i)
+    {
+#ifdef ARCH_BLACKHOLE
+        _llk_math_eltwise_unary_datacopy_<DataCopyType::A2D, DstSync::SyncHalf, is_fp32_dest_acc_en, BroadcastType::NONE, unpack_to_dest>(
+            DST_INDEX + i, formats.math, formats.math, 4);
+#else
+        _llk_math_eltwise_unary_datacopy_<DataCopyType::A2D, DstSync::SyncHalf, is_fp32_dest_acc_en, BroadcastType::NONE, unpack_to_dest>(
+            DST_INDEX + i, formats.math, formats.math);
+#endif
+    }
+    _llk_math_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+}
+
+#endif
+
+#ifdef LLK_TRISC_PACK
+
+#include "llk_pack.h"
+#include "llk_pack_common.h"
+#include "params.h"
+
+void run_kernel()
+{
+#ifdef ARCH_BLACKHOLE
+    _llk_pack_hw_configure_<is_fp32_dest_acc_en, false, false>(formats.pack_src, formats.pack_dst, 16 * 16 * 4, FACE_R_DIM, TILE_C_DIM, 4);
+    _llk_pack_init_<false, false, DstTileFaceLayout::RowMajor, false, false>(formats.pack_dst, FACE_R_DIM, TILE_C_DIM, 4);
+    _llk_pack_dest_init_<DstSync::SyncHalf, is_fp32_dest_acc_en, DstTileFaceLayout::RowMajor>();
+#else
+    _llk_pack_hw_configure_<is_fp32_dest_acc_en, false>(formats.pack_src, formats.pack_dst, TILE_SIZE_PACK, FACE_R_DIM, num_faces, PARTIAL_FACE_PACK);
+    _llk_pack_init_<false, false, DstTileFaceLayout::RowMajor, false>(formats.pack_dst, FACE_R_DIM, num_faces, PARTIAL_FACE_PACK);
+    _llk_pack_dest_init_<DstSync::SyncHalf, is_fp32_dest_acc_en, DstTileFaceLayout::RowMajor, false>();
+#endif
+
+    _llk_packer_wait_for_math_done_();
+    for (int i = 0; i < TILE_CNT; ++i)
+    {
+        _llk_pack_<DstSync::SyncHalf, is_fp32_dest_acc_en, false>(DST_INDEX + i, L1_ADDRESS(buffer_Res[i]));
+    }
+    _llk_pack_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+}
+#endif
