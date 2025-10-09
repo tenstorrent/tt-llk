e550ea7ab61a3154af819df20b1bac741a459e1d
Aswinmcw <azayasankaran@tenstorrent.com>
2025-10-09 13:06:16 +0000

    feat: add POLYVAL7 function for higher degree polynomial evaluation
    ### What's changed
    Introduced a new constexpr function POLYVAL7 that evaluates a 7th degree polynomial using the provided coefficients and input value.
    
    ### Type of change
    - [x] New feature (non-breaking change which adds functionality)

--- tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_polyval.h
+++ tt_llk_blackhole/common/inc/sfpu/ckernel_sfpu_polyval.h
@@ -12,4 +12,11 @@ constexpr T POLYVAL5(T coef4, T coef3, T coef2, T coef1, T coef0, T val)
 {
     return (((((coef4 * val) + coef3) * val + coef2) * val + coef1) * val + coef0);
 }
+
+template <typename T>
+constexpr T POLYVAL7(T coef6, T coef5, T coef4, T coef3, T coef2, T coef1, T coef0, T val)
+{
+    return (val * (val * (val * (val * (val * (val * (coef6 * val + coef5) + coef4) + coef3) + coef2) + coef1) + coef0));
+}
+
 } // namespace ckernel::sfpu
