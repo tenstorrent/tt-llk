9111432dae11f9cc3f56a858c3a391192f7ac215
Ana Mokan <amokan@tenstorrent.com>
2025-10-28 16:06:26 +0000

    Separate Quasar tests

--- tests/python_tests/test_pack_untilize_quasar.py
+++ tests/python_tests/test_pack_untilize_quasar.py
@@ -3,37 +3,54 @@
 
 import pytest
 import torch
+from helpers.chip_architecture import ChipArchitecture, get_chip_architecture
 from helpers.device import collect_results, write_stimuli_to_l1
 from helpers.format_config import DataFormat
 from helpers.golden_generators import UntilizeGolden, get_golden_generator
-from helpers.llk_params import format_dict
+from helpers.llk_params import DestAccumulation, format_dict
 from helpers.param_config import input_output_formats, parametrize
 from helpers.stimuli_generator import generate_stimuli
 from helpers.test_config import run_test
 from helpers.utils import passed_test
 
 
+def generate_dest_acc_and_input_dimensions():
+    """Generate all combinations of dest_acc and input dimensions."""
+    num_tile_rows = 32
+    num_tile_cols = 32
+    dest_acc_list = [DestAccumulation.No, DestAccumulation.Yes]
+    combinations = []
+
+    for dest_acc in dest_acc_list:
+        max_tiles_in_dest = 8 if dest_acc == DestAccumulation.No else 4
+        for rows in range(1, max_tiles_in_dest + 1):
+            for cols in range(1, (max_tiles_in_dest // rows) + 1):
+                dimensions = [rows * num_tile_rows, cols * num_tile_cols]
+                combinations.append((dest_acc, dimensions))
+
+    return combinations
+
+
 @parametrize(
-    test_name="pack_untilize_test",
+    test_name="pack_untilize_quasar_test",
     formats=input_output_formats(
         [
             DataFormat.Float16_b,
-            DataFormat.Float16,
-            DataFormat.Float32,
-            DataFormat.Int32,
-            DataFormat.Bfp8_b,
-        ]  # Pack Untilize doesn't work for block float formats (Bfp8_b); we only include as input format in our test
+            # DataFormat.Float16,
+            # DataFormat.Float32,
+        ],
+        same=get_chip_architecture() == ChipArchitecture.QUASAR,
     ),
+    dest_acc_and_dimensions=generate_dest_acc_and_input_dimensions(),
 )
-def test_pack_untilize(test_name, formats):
-    if formats.output_format == DataFormat.Bfp8_b:
-        pytest.skip("Pack Untilize does not support Bfp8_b format")
-    if (formats.input_format == DataFormat.Int32) ^ (
-        formats.output_format == DataFormat.Int32
-    ):
-        pytest.skip("Pack Untilize does not support mixing Int32 with other formats")
+def test_pack_untilize_quasar(test_name, formats, dest_acc_and_dimensions):
 
-    input_dimensions = [32, 128]
+    dest_acc, input_dimensions = dest_acc_and_dimensions
+
+    if formats.input_format == DataFormat.Float32 and dest_acc == DestAccumulation.No:
+        pytest.skip("Does not work")
+    if formats.input_format == DataFormat.Float16 and dest_acc == DestAccumulation.Yes:
+        pytest.skip("Does not work")
 
     src_A, src_B, tile_cnt = generate_stimuli(
         formats.input_format, formats.input_format, input_dimensions=input_dimensions
@@ -49,6 +66,7 @@ def test_pack_untilize(test_name, formats):
         "input_A_dimensions": input_dimensions,
         "input_B_dimensions": input_dimensions,
         "unpack_to_dest": formats.input_format.is_32_bit(),
+        "dest_acc": dest_acc,
     }
 
     res_address = write_stimuli_to_l1(
