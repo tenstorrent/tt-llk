9fb9b1cfa52e8d79d829524127a7dd99a647ee53
Filip Vranic <fvranic@tenstorrent.com>
2025-11-04 09:01:47 +0000

    fix: make code more friendly

--- tests/helpers/include/boot.h
+++ tests/helpers/include/boot.h
@@ -4,14 +4,18 @@
 
 #pragma once
 
+#include "cfg_defines.h"
 #include "ckernel.h"
 
 inline void device_setup()
 {
 #if defined(ARCH_WORMHOLE)
-    volatile std::uint32_t* const trisc0_start_address            = reinterpret_cast<volatile std::uint32_t*>(0x16DFF0);
-    volatile std::uint32_t* const trisc1_start_address            = reinterpret_cast<volatile std::uint32_t*>(0x16DFF4);
-    volatile std::uint32_t* const trisc2_start_address            = reinterpret_cast<volatile std::uint32_t*>(0x16DFF8);
+    constexpr std::uint32_t TRISC0_START_ADDRESS_PTR              = 0x16DFF0;
+    constexpr std::uint32_t TRISC1_START_ADDRESS_PTR              = 0x16DFF4;
+    constexpr std::uint32_t TRISC2_START_ADDRESS_PTR              = 0x16DFF8;
+    volatile std::uint32_t* const trisc0_start_address            = reinterpret_cast<volatile std::uint32_t*>(TRISC0_START_ADDRESS_PTR);
+    volatile std::uint32_t* const trisc1_start_address            = reinterpret_cast<volatile std::uint32_t*>(TRISC1_START_ADDRESS_PTR);
+    volatile std::uint32_t* const trisc2_start_address            = reinterpret_cast<volatile std::uint32_t*>(TRISC2_START_ADDRESS_PTR);
     volatile uint tt_reg_ptr* cfg_regs                            = reinterpret_cast<volatile uint tt_reg_ptr*>(TENSIX_CFG_BASE);
     cfg_regs[TRISC_RESET_PC_SEC0_PC_ADDR32]                       = *trisc0_start_address;
     cfg_regs[TRISC_RESET_PC_SEC1_PC_ADDR32]                       = *trisc1_start_address;
