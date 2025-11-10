db318c5f3043f764d39ca228eed5d087b50a4ffd
Ana Mokan <amokan@tenstorrent.com>
2025-11-10 14:57:25 +0000

    Move Quasar tilize untilize tests to quasar folder

--- tests/python_tests/quasar/test_unpack_tilize_quasar.py
+++ tests/python_tests/quasar/test_unpack_tilize_quasar.py
@@ -6,29 +6,43 @@ from typing import List
 import pytest
 import torch
 from helpers.chip_architecture import ChipArchitecture, get_chip_architecture
-from helpers.device import collect_results, write_stimuli_to_l1
+from helpers.device import (
+    collect_results,
+    write_stimuli_to_l1,
+)
 from helpers.format_config import DataFormat, FormatConfig
-from helpers.golden_generators import UntilizeGolden, get_golden_generator
-from helpers.llk_params import DestAccumulation, ImpliedMathFormat, format_dict
+from helpers.golden_generators import (
+    TilizeGolden,
+    get_golden_generator,
+)
+from helpers.llk_params import (
+    DestAccumulation,
+    ImpliedMathFormat,
+    UnpackerEngine,
+    format_dict,
+)
 from helpers.param_config import (
     generate_unary_input_dimensions,
     input_output_formats,
     parametrize,
 )
 from helpers.stimuli_generator import generate_stimuli
-from helpers.test_config import run_test
+from helpers.test_config import BootMode, run_test
 from helpers.utils import passed_test
 
 
-def generate_pack_untilize_combinations(
+def generate_unpack_tilize_combinations(
     formats_list: List[FormatConfig],
 ):
     """
-    Generate pack_untilize combinations.
+    Generate unpack_tilize combinations.
+
+    Rules:
+    1. Tilize 32b data into dest is not yet supported
 
     Args: List of input-output format pairs
 
-    Returns: List of (format, dest_acc, input_dimensions) tuples
+    Returns: List of (format, dest_acc, unpacker_sel, input_dimensions) tuples
     """
     dimensions_cache = {
         DestAccumulation.No: tuple(
@@ -46,39 +60,42 @@ def generate_pack_untilize_combinations(
         if in_fmt != fmt.output_format:
             continue
 
-        dest_acc_modes = (
-            (DestAccumulation.Yes,)
-            if in_fmt.is_32_bit()
-            else (DestAccumulation.No, DestAccumulation.Yes)
-        )
+        if in_fmt.is_32_bit():
+            continue  # Tilize 32b data into dest not yet supported
+
+        dest_acc_modes = (DestAccumulation.No, DestAccumulation.Yes)
+        unpacker_engines = (UnpackerEngine.UnpA, UnpackerEngine.UnpB)
 
         for dest_acc in dest_acc_modes:
-            for dimensions in dimensions_cache[dest_acc]:
-                combinations.append((fmt, dest_acc, dimensions))
+            for unpacker_sel in unpacker_engines:
+                for dimensions in dimensions_cache[dest_acc]:
+                    combinations.append((fmt, dest_acc, unpacker_sel, dimensions))
 
     return combinations
 
 
-PACK_UNTILIZE_FORMATS = input_output_formats(
+UNPACK_TILIZE_FORMATS = input_output_formats(
     [
         DataFormat.Float16_b,
         DataFormat.Float16,
     ]
 )
-ALL_PACK_UNTILIZE_COMBINATIONS = generate_pack_untilize_combinations(
-    PACK_UNTILIZE_FORMATS
+ALL_UNPACK_TILIZE_COMBINATIONS = generate_unpack_tilize_combinations(
+    UNPACK_TILIZE_FORMATS
 )
 
 
 @parametrize(
-    test_name="quasar_pack_untilize_test",
-    formats_dest_acc_dimensions=ALL_PACK_UNTILIZE_COMBINATIONS,
+    test_name="unpack_tilize_quasar_test",
+    formats_dest_acc_unpack_sel_dimensions=ALL_UNPACK_TILIZE_COMBINATIONS,
 )
-def test_pack_untilize_quasar(test_name, formats_dest_acc_dimensions):
-
-    formats = formats_dest_acc_dimensions[0]
-    dest_acc = formats_dest_acc_dimensions[1]
-    input_dimensions = formats_dest_acc_dimensions[2]
+def test_unpack_tilize_quasar(
+    test_name, formats_dest_acc_unpack_sel_dimensions, boot_mode=BootMode.DEFAULT
+):
+    formats = formats_dest_acc_unpack_sel_dimensions[0]
+    dest_acc = formats_dest_acc_unpack_sel_dimensions[1]
+    unpacker_sel = formats_dest_acc_unpack_sel_dimensions[2]
+    input_dimensions = formats_dest_acc_unpack_sel_dimensions[3]
 
     if get_chip_architecture() != ChipArchitecture.QUASAR:
         pytest.skip("Test is supported only on Quasar.")
@@ -87,11 +104,16 @@ def test_pack_untilize_quasar(test_name, formats_dest_acc_dimensions):
         pytest.skip("Fails.")
 
     src_A, src_B, tile_cnt = generate_stimuli(
-        formats.input_format, formats.input_format, input_dimensions=input_dimensions
+        formats.input_format,
+        formats.input_format,
+        input_dimensions=input_dimensions,
     )
 
-    generate_golden = get_golden_generator(UntilizeGolden)
-    golden_tensor = generate_golden(src_A, formats.output_format, input_dimensions)
+    generate_golden = get_golden_generator(TilizeGolden)
+    golden_src = src_B if unpacker_sel == UnpackerEngine.UnpB else src_A
+    golden_tensor = generate_golden(
+        golden_src, input_dimensions, formats.output_format, num_faces=4
+    )
 
     unpack_to_dest = (
         formats.input_format.is_32_bit() and dest_acc == DestAccumulation.Yes
@@ -100,11 +122,12 @@ def test_pack_untilize_quasar(test_name, formats_dest_acc_dimensions):
     test_config = {
         "formats": formats,
         "testname": test_name,
-        "tile_cnt": tile_cnt,
+        "dest_acc": dest_acc,
         "input_A_dimensions": input_dimensions,
         "input_B_dimensions": input_dimensions,
         "unpack_to_dest": unpack_to_dest,
-        "dest_acc": dest_acc,
+        "tile_cnt": tile_cnt,
+        "unpacker_engine_sel": unpacker_sel,
         "implied_math_format": ImpliedMathFormat.Yes,
     }
 
@@ -119,13 +142,15 @@ def test_pack_untilize_quasar(test_name, formats_dest_acc_dimensions):
         num_faces=4,
     )
 
-    run_test(test_config)
+    run_test(test_config, boot_mode=boot_mode)
 
     res_from_L1 = collect_results(
         formats, tile_count=tile_cnt, address=res_address, num_faces=4
     )
+
     assert len(res_from_L1) == len(golden_tensor)
 
-    res_tensor = torch.tensor(res_from_L1, dtype=format_dict[formats.output_format])
+    torch_format = format_dict[formats.output_format]
+    res_tensor = torch.tensor(res_from_L1, dtype=torch_format)
 
     assert passed_test(golden_tensor, res_tensor, formats.output_format)
