0814fa09ad0c9dd6728958898270b1eba15a5cb1
Andreja Jankovic <ajankovic@tenstorrent.com>
2025-11-14 11:57:13 +0000

    Finnish

--- tests/python_tests/test_sfpu_binary.py
+++ tests/python_tests/test_sfpu_binary.py
@@ -79,7 +79,9 @@ def test_sfpu_binary_int(test_name, formats, dest_acc, mathop):
     mathop=[MathOperation.SfpuAddTopRow],
     dest_acc=[DestAccumulation.No, DestAccumulation.Yes],
 )
-def test_sfpu_binary_add_top_row(test_name, formats, dest_acc, mathop):
+def test_sfpu_binary_add_top_row(
+    test_name, formats, dest_acc, mathop, worker_tensix_location
+):
     input_dimensions = [32, 32]
 
     src_A, src_B, tile_cnt = generate_stimuli(
@@ -115,11 +117,17 @@ def test_sfpu_binary_add_top_row(test_name, formats, dest_acc, mathop):
         formats.input_format,
         tile_count_A=tile_cnt,
         tile_count_B=tile_cnt,
+        location=worker_tensix_location,
     )
 
-    run_test(test_config)
+    run_test(test_config, worker_tensix_location)
 
-    res_from_L1 = collect_results(formats, tile_count=tile_cnt, address=res_address)
+    res_from_L1 = collect_results(
+        formats,
+        tile_count=tile_cnt,
+        address=res_address,
+        location=worker_tensix_location,
+    )
 
     torch_format = format_dict[formats.output_format]
     res_tensor = torch.tensor(res_from_L1, dtype=torch_format)
@@ -170,7 +178,6 @@ def sfpu_binary(test_name, formats, dest_acc, mathop, worker_tensix_location):
         formats.input_format,
         tile_count_A=tile_cnt,
         tile_count_B=tile_cnt,
-        location=worker_tensix_location,
         location=worker_tensix_location,
     )
 
