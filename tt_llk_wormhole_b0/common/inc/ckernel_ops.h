5309b4e21ab8121a89d26a6645e42616331fdc3e
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-11-24 20:33:07 +0000

    invert STATIC/DYNAMIC

--- tt_llk_wormhole_b0/common/inc/ckernel_ops.h
+++ tt_llk_wormhole_b0/common/inc/ckernel_ops.h
@@ -973,12 +973,12 @@
 #else
 // This file is included from non-compute cores, but any direct use of
 // these macros is going to end badly.
-#define LLTT_INSN(STATIC, ENCODING) ((void)(ENCODING), __builtin_unreachable())
+#define LLTT_INSN(DYNAMIC, ENCODING) ((void)(ENCODING), __builtin_unreachable())
 #endif
 
 #if defined (LLTT_INSN)
-#define TTI_INSN(ENCODING) LLTT_INSN(true, ENCODING)
-#define TT_INSN(ENCODING) LLTT_INSN(false, ENCODING)
+#define TTI_INSN(ENCODING) LLTT_INSN(false, ENCODING)
+#define TT_INSN(ENCODING) LLTT_INSN(true, ENCODING)
 #else
 // Old lltt that does not provide lltt:ttinsn and associated macro.
 #define TTI_INSN(ENCODING) __asm__ __volatile__(".ttinsn %0" : : "n"((ENCODING)))
