2fe63d6c129c7bb2ecc44f957ff2274427d686d2
Strahinja Stanisic <sstanisic@tenstorrent.com>
2025-11-24 16:47:05 +0000

    constraints :test_tube:: add 32bit integer dest_acc constraint

--- tests/python_tests/helpers/constraints.py
+++ tests/python_tests/helpers/constraints.py
@@ -14,6 +14,9 @@ def get_valid_dest_accumulation_modes(formats):
         - Input format has B type exponent (bfp8_b, float16_b)
         - Output format is A type exponent (float16)
         Reason: HW limitation, Packer cannot convert expB to expA, so we convert it to Float32 first as intermediate. (Source???)
+    - Dest accumulation must be ENABLED for the following format combination:
+        - Input format is 32bit integer format (int32, uint32)
+        Reason: HW limitation, Unpacker cannot unpack 32bit integer formats into SrcA and SrcB registers
     - Otherwise it can be ENABLED or DISABLED
 
     NOTE: There are more combos that fit this rule, but aren't handled in the codebase
@@ -24,6 +27,9 @@ def get_valid_dest_accumulation_modes(formats):
         formats.input in [DataFormat.Bfp8_b, DataFormat.Float16_b]
         and formats.output == DataFormat.Float16
     ):
+        return [DestAccumulation.Yes]
+
+    if formats.input in [DataFormat.Int32, DataFormat.UInt32]:
         return [DestAccumulation.Yes]
 
     return [DestAccumulation.No, DestAccumulation.Yes]
