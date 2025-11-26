7391aae5b2d5549d8c56346556e3d83d0d2ab6c2
mvlahovicTT <mvlahovic@tenstorrent.com>
2025-11-26 13:42:06 +0000

    refactor: enhance FPU unpacking

--- tests/sources/simple_fpu_add_test.cpp
+++ tests/sources/simple_fpu_add_test.cpp
@@ -16,18 +16,56 @@ uint32_t math_sync_tile_dst_index = 0;
 
 #ifdef LLK_TRISC_UNPACK
 
+#include "ckernel_template.h"
 #include "llk_unpack_AB.h"
 #include "llk_unpack_common.h"
 
 void run_kernel()
 {
+    using namespace ckernel;
+
     _llk_unpack_AB_hw_configure_<is_fp32_dest_acc_en, StochRndType::None>(formats.unpack_src, formats.unpack_src, formats.unpack_dst, formats.unpack_dst);
 
-    _llk_unpack_AB_init_<>();
+    cfg_reg_rmw_tensix<THCON_SEC0_REG2_Haloize_mode_RMW>(0);
+
+    constexpr std::uint32_t UNP_SEL = p_setadc::UNP_AB;
+    config_unpacker_x_end<UNP_SEL>(FACE_R_DIM);
+
+    volatile uint tt_reg_ptr *cfg = get_cfg_pointer();
+
+    constexpr uint unpacr_A = TT_OP_UNPACR(SrcA, 0b1, 0, 0, 0, 1, 1, p_unpacr::RAREFYB_DISABLE, 0, 0, 0, 0, 1);
+    constexpr uint unpacr_B = TT_OP_UNPACR(SrcB, 0b1, 0, 0, 0, 1, 1, p_unpacr::RAREFYB_DISABLE, 0, 0, 0, 0, 1);
+
+    ckernel_template unpack_mop(1, 4, unpacr_A, unpacr_B);
+
+    unpack_mop.program();
 
     for (int i = 0; i < TILE_CNT; i++)
     {
-        _llk_unpack_AB_<>(L1_ADDRESS(buffer_A[i]), L1_ADDRESS(buffer_B[i]));
+        TTI_SETADCZW(0b011, 0, 0, 0, 0, 0b1111);
+
+        wait_for_next_context(2);
+
+        if (0 == unp_cfg_context)
+        {
+            cfg[THCON_SEC0_REG3_Base_address_ADDR32] = L1_ADDRESS(buffer_A[i]);
+            cfg[THCON_SEC1_REG3_Base_address_ADDR32] = L1_ADDRESS(buffer_B[i]);
+        }
+        else
+        {
+            cfg[THCON_SEC0_REG3_Base_cntx1_address_ADDR32] = L1_ADDRESS(buffer_A[i]);
+            cfg[THCON_SEC1_REG3_Base_cntx1_address_ADDR32] = L1_ADDRESS(buffer_B[i]);
+        }
+
+        semaphore_post(semaphore::UNPACK_SYNC);
+
+        TTI_STALLWAIT(p_stall::STALL_UNPACK, p_stall::TRISC_CFG);
+
+        ckernel_template::run();
+
+        t6_semaphore_get(semaphore::UNPACK_SYNC);
+
+        switch_config_context(unp_cfg_context);
     }
 }
 
@@ -55,24 +93,17 @@ void run_kernel()
     }
         .set(ADDR_MOD_0);
 
-    addr_mod_t {
-        .srca = {.incr = 0},
-        .srcb = {.incr = 0},
-        .dest = {.incr = 0},
-    }
-        .set(ADDR_MOD_1);
-
     constexpr uint outerloop   = 4;
     constexpr uint innerloop   = 16 >> 3;
     const uint addr_mod        = ADDR_MOD_0;
     const uint32_t acc_to_dest = 0;
     auto broadcast_type        = p_elwise::SRCB_NO_BCAST;
 
-    ckernel_template mop_config(outerloop, innerloop, TT_OP_ELWADD(0, acc_to_dest, broadcast_type, addr_mod, 0));
+    ckernel_template math_mop(outerloop, innerloop, TT_OP_ELWADD(0, acc_to_dest, broadcast_type, addr_mod, 0));
 
-    mop_config.set_end_op(TT_OP_SETRWC(p_setrwc::CLR_AB, p_setrwc::CR_AB, 0, 0, 0, p_setrwc::SET_AB));
+    math_mop.set_end_op(TT_OP_SETRWC(p_setrwc::CLR_AB, p_setrwc::CR_AB, 0, 0, 0, p_setrwc::SET_AB));
 
-    mop_config.program();
+    math_mop.program();
 
     TTI_SETC16(CLR_DVALID_SrcA_Disable_ADDR32, 0);
 
