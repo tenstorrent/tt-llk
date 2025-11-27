69eecf79feeaa3125cefbee5914fbed92464434f
Marko Radosavljevic <markoradosavljevic@tenstorrent.com>
2025-11-27 14:34:17 +0000

    genarating config from operands instead of test config dict

--- tests/python_tests/helpers/fuse_operation.py
+++ tests/python_tests/helpers/fuse_operation.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass, field
 from typing import Dict, Optional, Type
 
 from .fuse_math import Math
-from .fuse_operand import OperandMapping
+from .fuse_operand import OperandMapping, OperandRegistry
 from .fuse_packer import Packer
 from .fuse_unpacker import Unpacker
 
@@ -18,6 +18,7 @@ class PipelineOperation:
     packer: Type[Packer]
     config: Dict
     operand_mapping: OperandMapping
+    operand_registry: OperandRegistry = field(default=None)
     _generated_config: Optional[Dict] = field(default=None, init=False, repr=False)
 
     @property
@@ -25,7 +26,9 @@ class PipelineOperation:
         if self._generated_config is None:
             from .test_config import generate_operation_config
 
-            self._generated_config = generate_operation_config(self.config)
+            self._generated_config = generate_operation_config(
+                self.operand_registry, self
+            )
         return self._generated_config
 
     def unpack(self) -> str:
