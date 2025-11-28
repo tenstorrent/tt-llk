2d836cc8856b03d5c8eea864b56b94ab67baee61
Marko Radosavljevic <markoradosavljevic@tenstorrent.com>
2025-11-28 14:12:13 +0000

    refactor: move pipeline creation to separate function

--- tests/python_tests/fuse_test.py
+++ tests/python_tests/fuse_test.py
@@ -11,16 +11,10 @@ from helpers.device import (
 )
 from helpers.format_config import DataFormat, FormatConfig, is_dest_acc_needed
 from helpers.fuse_golden import FuseGolden
-from helpers.fuse_math import BinarySfpu, Math, MatmulFpu, UnarySfpu
-from helpers.fuse_operand import OperandRegistry
-from helpers.fuse_operation import PipelineOperation
-from helpers.fuse_packer import MatmulPacker
-from helpers.fuse_unpacker import MatmulUnpacker
+from helpers.fuse_pipeline import create_fuse_pipeline
 from helpers.llk_params import (
-    ApproximationMode,
     DestAccumulation,
     MathFidelity,
-    MathOperation,
 )
 from helpers.matmul_sweep import generate_matmul_dimension_combinations
 from helpers.param_config import input_output_formats, parametrize
@@ -87,68 +81,10 @@ ALL_MATMUL_COMBINATIONS = generate_format_aware_matmul_combinations(
 def test_matmul(
     test_name, math_fidelity, format_dest_acc_and_dims, boot_mode=BootMode.DEFAULT
 ):
-    formats = format_dest_acc_and_dims[0]
-    dest_acc = format_dest_acc_and_dims[1]
-    input_A_dimensions = format_dest_acc_and_dims[2][0]
-    input_B_dimensions = format_dest_acc_and_dims[2][1]
-
-    operands = OperandRegistry()
-
-    pipeline = [
-        PipelineOperation(
-            operand_mapping=operands.create_mapping(
-                src_a="input_A",
-                src_b="input_B",
-                output="matmul_result",
-                src_a_dims=input_A_dimensions,
-                src_b_dims=input_B_dimensions,
-                input_format=formats.input_format,
-                output_format=formats.output_format,
-            ),
-            unpacker=MatmulUnpacker,
-            math=Math(MatmulFpu, []),
-            packer=MatmulPacker,
-            dest_acc=dest_acc,
-            math_fidelity=math_fidelity,
-        ),
-        PipelineOperation(
-            operand_mapping=operands.create_mapping(
-                src_a="matmul_result",
-                src_b="input_C",
-                output="final_output",
-                src_b_dims=input_B_dimensions,
-                input_format=formats.output_format,
-                output_format=formats.output_format,
-            ),
-            unpacker=MatmulUnpacker,
-            math=Math(
-                MatmulFpu,
-                [
-                    UnarySfpu(
-                        MathOperation.Sqrt,
-                        ApproximationMode.No,
-                        32 * operands.get("final_output").tile_count,
-                    ),
-                    UnarySfpu(
-                        MathOperation.Neg,
-                        ApproximationMode.No,
-                        32 * operands.get("final_output").tile_count,
-                    ),
-                    BinarySfpu(
-                        MathOperation.SfpuElwadd,
-                        ApproximationMode.No,
-                        32 * operands.get("final_output").tile_count,
-                        0,
-                        0,
-                        0,
-                    ),
-                ],
-            ),
-            packer=MatmulPacker,
-            dest_acc=dest_acc,
-            math_fidelity=math_fidelity,
-        ),
-    ]
+    pipeline = create_fuse_pipeline(
+        format_dest_acc_and_dims,
+        math_fidelity,
+    )
 
     write_pipeline_operands_to_l1(pipeline)
 
