813280a856f031f6b050e28aaf6ffaa64a8bb79a
Marko Radosavljevic <markoradosavljevic@tenstorrent.com>
2025-12-01 16:03:08 +0000

    added support for custom operand data

--- tests/python_tests/helpers/fuse_pipeline.py
+++ tests/python_tests/helpers/fuse_pipeline.py
@@ -33,12 +33,14 @@ def create_fuse_pipeline(
         PipelineOperation(
             operand_mapping=operands.create_mapping(
                 src_a="input_A",
-                src_b="input_A",
+                src_b="input_B",
                 output="elwadd1",
                 src_a_dims=input_A_dimensions,
-                src_b_dims=input_A_dimensions,
+                src_b_dims=input_B_dimensions,
                 input_format=formats.input_format,
                 output_format=formats.output_format,
+                src_a_tensor=a_data,
+                src_b_tensor=b_data,
             ),
             unpacker=EltwiseUnpacker,
             math=Math(EltwiseFpu(MathOperation.Elwadd), []),
