4ede502e4b8dff9c6276cb9bdc807fa68f3eb291
asr2003 <162500856+asr2003@users.noreply.github.com>
2025-12-04 19:23:59 +0530

    fix CI

--- tests/sources/eltwise_unary_sfpu_test.cpp
+++ tests/sources/eltwise_unary_sfpu_test.cpp
@@ -208,7 +208,7 @@ void run_kernel()
 #ifdef ARCH_BLACKHOLE
     _llk_pack_dest_init_<DstSync::SyncHalf, dest_datum_width, DstTileFaceLayout::RowMajor>();
 #else
-    _llk_pack_dest_init_<DstSync::SyncHalf, false, DstTileFaceLayout::RowMajor, false>();
+    _llk_pack_dest_init_<DstSync::SyncHalf, dest_datum_width, DstTileFaceLayout::RowMajor, false>();
 #endif
 
     _llk_packer_wait_for_math_done_();
