da7a72a2ded70783f2bd30437ea352f0fdc54e9a
Stefan Krsmanovic <skrsmanovic@tenstorrent.com>
2025-12-05 16:28:49 +0000

    #0: Add pack isolation for unpack untilize perf tests
    - Added hacky solution to support measuring isolated packer performance
    for unpack untilize perf test.

--- tests/sources/unpack_untilize_perf.cpp
+++ tests/sources/unpack_untilize_perf.cpp
@@ -18,7 +18,7 @@ uint32_t unp_cfg_context          = 0;
 uint32_t pack_sync_tile_dst_ptr   = 0;
 uint32_t math_sync_tile_dst_index = 0;
 
-static_assert(PERF_RUN_TYPE == PerfRunType::L1_TO_L1, "Only L1 to L1 is supported for this benchmark");
+// static_assert(PERF_RUN_TYPE == PerfRunType::L1_TO_L1, "Only L1 to L1 is supported for this benchmark");
 
 static constexpr uint32_t MAX_TILES_DEST = is_fp32_dest_acc_en ? 4 : 8;
 
@@ -47,11 +47,17 @@ void run_kernel()
 
     {
         ZONE_SCOPED("TILE_LOOP")
-
-        for (uint32_t tile = 0; tile < TILE_CNT; tile += FULL_CT_DIM)
+        if constexpr (PERF_RUN_TYPE == PerfRunType::PACK_ISOLATE)
         {
-            _llk_unpack_untilize_pass_<true>(PERF_ADDRESS(PERF_INPUT_A, tile), FULL_CT_DIM);
-            _llk_unpack_untilize_pass_<false>(PERF_ADDRESS(PERF_INPUT_A, tile), FULL_CT_DIM);
+            return;
+        }
+        else
+        {
+            for (uint32_t tile = 0; tile < TILE_CNT; tile += FULL_CT_DIM)
+            {
+                _llk_unpack_untilize_pass_<true>(PERF_ADDRESS(PERF_INPUT_A, tile), FULL_CT_DIM);
+                _llk_unpack_untilize_pass_<false>(PERF_ADDRESS(PERF_INPUT_A, tile), FULL_CT_DIM);
+            }
         }
         PROFILER_SYNC();
     }
@@ -85,20 +91,26 @@ void run_kernel()
 
     {
         ZONE_SCOPED("TILE_LOOP")
-
-        for (uint32_t loop = 0; loop < LOOP_FACTOR; loop++)
+        if constexpr (PERF_RUN_TYPE == PerfRunType::PACK_ISOLATE)
         {
-            for (uint32_t block_start = 0; block_start < TILE_CNT; block_start += MAX_TILES_DEST)
+            return;
+        }
+        else
+        {
+            for (uint32_t loop = 0; loop < LOOP_FACTOR; loop++)
             {
-                uint32_t block_tiles = std::min(TILE_CNT - block_start, MAX_TILES_DEST);
-
-                _llk_math_wait_for_dest_available_<DstSync::SyncHalf>();
-                for (uint32_t block_tile = 0; block_tile < block_tiles; ++block_tile)
+                for (uint32_t block_start = 0; block_start < TILE_CNT; block_start += MAX_TILES_DEST)
                 {
-                    _llk_math_eltwise_unary_datacopy_<DataCopyType::A2D, DstSync::SyncHalf, is_fp32_dest_acc_en, BroadcastType::NONE, unpack_to_dest>(
-                        block_tile, formats.math, formats.math);
+                    uint32_t block_tiles = std::min(TILE_CNT - block_start, MAX_TILES_DEST);
+
+                    _llk_math_wait_for_dest_available_<DstSync::SyncHalf>();
+                    for (uint32_t block_tile = 0; block_tile < block_tiles; ++block_tile)
+                    {
+                        _llk_math_eltwise_unary_datacopy_<DataCopyType::A2D, DstSync::SyncHalf, is_fp32_dest_acc_en, BroadcastType::NONE, unpack_to_dest>(
+                            block_tile, formats.math, formats.math);
+                    }
+                    _llk_math_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
                 }
-                _llk_math_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
             }
         }
         PROFILER_SYNC();
@@ -134,18 +146,35 @@ void run_kernel()
     {
         ZONE_SCOPED("TILE_LOOP")
 
-        for (uint32_t loop = 0; loop < LOOP_FACTOR; loop++)
+        if constexpr (PERF_RUN_TYPE == PerfRunType::PACK_ISOLATE)
         {
-            for (uint32_t block_start = 0; block_start < TILE_CNT; block_start += MAX_TILES_DEST)
+            for (uint32_t loop = 0; loop < LOOP_FACTOR; loop++)
             {
-                uint32_t block_tiles = std::min(TILE_CNT - block_start, MAX_TILES_DEST);
-
-                _llk_packer_wait_for_math_done_();
-                for (uint32_t block_tile = 0; block_tile < block_tiles; ++block_tile)
+                for (uint32_t block_start = 0; block_start < TILE_CNT; block_start += MAX_TILES_DEST)
                 {
-                    _llk_pack_<DstSync::SyncHalf, is_fp32_dest_acc_en, UNTILIZE>(block_tile, PERF_ADDRESS(PERF_OUTPUT, block_start + block_tile));
+                    uint32_t block_tiles = std::min(TILE_CNT - block_start, MAX_TILES_DEST);
+                    for (uint32_t block_tile = 0; block_tile < block_tiles; ++block_tile)
+                    {
+                        _llk_pack_<DstSync::SyncHalf, is_fp32_dest_acc_en, UNTILIZE>(block_tile, PERF_ADDRESS(PERF_OUTPUT, block_start + block_tile));
+                    }
                 }
-                _llk_pack_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+            }
+        }
+        else
+        {
+            for (uint32_t loop = 0; loop < LOOP_FACTOR; loop++)
+            {
+                for (uint32_t block_start = 0; block_start < TILE_CNT; block_start += MAX_TILES_DEST)
+                {
+                    uint32_t block_tiles = std::min(TILE_CNT - block_start, MAX_TILES_DEST);
+
+                    _llk_packer_wait_for_math_done_();
+                    for (uint32_t block_tile = 0; block_tile < block_tiles; ++block_tile)
+                    {
+                        _llk_pack_<DstSync::SyncHalf, is_fp32_dest_acc_en, UNTILIZE>(block_tile, PERF_ADDRESS(PERF_OUTPUT, block_start + block_tile));
+                    }
+                    _llk_pack_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
+                }
             }
         }
         PROFILER_SYNC();
