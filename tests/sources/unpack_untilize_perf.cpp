9d6d1b48bfbfefb52967d6a095f916d703126558
Nikola Cvetkovic <ncvetkovic@tenstorrent.com>
2025-12-13 08:17:56 +0000

    #33824: Remove _llk_unpack_untilize_hw_configure_

--- tests/sources/unpack_untilize_perf.cpp
+++ tests/sources/unpack_untilize_perf.cpp
@@ -40,7 +40,8 @@ void run_kernel()
 
     {
         ZONE_SCOPED("INIT")
-        _llk_unpack_untilize_hw_configure_<is_fp32_dest_acc_en, StochRndType::None>(formats.unpack_src, formats.unpack_dst, FACE_R_DIM, 0, 4);
+        _llk_unpack_hw_configure_<is_fp32_dest_acc_en>(
+            formats.unpack_src, formats.unpack_src, formats.unpack_dst, formats.unpack_dst, FACE_R_DIM, FACE_R_DIM, 4, 4);
         _llk_unpack_untilize_init_(formats.unpack_dst, TILE_SIZE, FACE_R_DIM);
         PROFILER_SYNC();
     }
