41c5bff8964d3d5422e27dcb6e1995abb3911d61
Strahinja Stanisic <sstanisic@tenstorrent.com>
2025-12-14 18:40:14 +0000

    llk_san: architechture

---
+++ tt_llk_common/llk_san.h
@@ -0,0 +1,166 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <string>
+
+#include "llk_san_extended.h"
+#include "llk_san_impl.h"
+
+namespace llk_san
+{
+
+// per thread state
+extern unpack_state_t unpack_state;
+extern math_state_t math_state;
+extern pack_state_t pack_state;
+
+// Goes in ComputeAPI
+// State set only
+// sstanisic todo: implement support_backtrace_impl
+// static inline void support_backtrace(std::string function_name)
+// {
+//     support_backtrace_impl(function_name);
+// }
+
+// Goes in LLK_API
+// State set only
+// sstanisic todo: implement support_globals_impl
+// static inline void support_globals(bool dst_acc_mode, DstSync dst_sync, bool approx, std::int32_t math_fidelity)
+// {
+//     support_globals_impl(dst_acc_mode, dst_sync, approx, math_fidelity);
+// }
+
+// State set only
+// sstanisic todo: implement support_operand_impl
+// template <llk_san_operand operand>
+// static inline void llk_san_support_operand(
+//     std::int32_t src_fmt,
+//     std::int32_t dst_fmt,
+//     std::int32_t num_faces,
+//     std::int32_t partial_face,
+//     std::int32_t face_r_dim,
+//     std::int32_t narrow_tile,
+//     std::int32_t tile_r_dim,
+//     std::int32_t tile_c_dim,
+//     std::int32_t tile_size,
+//     std::int32_t page_size)
+// {
+//     support_operand_impl<operand>(
+//         src_fmt, dst_fmt, num_faces, partial_face, face_r_dim, narrow_tile, tile_r_dim, tile_c_dim, tile_size, page_size);
+// }
+
+// Goes in LLK_LIB in HWConfigure and HWReconfig
+// State set + no hw config within kernel check
+template <bool reconfig = false>
+static inline void unpack_hw_configure(
+    state_t<bool> dst_acc_en,
+    state_t<int32_t> src_fmt_A,
+    state_t<int32_t> src_fmt_B,
+    state_t<int32_t> dst_fmt_A,
+    state_t<int32_t> dst_fmt_B,
+    state_t<int32_t> face_height_A,
+    state_t<int32_t> face_height_B,
+    state_t<int32_t> num_faces_A,
+    state_t<int32_t> num_faces_B)
+{
+    unpack_hw_configure_impl<reconfig>(
+        unpack_state, dst_acc_en, src_fmt_A, src_fmt_B, dst_fmt_A, dst_fmt_B, face_height_A, face_height_B, num_faces_A, num_faces_B);
+}
+
+// State set + no hw config within kernel check
+template <bool reconfig = false>
+static inline void math_hw_configure(state_t<int32_t> math_fmt_A, state_t<int32_t> math_fmt_B)
+{
+    math_hw_configure_impl<reconfig>(math_state, math_fmt_A, math_fmt_B);
+}
+
+// State set + no hw config within kernel check
+template <bool reconfig = false>
+static inline void pack_hw_configure(
+    state_t<bool> dest_acc_en,
+    state_t<int32_t> src_fmt,
+    state_t<int32_t> dst_fmt,
+    state_t<int32_t> face_height,
+    state_t<int32_t> tile_width,
+    state_t<int32_t> num_faces,
+    state_t<int32_t> partial_face,
+    state_t<int32_t> narrow_tile)
+{
+    pack_hw_configure_impl<reconfig>(pack_state, dest_acc_en, src_fmt, dst_fmt, face_height, tile_width, num_faces, partial_face, narrow_tile);
+}
+
+// Goes in LLK_LIB in Init, Execute and Uninit
+// No state set, just check that non x arguments match the stored ones
+static inline void unpack_operand_check(
+    state_t<bool> dst_acc_en,
+    state_t<int32_t> src_fmt_A,
+    state_t<int32_t> src_fmt_B,
+    state_t<int32_t> dst_fmt_A,
+    state_t<int32_t> dst_fmt_B,
+    state_t<int32_t> face_height_A,
+    state_t<int32_t> face_height_B,
+    state_t<int32_t> num_faces_A,
+    state_t<int32_t> num_faces_B)
+{
+    unpack_operand_check_impl(unpack_state, dst_acc_en, src_fmt_A, src_fmt_B, dst_fmt_A, dst_fmt_B, face_height_A, face_height_B, num_faces_A, num_faces_B);
+}
+
+// No state set, just check that non x arguments match the stored ones
+static inline void math_operand_check(state_t<int32_t> math_fmt_A, state_t<int32_t> math_fmt_B)
+{
+    math_operand_check_impl(math_state, math_fmt_A, math_fmt_B);
+}
+
+// No state set, just check that non x arguments match the stored ones
+static inline void pack_operand_check(
+    state_t<bool> dest_acc_en,
+    state_t<int32_t> src_fmt,
+    state_t<int32_t> dst_fmt,
+    state_t<int32_t> face_height,
+    state_t<int32_t> tile_width,
+    state_t<int32_t> num_faces,
+    state_t<int32_t> partial_face,
+    state_t<int32_t> narrow_tile)
+{
+    pack_operand_check_impl(pack_state, dest_acc_en, src_fmt, dst_fmt, face_height, tile_width, num_faces, partial_face, narrow_tile);
+}
+
+// Goes in LLK_LIB in Init
+// Store operation type and push arguments to state stack
+// sstanisic todo: implement init_impl
+// template <llk_san_op op, typename... Ts>
+// static inline void init(Ts... args)
+// {
+//     init_impl<op, Ts...>(unpack_state, op, args...);
+// }
+
+// Set must uninit flag for operation
+// sstanisic todo: implement must_uninit_impl
+// template <llk_san_op op>
+// static inline void llk_san_must_uninit()
+// {
+//     must_uninit_impl<op>(unpack_state, op);
+// }
+
+// Goes in LLK_LIB in Execute
+// Check operation type and arguments against stored ones
+// sstanisic todo: implement operation_impl
+// template <llk_san_op op, typename... Ts>
+// static inline void llk_san_operation(Ts... args)
+// {
+//     llk_san_operation_impl<op, Ts...>(llk_san_state, op, args...);
+// }
+
+// Goes in LLK_LIB in Uninit
+// Check operation type and clear must uninit flag
+// sstanisic todo: implement uninit_impl
+// template <llk_san_op op>
+// static inline void llk_san_uninit()
+// {
+//     llk_san_uninit_impl<op>(llk_san_state, op);
+// }
+
+}; // namespace llk_san
---
+++ tt_llk_common/llk_san_impl.h
@@ -0,0 +1,206 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <string>
+
+#include "llk_assert.h"
+#include "llk_san_types.h"
+
+namespace llk_san
+{
+
+// Goes in ComputeAPI
+// State set only
+// sstanisic todo: implement support_backtrace_impl
+// static inline void support_backtrace_impl(std::string function_name)
+// {
+//     LLK_ASSERT(false, "not implemented");
+// }
+
+// Goes in LLK_API
+// State set only
+// sstanisic todo: implement support_globals_impl
+// static inline void support_globals_impl(bool dst_acc_mode, DstSync dst_sync, bool approx, std::int32_t math_fidelity)
+// {
+//     LLK_ASSERT(false, "not implemented");
+// }
+
+// // State set only
+// sstanisic todo: implement support_operand_impl
+// template <llk_san_operand_t operand>
+// static inline void llk_san_support_operand_impl(
+//     llk_san_state_t& llk_san_state,
+//     std::int32_t src_fmt,
+//     std::int32_t dst_fmt,
+//     std::int32_t num_faces,
+//     std::int32_t partial_face,
+//     std::int32_t face_r_dim,
+//     std::int32_t narrow_tile,
+//     std::int32_t tile_r_dim,
+//     std::int32_t tile_c_dim,
+//     std::int32_t tile_size,
+//     std::int32_t page_size)
+// {
+//     LLK_SAN_PANIC(false, "not implemented");
+// }
+
+// Goes in LLK_LIB in HWConfigure and HWReconfig
+// State set + no hw config within kernel check
+template <bool reconfig>
+static inline void unpack_hw_configure_impl(
+    unpack_state_t& state,
+    state_t<bool> dst_acc_en,
+    state_t<int32_t> src_fmt_A,
+    state_t<int32_t> src_fmt_B,
+    state_t<int32_t> dst_fmt_A,
+    state_t<int32_t> dst_fmt_B,
+    state_t<int32_t> face_height_A,
+    state_t<int32_t> face_height_B,
+    state_t<int32_t> num_faces_A,
+    state_t<int32_t> num_faces_B)
+{
+    LLK_SAN_PANIC(!reconfig && state.is_configured, "panic: llk_san: user tried to configure unpacker twice");
+    LLK_SAN_PANIC(reconfig && !state.is_configured, "panic: llk_san: user tried to reconfigure unpacker before configuring it");
+
+    unpack_src_state_t& src_a = state.src_a;
+
+    src_a.input_format  = src_fmt_A;
+    src_a.output_format = dst_fmt_A;
+    src_a.face_height   = face_height_A;
+    src_a.num_faces     = num_faces_A;
+
+    unpack_src_state_t& src_b = state.src_b;
+
+    src_b.input_format  = src_fmt_B;
+    src_b.output_format = dst_fmt_B;
+    src_b.face_height   = face_height_B;
+    src_b.num_faces     = num_faces_B;
+
+    state.dest_width_32 = dst_acc_en;
+    state.is_configured = true;
+}
+
+// State set + no hw config within kernel check
+template <bool reconfig = false>
+static inline void math_hw_configure_impl(math_state_t& state, state_t<int32_t> math_fmt_A, state_t<int32_t> math_fmt_B)
+{
+    LLK_SAN_PANIC(!reconfig && state.is_configured, "panic: llk_san: user tried to configure math twice");
+    LLK_SAN_PANIC(reconfig && !state.is_configured, "panic: llk_san: user tried to reconfigure math before configuring it");
+
+    state.src_a.input_format = math_fmt_A;
+    state.src_b.input_format = math_fmt_B;
+    state.is_configured      = true;
+}
+
+// State set + no hw config within kernel check
+template <bool reconfig>
+static inline void pack_hw_configure_impl(
+    pack_state_t& state,
+    state_t<bool> dest_acc_en,
+    state_t<int32_t> src_fmt,
+    state_t<int32_t> dst_fmt,
+    state_t<int32_t> face_height,
+    state_t<int32_t> tile_width,
+    state_t<int32_t> num_faces,
+    state_t<int32_t> partial_face,
+    state_t<int32_t> narrow_tile)
+{
+    LLK_SAN_PANIC(reconfig && !state.is_configured, "panic: llk_san: user tried to reconfigure packer before configuring it");
+    LLK_SAN_PANIC(!reconfig && state.is_configured, "panic: llk_san: user tried to configure packer twice");
+
+    state.input_format  = src_fmt;
+    state.output_format = dst_fmt;
+    state.face_height   = face_height;
+    state.tile_width    = tile_width;
+    state.num_faces     = num_faces;
+    state.partial_face  = partial_face;
+    state.narrow_tile   = narrow_tile;
+    state.dest_width_32 = dest_acc_en;
+    state.is_configured = true;
+}
+
+// Goes in LLK_LIB in Init, Execute and Uninit
+// No state set, just check that non x arguments match the stored ones
+static inline void unpack_operand_check_impl(
+    unpack_state_t& state,
+    state_t<bool> dest_acc_en,
+    state_t<int32_t> src_fmt_A,
+    state_t<int32_t> src_fmt_B,
+    state_t<int32_t> dst_fmt_A,
+    state_t<int32_t> dst_fmt_B,
+    state_t<int32_t> face_height_A,
+    state_t<int32_t> face_height_B,
+    state_t<int32_t> num_faces_A,
+    state_t<int32_t> num_faces_B)
+{
+    LLK_SAN_PANIC(!state.is_configured, "panic: llk_san: executing init/execute/uninit before hwconfigure");
+    LLK_SAN_PANIC(state.dest_width_32 == dest_acc_en, "panic: llk_san: dest_acc_en doesn't match state.dest_width_32");
+    LLK_SAN_PANIC(state.src_a.input_format != src_fmt_A, "panic: llk_san: src_fmt_A doesn't match state.src_a.input_format");
+    LLK_SAN_PANIC(state.src_b.input_format != src_fmt_B, "panic: llk_san: src_fmt_B doesn't match state.src_b.input_format");
+    LLK_SAN_PANIC(state.src_a.output_format != dst_fmt_A, "panic: llk_san: dst_fmt_A doesn't match state.src_a.output_format");
+    LLK_SAN_PANIC(state.src_b.output_format != dst_fmt_B, "panic: llk_san: dst_fmt_B doesn't match state.src_b.output_format");
+    LLK_SAN_PANIC(state.src_a.face_height != face_height_A, "panic: llk_san: face_height_A doesn't match state.src_a.face_height");
+    LLK_SAN_PANIC(state.src_b.face_height != face_height_B, "panic: llk_san: face_height_B doesn't match state.src_b.face_height");
+    LLK_SAN_PANIC(state.src_a.num_faces != num_faces_A, "panic: llk_san: num_faces_A doesn't match state.src_a.num_faces");
+    LLK_SAN_PANIC(state.src_b.num_faces != num_faces_B, "panic: llk_san: num_faces_B doesn't match state.src_b.num_faces");
+}
+
+// No state set, just check that non x arguments match the stored ones
+static inline void math_operand_check_impl(math_state_t& state, state_t<int32_t> math_fmt_A, state_t<int32_t> math_fmt_B)
+{
+    LLK_SAN_PANIC(state.src_a.input_format != math_fmt_A, "panic: llk_san: math_fmt_A doesn't match state.src_a.input_format");
+    LLK_SAN_PANIC(state.src_b.input_format != math_fmt_B, "panic: llk_san: math_fmt_B doesn't match state.src_b.input_format");
+}
+
+// No state set, just check that non x arguments match the stored ones
+static inline void pack_operand_check_impl(
+    pack_state_t& state,
+    state_t<bool> dest_acc_en,
+    state_t<int32_t> src_fmt,
+    state_t<int32_t> dst_fmt,
+    state_t<int32_t> face_height,
+    state_t<int32_t> tile_width,
+    state_t<int32_t> num_faces,
+    state_t<int32_t> partial_face,
+    state_t<int32_t> narrow_tile)
+{
+    LLK_SAN_PANIC(state.dest_width_32 != dest_acc_en, "panic: llk_san: dest_acc_en doesn't match state.dest_width_32");
+    LLK_SAN_PANIC(state.input_format != src_fmt, "panic: llk_san: src_fmt doesn't match state.input_format");
+    LLK_SAN_PANIC(state.output_format != dst_fmt, "panic: llk_san: dst_fmt doesn't match state.output_format");
+    LLK_SAN_PANIC(state.face_height != face_height, "panic: llk_san: face_height doesn't match state.face_height");
+    LLK_SAN_PANIC(state.tile_width != tile_width, "panic: llk_san: tile_width doesn't match state.tile_width");
+    LLK_SAN_PANIC(state.num_faces != num_faces, "panic: llk_san: num_faces doesn't match state.num_faces");
+    LLK_SAN_PANIC(state.partial_face != partial_face, "panic: llk_san: partial_face doesn't match state.partial_face");
+    LLK_SAN_PANIC(state.narrow_tile != narrow_tile, "panic: llk_san: narrow_tile doesn't match state.narrow_tile");
+}
+
+// Goes in LLK_LIB in Init
+// Store operation type and push arguments to state stack
+// sstanisic todo: implement init_impl
+// static inline void init_impl(Ts... args)
+// {
+//     LLK_ASSERT(false, "not implemented");
+// }
+
+// Goes in LLK_LIB in Execute
+// Check operation type and arguments against stored ones
+// sstanisic todo: implement operation_impl
+// template <llk_san_op op, typename... Ts>
+// static inline void operation_impl(Ts... args)
+// {
+//     LLK_ASSERT(false, "not implemented");
+// }
+
+// Goes in LLK_LIB in Uninit
+// Check operation type and clear must uninit flag
+// sstanisic todo: implement uninit_impl
+// template <llk_san_op op>
+// static inline void uninit_impl()
+// {
+//     LLK_ASSERT(false, "not implemented");
+// }
+
+} // namespace llk_san
