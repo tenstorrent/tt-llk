871569239c3a9593ef758d75e05350cc878f32a3
ldjurovicTT <ldjurovic@tenstorrent.com>
2025-12-17 10:23:54 +0000

    Mutex guard class

---
+++ tt_llk_wormhole_b0/common/inc/ckernel_mutex_guard.h
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+namespace ckernel
+{
+
+class T6MutexGuard
+{
+public:
+    explicit T6MutexGuard(const uint8_t index) : mutex_index(index)
+    {
+        t6_mutex_acquire(mutex_index);
+    }
+
+    ~T6MutexGuard()
+    {
+        t6_mutex_release(mutex_index);
+    }
+
+    // Non-copyable
+    T6MutexGuard(const T6MutexGuard&)            = delete;
+    T6MutexGuard& operator=(const T6MutexGuard&) = delete;
+
+    // Non-movable
+    T6MutexGuard(T6MutexGuard&&)            = delete;
+    T6MutexGuard& operator=(T6MutexGuard&&) = delete;
+
+private:
+    const uint8_t mutex_index;
+};
+
+} // namespace ckernel
