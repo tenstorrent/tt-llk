e32de7bf6a015296e506f17724617ac81e7f3dd4
Strahinja Stanisic <sstanisic@tenstorrent.com>
2025-12-17 13:16:32 +0000

    llk_san: arch: LLK_SAN_ASSERT, LLK_SAN_PANIC

--- tt_llk_common/llk_san_impl.h
+++ tt_llk_common/llk_san_impl.h
@@ -62,8 +62,8 @@ static inline void unpack_hw_configure_impl(
     state_t<uint32_t> num_faces_A,
     state_t<uint32_t> num_faces_B)
 {
-    LLK_SAN_PANIC(!reconfig && state.is_configured, "panic: llk_san: user tried to configure unpacker twice");
-    LLK_SAN_PANIC(reconfig && !state.is_configured, "panic: llk_san: user tried to reconfigure unpacker before configuring it");
+    LLK_PANIC(!reconfig && state.is_configured, "panic: llk_san: user tried to configure unpacker twice");
+    LLK_PANIC(reconfig && !state.is_configured, "panic: llk_san: user tried to reconfigure unpacker before configuring it");
 
     unpack_src_state_t& src_a = state.src_a;
 
@@ -87,8 +87,8 @@ static inline void unpack_hw_configure_impl(
 template <bool reconfig = false>
 static inline void math_hw_configure_impl(math_state_t& state, state_t<uint32_t> math_fmt_A, state_t<uint32_t> math_fmt_B)
 {
-    LLK_SAN_PANIC(!reconfig && state.is_configured, "panic: llk_san: user tried to configure math twice");
-    LLK_SAN_PANIC(reconfig && !state.is_configured, "panic: llk_san: user tried to reconfigure math before configuring it");
+    LLK_PANIC(!reconfig && state.is_configured, "panic: llk_san: user tried to configure math twice");
+    LLK_PANIC(reconfig && !state.is_configured, "panic: llk_san: user tried to reconfigure math before configuring it");
 
     state.src_a.input_format = math_fmt_A;
     state.src_b.input_format = math_fmt_B;
@@ -108,8 +108,8 @@ static inline void pack_hw_configure_impl(
     state_t<bool> partial_face,
     state_t<bool> narrow_tile)
 {
-    LLK_SAN_PANIC(reconfig && !state.is_configured, "panic: llk_san: user tried to reconfigure packer before configuring it");
-    LLK_SAN_PANIC(!reconfig && state.is_configured, "panic: llk_san: user tried to configure packer twice");
+    LLK_PANIC(reconfig && !state.is_configured, "panic: llk_san: user tried to reconfigure packer before configuring it");
+    LLK_PANIC(!reconfig && state.is_configured, "panic: llk_san: user tried to configure packer twice");
 
     state.input_format  = src_fmt;
     state.output_format = dst_fmt;
@@ -136,24 +136,26 @@ static inline void unpack_operand_check_impl(
     state_t<uint32_t> num_faces_A,
     state_t<uint32_t> num_faces_B)
 {
-    LLK_SAN_PANIC(!state.is_configured, "panic: llk_san: executing init/execute/uninit before hwconfigure");
-    LLK_SAN_PANIC(state.dest_width_32 != dest_acc_en, "panic: llk_san: dest_acc_en doesn't match state.dest_width_32");
-    LLK_SAN_PANIC(state.src_a.input_format != src_fmt_A, "panic: llk_san: src_fmt_A doesn't match state.src_a.input_format");
-    LLK_SAN_PANIC(state.src_b.input_format != src_fmt_B, "panic: llk_san: src_fmt_B doesn't match state.src_b.input_format");
-    LLK_SAN_PANIC(state.src_a.output_format != dst_fmt_A, "panic: llk_san: dst_fmt_A doesn't match state.src_a.output_format");
-    LLK_SAN_PANIC(state.src_b.output_format != dst_fmt_B, "panic: llk_san: dst_fmt_B doesn't match state.src_b.output_format");
-    LLK_SAN_PANIC(state.src_a.face_height != face_height_A, "panic: llk_san: face_height_A doesn't match state.src_a.face_height");
-    LLK_SAN_PANIC(state.src_b.face_height != face_height_B, "panic: llk_san: face_height_B doesn't match state.src_b.face_height");
-    LLK_SAN_PANIC(state.src_a.num_faces != num_faces_A, "panic: llk_san: num_faces_A doesn't match state.src_a.num_faces");
-    LLK_SAN_PANIC(state.src_b.num_faces != num_faces_B, "panic: llk_san: num_faces_B doesn't match state.src_b.num_faces");
+    LLK_PANIC(!state.is_configured, "panic: llk_san: executing init/execute/uninit before hwconfigure");
+
+    LLK_SAN_ASSERT(state.dest_width_32, dest_acc_en, "panic: llk_san: dest_acc_en doesn't match state.dest_width_32");
+
+    LLK_SAN_ASSERT(state.src_a.input_format, src_fmt_A, "panic: llk_san: src_fmt_A doesn't match state.src_a.input_format");
+    LLK_SAN_ASSERT(state.src_b.input_format, src_fmt_B, "panic: llk_san: src_fmt_B doesn't match state.src_b.input_format");
+    LLK_SAN_ASSERT(state.src_a.output_format, dst_fmt_A, "panic: llk_san: dst_fmt_A doesn't match state.src_a.output_format");
+    LLK_SAN_ASSERT(state.src_b.output_format, dst_fmt_B, "panic: llk_san: dst_fmt_B doesn't match state.src_b.output_format");
+    LLK_SAN_ASSERT(state.src_a.face_height, face_height_A, "panic: llk_san: face_height_A doesn't match state.src_a.face_height");
+    LLK_SAN_ASSERT(state.src_b.face_height, face_height_B, "panic: llk_san: face_height_B doesn't match state.src_b.face_height");
+    LLK_SAN_ASSERT(state.src_a.num_faces, num_faces_A, "panic: llk_san: num_faces_A doesn't match state.src_a.num_faces");
+    LLK_SAN_ASSERT(state.src_b.num_faces, num_faces_B, "panic: llk_san: num_faces_B doesn't match state.src_b.num_faces");
 }
 
 // No state set, just check that non x arguments match the stored ones
 static inline void math_operand_check_impl(math_state_t& state, state_t<uint32_t> math_fmt_A, state_t<uint32_t> math_fmt_B)
 {
-    LLK_SAN_PANIC(!state.is_configured, "panic: llk_san: executing init/execute/uninit before hwconfigure");
-    LLK_SAN_PANIC(state.src_a.input_format != math_fmt_A, "panic: llk_san: math_fmt_A doesn't match state.src_a.input_format");
-    LLK_SAN_PANIC(state.src_b.input_format != math_fmt_B, "panic: llk_san: math_fmt_B doesn't match state.src_b.input_format");
+    LLK_PANIC(!state.is_configured, "panic: llk_san: executing init/execute/uninit before hwconfigure");
+    LLK_SAN_ASSERT(state.src_a.input_format, math_fmt_A, "panic: llk_san: math_fmt_A doesn't match state.src_a.input_format");
+    LLK_SAN_ASSERT(state.src_b.input_format, math_fmt_B, "panic: llk_san: math_fmt_B doesn't match state.src_b.input_format");
 }
 
 // No state set, just check that non x arguments match the stored ones
@@ -162,21 +164,21 @@ static inline void pack_operand_check_impl(
     state_t<bool> dest_acc_en,
     state_t<uint32_t> src_fmt,
     state_t<uint32_t> dst_fmt,
-    state_t<uint32_t> face_height,
+    [[maybe_unused]] state_t<uint32_t> face_height,
     state_t<uint32_t> tile_width,
     state_t<uint32_t> num_faces,
     state_t<bool> partial_face,
     state_t<bool> narrow_tile)
 {
-    LLK_SAN_PANIC(!state.is_configured, "panic: llk_san: executing init/execute/uninit before hwconfigure");
-    LLK_SAN_PANIC(state.dest_width_32 != dest_acc_en, "panic: llk_san: dest_acc_en doesn't match state.dest_width_32");
-    LLK_SAN_PANIC(state.input_format != src_fmt, "panic: llk_san: src_fmt doesn't match state.input_format");
-    LLK_SAN_PANIC(state.output_format != dst_fmt, "panic: llk_san: dst_fmt doesn't match state.output_format");
-    LLK_SAN_PANIC(state.face_height != face_height, "panic: llk_san: face_height doesn't match state.face_height");
-    LLK_SAN_PANIC(state.tile_width != tile_width, "panic: llk_san: tile_width doesn't match state.tile_width");
-    LLK_SAN_PANIC(state.num_faces != num_faces, "panic: llk_san: num_faces doesn't match state.num_faces");
-    LLK_SAN_PANIC(state.partial_face != partial_face, "panic: llk_san: partial_face doesn't match state.partial_face");
-    LLK_SAN_PANIC(state.narrow_tile != narrow_tile, "panic: llk_san: narrow_tile doesn't match state.narrow_tile");
+    LLK_PANIC(!state.is_configured, "panic: llk_san: executing init/execute/uninit before hwconfigure");
+    LLK_SAN_ASSERT(state.dest_width_32, dest_acc_en, "panic: llk_san: dest_acc_en doesn't match state.dest_width_32");
+    LLK_SAN_ASSERT(state.input_format, src_fmt, "panic: llk_san: src_fmt doesn't match state.input_format");
+    LLK_SAN_ASSERT(state.output_format, dst_fmt, "panic: llk_san: dst_fmt doesn't match state.output_format");
+    // sstanisic fixme: LLK_SAN_ASSERT(state.face_height, face_height, "panic: llk_san: face_height doesn't match state.face_height");
+    LLK_SAN_ASSERT(state.tile_width, tile_width, "panic: llk_san: tile_width doesn't match state.tile_width");
+    LLK_SAN_ASSERT(state.num_faces, num_faces, "panic: llk_san: num_faces doesn't match state.num_faces");
+    LLK_SAN_ASSERT(state.partial_face, partial_face, "panic: llk_san: partial_face doesn't match state.partial_face");
+    LLK_SAN_ASSERT(state.narrow_tile, narrow_tile, "panic: llk_san: narrow_tile doesn't match state.narrow_tile");
 }
 
 // Goes in LLK_LIB in Init
