581869c01227bb6e43ee5a5eec7145a0bc19caf3
Nemanja Divnic <ndivnic@tenstorrent.com>
2025-12-17 11:16:37 +0000

    Fix tests after merging latest main.

--- tests/helpers/include/sfpu_operations.h
+++ tests/helpers/include/sfpu_operations.h
@@ -11,6 +11,8 @@
 
 namespace test_utils
 {
+using namespace ckernel;
+using namespace ckernel::sfpu;
 
 /**
  * Template function to call SFPU operations with parameterized iteration count
@@ -25,8 +27,6 @@ namespace test_utils
 template <bool APPROX_MODE, bool is_fp32_dest_acc_en, int ITERATIONS>
 void call_sfpu_operation(SfpuType operation, uint32_t math_format = 0)
 {
-    using namespace ckernel;
-    using namespace ckernel::sfpu;
     switch (operation)
     {
         case SfpuType::abs:
