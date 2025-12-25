1485dc11a66f07f1ffb9d8d4e9200e82185f0ad5
Strahinja Stanisic <sstanisic@tenstorrent.com>
2025-12-25 12:21:29 +0000

    llk_san: arch:  implement operation_save, operation_check

--- tests/helpers/src/trisc.cpp
+++ tests/helpers/src/trisc.cpp
@@ -11,9 +11,8 @@
 // Necessary for ckernel variables
 #include "ckernel_helper.h" // Only for WH/BH
 #endif
-#include "profiler.h"
 #include "llk_san_types.h"
-
+#include "profiler.h"
 
 #if defined(LLK_TRISC_UNPACK) && defined(LLK_BOOT_MODE_TRISC)
 #include "boot.h"
@@ -34,9 +33,8 @@ uint32_t open_zone_cnt    = 0;
 
 namespace llk_san
 {
-unpack_state_t unpack_state;
-math_state_t math_state;
-pack_state_t pack_state;
+hw_state_t hw_state;
+operation_state_t operation_state;
 } // namespace llk_san
 
 extern const volatile struct RuntimeParams __runtime_args_start[];
