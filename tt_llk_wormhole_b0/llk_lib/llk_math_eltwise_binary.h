3bb65320693a19d7af511ffdd8b0c2445674c1f0
Filip Vranic <fvranic@tenstorrent.com>
2025-12-25 20:27:11 +0100

    Update llk_math_eltwise_binary.h

--- tt_llk_wormhole_b0/llk_lib/llk_math_eltwise_binary.h
+++ tt_llk_wormhole_b0/llk_lib/llk_math_eltwise_binary.h
@@ -59,13 +59,13 @@ inline void _llk_math_eltwise_binary_(const std::uint32_t num_faces, uint dst_in
                 eltwise_binary_reuse_dest_as_src<binary_reuse_dest>();
                 ckernel_template::run();
             }
-            TTI_SETRWC(p_setrwc::CLR_B, 0, 0, 0, 0, 0)
+            TTI_SETRWC(p_setrwc::CLR_B, 0, 0, 0, 0, 0);
             if (num_faces == 4)
             {
 #pragma GCC unroll 0
                 for (std::uint32_t n = 0; n < outerloop; n++)
                 {
-                    eltwise_binary_reuse_dest_as_src<binary_reuse_dest>();
+                    eltwise_binary_reuse_dest_as_src<binary_reuse_dest>()
                     ckernel_template::run();
                 }
                 TTI_SETRWC(p_setrwc::CLR_B, 0, 0, 0, 0, 0);
