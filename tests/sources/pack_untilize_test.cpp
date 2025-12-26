187d2af116bbf94d5bf2b65191270424760fdae4
njokovicTT <njokovic@tenstorrent.com>
2025-12-26 15:27:38 +0000

    Moving ALU config bits for FP32 DEST accumulate from UNPACKER hw_configure into MATH hw_configure

--- tests/sources/pack_untilize_test.cpp
+++ tests/sources/pack_untilize_test.cpp
@@ -54,7 +54,7 @@ void run_kernel()
     _llk_math_eltwise_unary_datacopy_init_<DataCopyType::A2D, is_fp32_dest_acc_en, BroadcastType::NONE, is_int_fpu_en>(4, formats.math);
 #endif
     _llk_math_pack_sync_init_<DST_SYNC, is_fp32_dest_acc_en>();
-    _llk_math_hw_configure_(formats.math, formats.math);
+    _llk_math_hw_configure_<is_fp32_dest_acc_en>(formats.math, formats.math);
 #ifdef ARCH_BLACKHOLE
     _llk_math_reconfig_remap_(true);
 #endif
