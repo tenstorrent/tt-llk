bd70c9b8fc1300ac32950e6f102e8f2858e7ea27
Vuk Vukomanovic <vvukomanovic@tenstorrent.com>
2025-12-29 14:06:32 +0000

    Add uninits for BH

--- tt_llk_blackhole/llk_lib/llk_unpack_A.h
+++ tt_llk_blackhole/llk_lib/llk_unpack_A.h
@@ -229,6 +229,14 @@ inline void _llk_unpack_A_init_(
     _llk_unpack_A_mop_config_<BType, acc_to_dest, binary_reuse_dest, unpack_to_dest>(transpose_of_faces > 0, num_faces, unpack_src_format, unpack_dst_format);
 }
 
+template <BroadcastType BType = BroadcastType::NONE>
+inline void _llk_unpack_A_uninit_(const std::uint32_t face_r_dim = FACE_R_DIM)
+{
+    constexpr std::uint32_t UNP_SEL = (BType == BroadcastType::NONE) ? p_setadc::UNP_A : p_setadc::UNP_B;
+    TT_SETADCXX(UNP_SEL, face_r_dim * FACE_C_DIM - 1, 0x0);
+    reg_write(RISCV_DEBUG_REG_DBG_FEATURE_DISABLE, 0);
+}
+
 template <
     BroadcastType BType                          = BroadcastType::NONE,
     bool acc_to_dest                             = false,
