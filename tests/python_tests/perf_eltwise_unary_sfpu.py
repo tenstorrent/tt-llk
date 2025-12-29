624956d047c8fd6ff07f00cd1f63dd5e217937bf
Nemanja Divnic <ndivnic@tenstorrent.com>
2025-12-29 13:19:36 +0000

    Adjust new tests to new testing infra.

--- tests/python_tests/perf_eltwise_unary_sfpu.py
+++ tests/python_tests/perf_eltwise_unary_sfpu.py
@@ -18,19 +18,31 @@ from helpers.llk_params import (
     DestAccumulation,
     FastMode,
     MathOperation,
+    PerfRunType,
     StableSort,
+    Transpose,
 )
 from helpers.param_config import input_output_formats, parametrize
-from helpers.perf import ALL_RUN_TYPES, perf_benchmark, update_report
+from helpers.profiler import ProfilerConfig
+from helpers.stimuli_config import StimuliConfig
 from helpers.stimuli_generator import calculate_tile_and_face_counts
-
-FACE_R_DIM = 16
-NUM_FACES = 4
+from helpers.test_variant_parameters import (
+    APPROX_MODE,
+    FAST_MODE,
+    INPUT_DIMENSIONS,
+    ITERATIONS,
+    LOOP_FACTOR,
+    MATH_OP,
+    NUM_FACES,
+    STABLE_SORT,
+    TILE_COUNT,
+    UNPACK_TRANS_FACES,
+    UNPACK_TRANS_WITHING_FACE,
+)
 
 
 @pytest.mark.perf
 @parametrize(
-    test_name="eltwise_unary_sfpu_perf",
     formats=input_output_formats(
         [
             DataFormat.Float32,
@@ -72,16 +84,12 @@ NUM_FACES = 4
         StableSort.Yes,
         StableSort.No,
     ],
-    face_r_dim=[FACE_R_DIM],
-    num_faces=[NUM_FACES],
     input_dimensions=[
         [128, 64],  # tile_cnt: 8
     ],  # Specifying different input sizes to cover different tile counts
-    run_types=[ALL_RUN_TYPES],
 )
 def test_perf_eltwise_unary_sfpu(
     perf_report,
-    test_name,
     formats,
     mathop,
     approx_mode,
@@ -90,10 +98,8 @@ def test_perf_eltwise_unary_sfpu(
     iterations,
     fast_mode,
     stable_sort,
-    face_r_dim,
-    num_faces,
     input_dimensions,
-    run_types,
+    workers_tensix_coordinates,
 ):
     # Skip tests where template parameters are not used by the operation
     # Operations that don't use is_fp32_dest_acc_en parameter
@@ -147,34 +153,54 @@ def test_perf_eltwise_unary_sfpu(
             f"{mathop} does not use stable_sort parameter - skipping Yes variant"
         )
 
+    # Calculate tile count from input dimensions
+    tile_count_A, tile_count_B, faces_to_generate = calculate_tile_and_face_counts(
+        input_dimensions, input_dimensions, face_r_dim=16, num_faces=4
+    )
+
     # If dest_acc is on, we unpack Float32 into 16-bit format in src registers
     # (later copied over in dest reg for SFPU op)
-    #
     unpack_to_dest = (
         formats.input_format.is_32_bit() and dest_acc == DestAccumulation.No
     )
 
-    tile_cnt, faces_to_generate = calculate_tile_and_face_counts(
-        input_dimensions, face_r_dim, num_faces
+    configuration = ProfilerConfig(
+        "sources/eltwise_unary_sfpu_perf.cpp",
+        formats,
+        run_types=[
+            PerfRunType.L1_TO_L1,
+            PerfRunType.UNPACK_ISOLATE,
+            PerfRunType.MATH_ISOLATE,
+            PerfRunType.PACK_ISOLATE,
+            PerfRunType.L1_CONGESTION,
+        ],
+        templates=[
+            INPUT_DIMENSIONS(input_dimensions, input_dimensions),
+            MATH_OP(mathop=mathop),
+            APPROX_MODE(approx_mode),
+            ITERATIONS(iterations),
+            FAST_MODE(fast_mode),
+            STABLE_SORT(stable_sort),
+        ],
+        runtimes=[
+            TILE_COUNT(tile_count_A),
+            LOOP_FACTOR(loop_factor),
+            NUM_FACES(num_faces=faces_to_generate),
+            UNPACK_TRANS_FACES(Transpose.No),
+            UNPACK_TRANS_WITHING_FACE(Transpose.No),
+        ],
+        variant_stimuli=StimuliConfig(
+            None,
+            formats.input_format,
+            None,
+            formats.input_format,
+            formats.output_format,
+            tile_count_A=tile_count_A,
+            tile_count_B=tile_count_B,
+            tile_count_res=tile_count_A,
+        ),
+        unpack_to_dest=unpack_to_dest,
+        dest_acc=dest_acc,
     )
 
-    test_config = {
-        "formats": formats,
-        "testname": test_name,
-        "dest_acc": dest_acc,
-        "input_A_dimensions": input_dimensions,
-        "input_B_dimensions": input_dimensions,
-        "mathop": mathop,
-        "approx_mode": approx_mode,
-        "unpack_to_dest": unpack_to_dest,
-        "num_faces": faces_to_generate,
-        "face_r_dim": face_r_dim,
-        "tile_cnt": tile_cnt,
-        "loop_factor": loop_factor,
-        "iterations": iterations,
-        "fast_mode": fast_mode,
-        "stable_sort": stable_sort,
-    }
-
-    results = perf_benchmark(test_config, run_types)
-    update_report(perf_report, test_config, results)
+    configuration.run(perf_report, location=workers_tensix_coordinates)
