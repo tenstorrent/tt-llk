bbddc5dd812a9bfea4c20c27e12fbc35592f6ac6
Nemanja Divnic <ndivnic@tenstorrent.com>
2025-12-29 16:52:25 +0000

    Fix typo.

--- tests/python_tests/perf_eltwise_unary_sfpu.py
+++ tests/python_tests/perf_eltwise_unary_sfpu.py
@@ -37,7 +37,7 @@ from helpers.test_variant_parameters import (
     STABLE_SORT,
     TILE_COUNT,
     UNPACK_TRANS_FACES,
-    UNPACK_TRANS_WITHING_FACE,
+    UNPACK_TRANS_WITHIN_FACE,
 )
 
 
@@ -187,7 +187,7 @@ def test_perf_eltwise_unary_sfpu(
             LOOP_FACTOR(loop_factor),
             NUM_FACES(num_faces=faces_to_generate),
             UNPACK_TRANS_FACES(Transpose.No),
-            UNPACK_TRANS_WITHING_FACE(Transpose.No),
+            UNPACK_TRANS_WITHIN_FACE(Transpose.No),
         ],
         variant_stimuli=StimuliConfig(
             None,
