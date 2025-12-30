f9a9d42581c22674d1d6b5b93befded6f8f9e3af
Marko Radosavljevic <markoradosavljevic@tenstorrent.com>
2025-12-30 10:34:35 +0000

    cpp test name based on yaml name

--- tests/python_tests/fused_test.py
+++ tests/python_tests/fused_test.py
@@ -10,26 +10,27 @@ from helpers.device import (
     write_pipeline_operands_to_l1,
 )
 from helpers.format_config import DataFormat, FormatConfig
+from helpers.fused_generator import FUSED_TESTS_DIR
 from helpers.fused_golden import FusedGolden
 from helpers.fused_pipeline import create_fuse_pipeline
 from helpers.test_config import BootMode, ProfilerBuild, TestConfig
 
 FUSER_CONFIG_DIR = Path(__file__).parent / "fuser_config"
 yaml_files = sorted(FUSER_CONFIG_DIR.glob("*.yaml"))
-test_names = [f.stem for f in yaml_files]  # Imena fajlova bez ekstenzije
+test_names = [f.stem for f in yaml_files]
 
 
 @pytest.mark.parametrize("test_name", test_names, ids=test_names)
-def test_fused(test_name):
-
+def test_fused(test_name, regenerate_cpp):
     yaml_path = FUSER_CONFIG_DIR / f"{test_name}.yaml"
+    cpp_path = FUSED_TESTS_DIR / f"{test_name}.cpp"
 
     pipeline = create_fuse_pipeline(str(yaml_path))
 
     write_pipeline_operands_to_l1(pipeline)
 
     config = TestConfig(
-        test_name=test_name,
+        test_name=cpp_path,
         formats=FormatConfig(
             unpack_A_src=DataFormat.Float16,
             unpack_A_dst=DataFormat.Float16,
@@ -44,7 +45,7 @@ def test_fused(test_name):
         profiler_build=ProfilerBuild.No,
     )
 
-    config.run_fused(pipeline)
+    config.run_fused(pipeline, regenerate_cpp)
 
     collect_pipeline_results(pipeline)
 
