4e6d5a1a449f43e001f90fb3f0ca6a07f05c69b1
Nemanja Divnic <ndivnic@tenstorrent.com>
2026-01-04 23:56:13 +0000

    Squashed commit: Runtime assert experiment and binary SFPU perf tests

--- tt_llk_quasar/common/inc/cmath_common.h
+++ tt_llk_quasar/common/inc/cmath_common.h
@@ -159,8 +159,13 @@ inline void _zero_dest_reg_()
 template <ckernel::trisc::DstTileShape TILE_SHAPE>
 inline void _set_dst_write_addr_(const uint32_t tile_index)
 {
+    // static_assert(
+    //     TILE_SHAPE == ckernel::trisc::DstTileShape::Tile32x32 || TILE_SHAPE == ckernel::trisc::DstTileShape::Tile32x16 ||
+    //         TILE_SHAPE == ckernel::trisc::DstTileShape::Tile16x16,
+    //     "Invalid tile shape");
     const uint tile_shape_idx = (TILE_SHAPE == ckernel::trisc::DstTileShape::Tile32x32) ? 6 : ((TILE_SHAPE == ckernel::trisc::DstTileShape::Tile32x16) ? 5 : 4);
     const uint dst_index      = (tile_index << tile_shape_idx) + ckernel::trisc::_get_dest_buffer_base_();
+    LLK_ASSERT(dst_index < ckernel::trisc::DEST_REGISTER_FULL_SIZE, "dst_index out of range");
     ckernel::trisc::_set_dest_section_base_<TRISC_ID>(dst_index);
 }
 
@@ -171,6 +176,8 @@ inline void _set_dst_write_addr_by_rows_(const uint32_t num_rows_per_tile, const
             ? 6
             : ((num_rows_per_tile == 32) ? 5 : ((num_rows_per_tile == 16) ? 4 : ((num_rows_per_tile == 8) ? 3 : ((num_rows_per_tile == 4) ? 2 : 1))));
     const uint dst_index = (tile_index << tile_shape_idx) + ckernel::trisc::_get_dest_buffer_base_();
+    // LLK_ASSERT(num_rows_per_tile == (1 << tile_shape_idx), "num_rows_per_tile must be a power of two corresponding to tile_shape_idx");
+    LLK_ASSERT(dst_index < ckernel::trisc::DEST_REGISTER_FULL_SIZE, "dst_index out of range");
     ckernel::trisc::_set_dest_section_base_<TRISC_ID>(dst_index);
 }
 
