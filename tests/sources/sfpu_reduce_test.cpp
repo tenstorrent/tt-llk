88305d0cb5be7e566570b6ce998ae780c54988fe
Sofija Kotarac <skotarac@tenstorrent.com>
2026-01-05 21:27:26 +0100

    Fix loop variable type in sfpu_reduce_test.cpp

--- tests/sources/sfpu_reduce_test.cpp
+++ tests/sources/sfpu_reduce_test.cpp
@@ -73,7 +73,7 @@ void run_kernel(const volatile struct RuntimeParams *params)
 
     if (REDUCE_DIM == ReduceDim::REDUCE_COL)
     {
-        for (uint32_t i = 0; i < TILE_CNT; i++)
+        for (int i = 0; i < params->TILE_CNT; ++i)
         {
             _llk_math_eltwise_unary_sfpu_start_<DstSync::SyncHalf>(i);
             ckernel::sfpu::_calculate_reduce_<POOL_TYPE, REDUCE_DIM, static_cast<DataFormat>(formats.math)>();
