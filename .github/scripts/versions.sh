5f8f45d95cf19f716bb8304a94667618af9de3f7
Filip Vranic <fvranic@tenstorrent.com>
2026-01-07 21:35:16 +0100

    Update versions.sh

--- .github/scripts/versions.sh
+++ .github/scripts/versions.sh
@@ -7,12 +7,12 @@
 # This file should be sourced by installation scripts
 
 # tt-exalens configuration
-export EXALENS_VERSION="0.1.6-cp310-cp310-linux_x86_64"
+export EXALENS_VERSION="0.1.7-py3-none-any"
 export EXALENS_TAG="v${EXALENS_VERSION%%-*}"
 export EXALENS_WHEEL="tt_exalens-${EXALENS_VERSION}.whl"
 export EXALENS_URL="https://github.com/tenstorrent/tt-exalens/releases/download/${EXALENS_TAG}/${EXALENS_WHEEL}"
 
 # tt-smi configuration
-export TT_SMI_VERSION="3.0.38"
+export TT_SMI_VERSION="3.1.0"
 export TT_SMI_WHEEL="tt_smi-${TT_SMI_VERSION}-py3-none-any.whl"
 export TT_SMI_URL="https://github.com/tenstorrent/tt-smi/releases/download/v${TT_SMI_VERSION}/${TT_SMI_WHEEL}"
