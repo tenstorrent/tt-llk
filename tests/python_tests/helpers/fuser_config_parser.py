92b159c7d713250a6680ebe1de6cc6780fd59e9e
CodeMan62 <sharmahimanshu15082007@gmail.com>
2026-01-09 20:35:06 +0530

    pre-commit

--- tests/python_tests/helpers/fuser_config_parser.py
+++ tests/python_tests/helpers/fuser_config_parser.py
@@ -184,7 +184,8 @@ def parse_math_operation(
             if sfpu_type == "UnarySfpu":
                 operation = SFPU_UNARY_OPERATION_MAP[sfpu_config["operation"]]
                 approx_mode = APPROXIMATION_MODE_MAP.get(
-                    sfpu_config.get("approximation_mode", "Precise"), ApproximationMode.Precise
+                    sfpu_config.get("approximation_mode", "Precise"),
+                    ApproximationMode.Precise,
                 )
                 iterations = sfpu_config.get("iterations", 8)
                 dest_idx = sfpu_config.get("dst_dest_tile_index", 0)
@@ -199,7 +200,8 @@ def parse_math_operation(
             elif sfpu_type == "BinarySfpu":
                 operation = SFPU_BINARY_OPERATION_MAP[sfpu_config["operation"]]
                 approx_mode = APPROXIMATION_MODE_MAP.get(
-                    sfpu_config.get("approximation_mode", "Precise"), ApproximationMode.Precise
+                    sfpu_config.get("approximation_mode", "Precise"),
+                    ApproximationMode.Precise,
                 )
                 iterations = sfpu_config.get("iterations", 8)
                 src1_dest_tile_index = sfpu_config.get("src1_dest_tile_index", 0)
