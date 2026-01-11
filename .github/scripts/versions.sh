4f12317804b323187ccbb42a7853e47a3ea5a88d
Filip Vranic <fvranic@tenstorrent.com>
2026-01-11 21:31:10 +0000

    fixup! fixup! fixup! fixup! Update pip install command to use test PyPI

--- .github/scripts/versions.sh
+++ .github/scripts/versions.sh
@@ -16,3 +16,5 @@ export EXALENS_URL="https://github.com/tenstorrent/tt-exalens/releases/download/
 export TT_SMI_VERSION="3.0.38"
 export TT_SMI_WHEEL="tt_smi-${TT_SMI_VERSION}-py3-none-any.whl"
 export TT_SMI_URL="https://github.com/tenstorrent/tt-smi/releases/download/v${TT_SMI_VERSION}/${TT_SMI_WHEEL}"
+
+export TT_UMD_VERSION="0.7.4"
