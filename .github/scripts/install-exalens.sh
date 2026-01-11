3ae8c32d101d11488186b3a3d123529002fbd916
Filip Vranic <fvranic@tenstorrent.com>
2026-01-11 21:31:10 +0000

    fixup! fixup! fixup! fixup! Update pip install command to use test PyPI

--- .github/scripts/install-exalens.sh
+++ .github/scripts/install-exalens.sh
@@ -10,6 +10,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=.github/scripts/versions.sh
 source "${SCRIPT_DIR}/versions.sh"
 
+# Install tt-umd first from test.pypi.org
+pip install -q --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ tt-umd==${TT_UMD_VERSION}
+
 wget -q -O "${EXALENS_WHEEL}" "${EXALENS_URL}" || exit 1
-pip install -q --extra-index-url https://test.pypi.org/simple/ --no-cache-dir "${EXALENS_WHEEL}"
+pip install -q --no-cache-dir "${EXALENS_WHEEL}"
 rm "${EXALENS_WHEEL}"
