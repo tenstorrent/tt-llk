02640071e1f4ec0a5aa2a91aa6dfddb6d3fce257
uvelimirovic <uvelimirovic@tenstorrent.com>
2026-01-15 14:36:31 +0000

    Added unpack_AB_uninit to the end of unpack_AB kernel.

--- tests/sources/unpack_AB_test.cpp
+++ tests/sources/unpack_AB_test.cpp
@@ -39,6 +39,7 @@ void run_kernel(const volatile struct RuntimeParams *params)
     {
         _llk_unpack_AB_<>(L1_ADDRESS(buffer_A[i]), L1_ADDRESS(buffer_B[i]));
     }
+    _llk_unpack_AB_uninit_(params->TEST_FACE_R_DIM);
 }
 
 #endif
