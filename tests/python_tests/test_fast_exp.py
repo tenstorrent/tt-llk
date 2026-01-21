b920a4aac8df1927f14a22ef303011aa553636b8
Stephen Osborne <sosborne@tenstorrent.com>
2026-01-21 01:30:49 +0000

    cleanup python test

--- tests/python_tests/test_fast_exp.py
+++ tests/python_tests/test_fast_exp.py
@@ -24,8 +24,8 @@ from helpers.test_config import ProfilerBuild, run_test
     test_name="fast_exp_test",
     formats=input_output_formats(
         [
-            # DataFormat.Float16_b,
-            DataFormat.Float32,
+            DataFormat.Float16_b,
+            # DataFormat.Float32,
         ]
     ),
     input_dimensions=[
@@ -33,7 +33,7 @@ from helpers.test_config import ProfilerBuild, run_test
     ],  # [[32, 32], [32, 64], [64, 32], [64, 64], [128, 32], [32, 128]],
     approx_mode=[ApproximationMode.Yes],
     mathop=[MathOperation.Exp],
-    dest_acc=[DestAccumulation.Yes],  # , DestAccumulation.Yes],
+    dest_acc=[DestAccumulation.No],  # , DestAccumulation.Yes],
 )
 def test_eltwise_unary_sfpu_float(
     test_name, approx_mode, formats, mathop, dest_acc, input_dimensions
@@ -106,118 +106,69 @@ def eltwise_unary_sfpu(
         "tile_cnt": tile_cnt,
     }
 
-    # for config_idx, exp_config in enumerate(
-    #    [
-    #        (2, 1, True),
-    #        (2, 2, True),
-    #        (2, 3, True),
-    #        (2, 4, True),
-    #        (2, 5, True),
-    #        (4, 1, True),
-    #        (4, 2, True),
-    #        (4, 3, True),
-    #        (4, 4, True),
-    #        (4, 5, True),
-    #        (6, 1, True),
-    #        (6, 2, True),
-    #        (6, 3, True),
-    #        (6, 4, True),
-    #        (6, 5, True),
-    #        (0, 0, False),
-    #    ]
-    # ):
-    if True:
+    res_address = write_stimuli_to_l1(
+        test_config,
+        src_A,
+        src_B,
+        formats.input_format,
+        formats.input_format,
+        tile_count_A=tile_cnt,
+        tile_count_B=tile_cnt,
+    )
 
-        # Update configuration header.
-        # with open("tt_llk_wormhole_b0/common/inc/sfpu/exp_config_params.h", "w") as f:
-        #    if exp_config[2]:
-        #        f.write("#define EXPONENTIAL_TESTING\n")
-        #    f.write("#define EXP_CONFIG_NUM_TERMS " + str(exp_config[0]) + "\n")
-        #    f.write("#define EXP_CONFIG_NUM_SCALE " + str(exp_config[1]) + "\n")
+    run_test(test_config, profiler_build=ProfilerBuild.Yes)
+    runtime = Profiler.get_data(test_config["testname"])
+    timestamps = runtime.math().timestamps()  # .marker("TEST_TIMESTAMP").frame()
+    zones = runtime.zones().marker("TILE_LOOP").frame()
+    print(zones)
 
-        res_address = write_stimuli_to_l1(
-            test_config,
-            src_A,
-            src_B,
-            formats.input_format,
-            formats.input_format,
-            tile_count_A=tile_cnt,
-            tile_count_B=tile_cnt,
-        )
+    res_from_L1 = collect_results(formats, tile_count=tile_cnt, address=res_address)
 
-        run_test(test_config, profiler_build=ProfilerBuild.Yes)
-        runtime = Profiler.get_data(test_config["testname"])
-        timestamps = runtime.math().timestamps()  # .marker("TEST_TIMESTAMP").frame()
-        zones = runtime.zones().marker("TILE_LOOP").frame()
-        print(zones)
+    assert len(res_from_L1) == len(golden_tensor)
 
-        res_from_L1 = collect_results(formats, tile_count=tile_cnt, address=res_address)
+    torch_format = format_dict[formats.output_format]
+    res_tensor = torch.tensor(res_from_L1, dtype=torch_format)
 
-        assert len(res_from_L1) == len(golden_tensor)
+    src_A_np = src_A.double().numpy()
+    res_tensor_np = res_tensor.double().numpy()
+    golden_tensor_np = golden_tensor.double().numpy()
 
-        torch_format = format_dict[formats.output_format]
-        res_tensor = torch.tensor(res_from_L1, dtype=torch_format)
+    import numpy as np
 
-        # assert passed_test(
-        #    golden_tensor,
-        #    res_tensor,
-        #    formats.output_format,
-        #    custom_atol=0.1,
-        #    custom_rtol=0.1,
-        #    one_face_check=False,
-        # )
+    N = 5
+    print("")
+    print("INPUT_SIZE:", size)
+    print("INPUT: ", src_A_np[0:N])
+    print("OUTPUT:", res_tensor_np[0:N])
+    print("GOLDEN:", golden_tensor_np[0:N])
+    print("")
+    print("INPUT: ", src_A_np[size - N : size])
+    print("OUTPUT:", res_tensor_np[size - N : size])
+    print("GOLDEN:", golden_tensor_np[size - N : size])
 
-        src_A_np = src_A.double().numpy()
-        res_tensor_np = res_tensor.double().numpy()
-        golden_tensor_np = golden_tensor.double().numpy()
+    # for i in range(256):
+    #    print(
+    #        i,
+    #        src_A_np[i],
+    #        golden_tensor_np[i],
+    #        res_tensor_np[i],
+    #        np.max(np.abs(res_tensor_np[i] - golden_tensor_np[i])),
+    #    )
 
-        import numpy as np
-
-        N = 5
-        print("")
-        print("INPUT_SIZE:", size)
-        print("INPUT: ", src_A_np[0:N])
-        print("OUTPUT:", res_tensor_np[0:N])
-        print("GOLDEN:", golden_tensor_np[0:N])
-        print("")
-        print("INPUT: ", src_A_np[size - N : size])
-        print("OUTPUT:", res_tensor_np[size - N : size])
-        print("GOLDEN:", golden_tensor_np[size - N : size])
-
-        # for i in range(256):
-        #    print(
-        #        i,
-        #        src_A_np[i],
-        #        golden_tensor_np[i],
-        #        res_tensor_np[i],
-        #        np.max(np.abs(res_tensor_np[i] - golden_tensor_np[i])),
-        #    )
-
-        print(
-            "MAXABSDIFF:",
-            np.max(np.abs(res_tensor_np[0:256] - golden_tensor_np[0:256])),
-        )
-        print(
-            "MAXRELDIFF:",
-            np.max(
-                np.abs(
-                    (res_tensor_np[0:256] - golden_tensor_np[0:256])
-                    / (golden_tensor_np[0:256] + 1e-8)
-                )
-            ),
-        )
-        print(
-            "MEANERRSQ:",
-            np.mean((res_tensor_np[0:256] - golden_tensor_np[0:256]) ** 2),
-        )
-
-        # Write results to file.
-        # with open(
-        #    "exponential_accuracy_test.txt", "w" if config_idx == 0 else "a"
-        # ) as f:
-        #    if config_idx == 0:
-        #        f.write("experiment, num_terms, num_scale, i, input, output, golden\n")
-        #    for i in range(size):
-        #        f.write(
-        #            f"{config_idx}, {exp_config[0]}, {exp_config[1]}, {i}, {src_A_np[i]}, {res_tensor_np[i]}, {golden_tensor_np[i]}\n"
-        #        )
+    print(
+        "MAXABSDIFF:",
+        np.max(np.abs(res_tensor_np[0:256] - golden_tensor_np[0:256])),
+    )
+    print(
+        "MAXRELDIFF:",
+        np.max(
+            np.abs(
+                (res_tensor_np[0:256] - golden_tensor_np[0:256])
+                / (golden_tensor_np[0:256] + 1e-8)
+            )
+        ),
+    )
+    print(
+        "MEANERRSQ:",
+        np.mean((res_tensor_np[0:256] - golden_tensor_np[0:256]) ** 2),
+    )
