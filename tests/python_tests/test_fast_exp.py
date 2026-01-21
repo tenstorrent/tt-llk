4a854d890d946259683e748c5f1aaa9116a0363c
Stephen Osborne <sosborne@tenstorrent.com>
2026-01-21 02:27:09 +0000

    update python test

--- tests/python_tests/test_fast_exp.py
+++ tests/python_tests/test_fast_exp.py
@@ -1,13 +1,13 @@
 # SPDX-FileCopyrightText: © 2025 Tenstorrent AI ULC
 # SPDX-License-Identifier: Apache-2.0
 
-
 import pytest
 import torch
 from helpers.chip_architecture import ChipArchitecture, get_chip_architecture
-from helpers.device import collect_results, write_stimuli_to_l1
+
+# from helpers.profiler import ProfilerConfig
 from helpers.format_config import DataFormat, InputOutputFormat
-from helpers.golden_generators import UnarySFPUGolden, get_golden_generator
+from helpers.golden_generators import UntilizeGolden, get_golden_generator
 from helpers.llk_params import (
     ApproximationMode,
     DestAccumulation,
@@ -15,29 +15,35 @@ from helpers.llk_params import (
     format_dict,
 )
 from helpers.param_config import input_output_formats, parametrize
-from helpers.profiler import Profiler
+from helpers.stimuli_config import StimuliConfig
 from helpers.stimuli_generator import generate_stimuli
-from helpers.test_config import ProfilerBuild, run_test
+from helpers.test_config import TestConfig
+from helpers.test_variant_parameters import (
+    INPUT_DIMENSIONS,
+    TILE_COUNT,
+)
 
 
 @parametrize(
-    test_name="fast_exp_test",
     formats=input_output_formats(
         [
             DataFormat.Float16_b,
             # DataFormat.Float32,
         ]
     ),
-    input_dimensions=[
-        [32, 32]
-    ],  # [[32, 32], [32, 64], [64, 32], [64, 64], [128, 32], [32, 128]],
     approx_mode=[ApproximationMode.Yes],
     mathop=[MathOperation.Exp],
     dest_acc=[DestAccumulation.No],  # , DestAccumulation.Yes],
 )
 def test_eltwise_unary_sfpu_float(
-    test_name, approx_mode, formats, mathop, dest_acc, input_dimensions
+    formats: list[InputOutputFormat],
+    approx_mode: ApproximationMode,
+    mathop: MathOperation,
+    dest_acc: DestAccumulation,
+    workers_tensix_coordinates: str,
 ):
+    input_dimensions = [32, 32]
+
     arch = get_chip_architecture()
 
     if dest_acc == DestAccumulation.No and arch == ChipArchitecture.BLACKHOLE:
@@ -58,18 +64,13 @@ def test_eltwise_unary_sfpu_float(
             reason="Exp-related operations are not supported for bf8_b format in approximation mode."
         )
 
-    eltwise_unary_sfpu(
-        test_name, formats, dest_acc, approx_mode, mathop, input_dimensions
-    )
-
-
-def eltwise_unary_sfpu(
-    test_name, formats, dest_acc, approx_mode, mathop, input_dimensions
-):
     torch.manual_seed(0)
 
-    src_A, src_B, tile_cnt = generate_stimuli(
-        formats.input_format, formats.input_format, input_dimensions=input_dimensions
+    src_A, tile_cnt_A, src_B, tile_cnt_B = generate_stimuli(
+        stimuli_format_A=formats.input_format,
+        input_dimensions_A=input_dimensions,
+        stimuli_format_B=formats.input_format,
+        input_dimensions_B=input_dimensions,
     )
 
     min_val, max_val = -5, 5
@@ -78,56 +79,51 @@ def eltwise_unary_sfpu(
     for i in range(size):
         src_A[i] = min_val + (max_val - min_val) * i / (size - 1.0)
 
-    generate_golden = get_golden_generator(UnarySFPUGolden)
+    generate_golden = get_golden_generator(UntilizeGolden)
     golden_tensor = generate_golden(
-        mathop,
-        src_A,
-        formats.output_format,
-        dest_acc,
-        formats.input_format,
-        input_dimensions,
+        src_A, formats.output_format, dimensions=input_dimensions
     )
 
     unpack_to_dest = (
-        formats.input_format.is_32_bit()
-        and dest_acc
-        == DestAccumulation.Yes  # If dest_acc is off, we unpack Float32 into 16-bit format in src registers (later copied over in dest reg for SFPU op)
+        formats.input_format.is_32_bit() and dest_acc == DestAccumulation.Yes
     )
 
-    test_config = {
-        "formats": formats,
-        "testname": test_name,
-        "dest_acc": dest_acc,
-        "input_A_dimensions": input_dimensions,
-        "input_B_dimensions": input_dimensions,
-        "mathop": mathop,
-        "approx_mode": approx_mode,
-        "unpack_to_dest": unpack_to_dest,
-        "tile_cnt": tile_cnt,
-    }
-
-    res_address = write_stimuli_to_l1(
-        test_config,
-        src_A,
-        src_B,
-        formats.input_format,
-        formats.input_format,
-        tile_count_A=tile_cnt,
-        tile_count_B=tile_cnt,
+    # configuration = ProfilerConfig(
+    configuration = TestConfig(
+        "sources/fast_exp_test.cpp",
+        formats,
+        templates=[
+            INPUT_DIMENSIONS(input_dimensions, input_dimensions),
+        ],
+        runtimes=[
+            TILE_COUNT(tile_cnt_A),
+        ],
+        variant_stimuli=StimuliConfig(
+            src_A,
+            formats.input_format,
+            src_B,
+            formats.input_format,
+            formats.output_format,
+            tile_count_A=tile_cnt_A,
+            tile_count_B=tile_cnt_B,
+            tile_count_res=tile_cnt_A,
+        ),
+        dest_acc=dest_acc,
+        unpack_to_dest=unpack_to_dest,
     )
 
-    run_test(test_config, profiler_build=ProfilerBuild.Yes)
-    runtime = Profiler.get_data(test_config["testname"])
-    timestamps = runtime.math().timestamps()  # .marker("TEST_TIMESTAMP").frame()
-    zones = runtime.zones().marker("TILE_LOOP").frame()
-    print(zones)
+    res_from_L1 = configuration.run(workers_tensix_coordinates)
 
-    res_from_L1 = collect_results(formats, tile_count=tile_cnt, address=res_address)
+    # runtime = Profiler.get_data(test_config["testname"])
+    # timestamps = runtime.math().timestamps()  # .marker("TEST_TIMESTAMP").frame()
+    # zones = runtime.zones().marker("TILE_LOOP").frame()
+    # print(zones)
 
-    assert len(res_from_L1) == len(golden_tensor)
+    assert len(res_from_L1) == len(
+        golden_tensor
+    ), "Result tensor and golder tensor are not of the same length"
 
-    torch_format = format_dict[formats.output_format]
-    res_tensor = torch.tensor(res_from_L1, dtype=torch_format)
+    res_tensor = torch.tensor(res_from_L1, dtype=format_dict[formats.output_format])
 
     src_A_np = src_A.double().numpy()
     res_tensor_np = res_tensor.double().numpy()
