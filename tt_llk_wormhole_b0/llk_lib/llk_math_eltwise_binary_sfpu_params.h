b4bc067d5197c422ed6a1de7f5c453c9e0837c25
Filip Vranic <fvranic@tenstorrent.com>
2026-01-21 22:31:28 +0000

    feat: provide two parameter overload for _llk_math_eltwise_binary_sfpu_params_

--- tt_llk_wormhole_b0/llk_lib/llk_math_eltwise_binary_sfpu_params.h
+++ tt_llk_wormhole_b0/llk_lib/llk_math_eltwise_binary_sfpu_params.h
@@ -8,9 +8,9 @@
 #include "llk_math_eltwise_binary_sfpu.h"
 #include "llk_sfpu_types.h"
 
-template <bool APPROXIMATE, typename Callable, typename... Args>
-inline void _llk_math_eltwise_binary_sfpu_params_(
-    Callable&& sfpu_func, uint dst_index_in0, uint dst_index_in1, uint dst_index_out, int vector_mode = (int)VectorMode::RC, Args&&... args)
+// Internal implementation - takes a nullary callable that wraps the actual sfpu invocation
+template <typename InvokeFunc>
+inline void _llk_math_eltwise_binary_sfpu_params_impl_(InvokeFunc&& invoke, int vector_mode)
 {
     _llk_math_eltwise_binary_sfpu_start_<DST_SYNC_MODE>(0);
 
@@ -22,7 +22,7 @@ inline void _llk_math_eltwise_binary_sfpu_params_(
 #pragma GCC unroll 0
         for (int face = 0; face < 2; face++)
         {
-            std::forward<Callable>(sfpu_func)(dst_index_in0, dst_index_in1, dst_index_out, std::forward<Args>(args)...);
+            invoke();
             TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
             TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
         }
@@ -38,7 +38,7 @@ inline void _llk_math_eltwise_binary_sfpu_params_(
 #pragma GCC unroll 0
         for (int face = 0; face < 2; face++)
         {
-            std::forward<Callable>(sfpu_func)(dst_index_in0, dst_index_in1, dst_index_out, std::forward<Args>(args)...);
+            invoke();
             TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
             TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
             TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
@@ -51,14 +51,37 @@ inline void _llk_math_eltwise_binary_sfpu_params_(
 #pragma GCC unroll 0
         for (int face = 0; face < 4; face++)
         {
-            std::forward<Callable>(sfpu_func)(dst_index_in0, dst_index_in1, dst_index_out, std::forward<Args>(args)...);
+            invoke();
             TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
             TTI_SETRWC(p_setrwc::CLR_NONE, p_setrwc::CR_D, 8, 0, 0, p_setrwc::SET_D);
         }
     }
     else
     {
-        std::forward<Callable>(sfpu_func)(dst_index_in0, dst_index_in1, dst_index_out, std::forward<Args>(args)...);
+        invoke();
     }
+
     _llk_math_eltwise_binary_sfpu_done_();
+}
+
+// Overload for callables that take (dst_index_in0, dst_index_in1, dst_index_out, args...)
+template <bool APPROXIMATE, typename Callable, typename... Args>
+inline void _llk_math_eltwise_binary_sfpu_params_(
+    Callable&& sfpu_func, uint dst_index_in0, uint dst_index_in1, uint dst_index_out, int vector_mode = static_cast<int>(VectorMode::RC), Args&&... args)
+{
+    _llk_math_eltwise_binary_sfpu_params_impl_([&]() { sfpu_func(dst_index_in0, dst_index_in1, dst_index_out, std::forward<Args>(args)...); }, vector_mode);
+}
+
+// Overload for callables that take (dst_index_in0, dst_index_in1, args...) - no dst_index_out
+template <bool APPROXIMATE, typename Callable, typename... Args>
+inline void _llk_math_eltwise_binary_sfpu_params_(Callable&& sfpu_func, uint dst_index_in0, uint dst_index_in1, int vector_mode, Args&&... args)
+{
+    _llk_math_eltwise_binary_sfpu_params_impl_([&]() { sfpu_func(dst_index_in0, dst_index_in1, std::forward<Args>(args)...); }, vector_mode);
+}
+
+// Overload for callables that only take (dst_index_in0, dst_index_in1) - no args, default vector_mode
+template <bool APPROXIMATE, typename Callable>
+inline void _llk_math_eltwise_binary_sfpu_params_(Callable&& sfpu_func, uint dst_index_in0, uint dst_index_in1)
+{
+    _llk_math_eltwise_binary_sfpu_params_impl_([&]() { sfpu_func(dst_index_in0, dst_index_in1); }, static_cast<int>(VectorMode::RC));
 }
