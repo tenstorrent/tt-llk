7053aee0c7e99983be9702880e964546277672ae
ldjurovicTT <ldjurovic@tenstorrent.com>
2026-01-22 12:21:44 +0000

    Cleanup code. Rename functions. Add comments

--- tests/sources/tilized_eltwise_transpose_bcast_test.cpp
+++ tests/sources/tilized_eltwise_transpose_bcast_test.cpp
@@ -30,12 +30,12 @@ void run_kernel(const volatile struct RuntimeParams *params)
     _llk_unpack_hw_configure_<is_fp32_dest_acc_en>(
         formats.unpack_src, formats.unpack_src, formats.unpack_dst, formats.unpack_dst, FACE_R_DIM, FACE_R_DIM, 4 /* num_faces */, 4 /* num_faces */);
 
-    sdpa_optimization_init_();
+    _llk_unpackA_bcastB_row_as_col_init_();
 
     // Unpack tiles: srcA normal, srcB with column broadcast (needs transpose to pass)
     for (int i = 0; i < params->TILE_CNT; ++i)
     {
-        sdpa_optimization_(L1_ADDRESS(buffer_A[i]), L1_ADDRESS(buffer_B[i]));
+        _llk_unpackA_bcastB_row_as_col_(L1_ADDRESS(buffer_A[i]), L1_ADDRESS(buffer_B[i]));
     }
 }
 
@@ -55,13 +55,13 @@ void run_kernel(const volatile struct RuntimeParams *params)
     _llk_math_pack_sync_init_<dest_sync, is_fp32_dest_acc_en>();
     _llk_math_hw_configure_<is_fp32_dest_acc_en>(formats.math, formats.math);
 
-    sdpa_optimization_new_init_();
+    _llk_math_eltwise_binary_bcastB_row_as_col_init_();
 
     _llk_math_wait_for_dest_available_<dest_sync>();
 
     for (int i = 0; i < params->TILE_CNT; ++i)
     {
-        sdpa_optimization_new_(i);
+        _llk_math_eltwise_binary_bcastB_row_as_col_(i);
     }
 
     _llk_math_dest_section_done_<dest_sync, is_fp32_dest_acc_en>();
