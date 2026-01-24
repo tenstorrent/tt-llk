90fa7d20fafd332869a9bc926ac24a5d5bd12f2c
njokovicTT <njokovic@tenstorrent.com>
2026-01-24 18:26:28 +0000

    Second iteration

--- tt_llk_blackhole/llk_lib/llk_math_eltwise_unary_datacopy.h
+++ tt_llk_blackhole/llk_lib/llk_math_eltwise_unary_datacopy.h
@@ -375,11 +375,16 @@ inline void eltwise_unary_configure_mop(uint rows_per_inst, uint total_rows, con
     }
 }
 
-template <DataCopyType type, bool is_fp32_dest_acc_en, BroadcastType src_b_bcast_type = BroadcastType::NONE, bool tilize = false, bool is_int_fpu_en = false>
+template <DataCopyType type, bool is_fp32_dest_acc_en, BroadcastType src_b_bcast_type = BroadcastType::NONE, bool tilize = false, bool is_int_fpu_en = false, bool unpack_to_dest = false>
 inline void _llk_math_eltwise_unary_datacopy_init_(const std::uint32_t num_faces = 4, const std::uint32_t dst_format = 255)
 {
     LLK_ASSERT(num_faces == 1 || num_faces == 2 || num_faces == 4, "num_faces must be 1, 2, or 4");
     eltwise_unary_configure_addrmod<type, src_b_bcast_type>(dst_format);
+
+    if constexpr (src_b_bcast_type != BroadcastType::NONE)
+    {
+        _llk_math_dbg_feature_disable_();
+    }
 
     if constexpr (type == A2D && src_b_bcast_type == BroadcastType::NONE)
     {
