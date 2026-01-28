17481208f27a9b0fd63e0e945224dcf3a6abc8fb
ldjurovicTT <ldjurovic@tenstorrent.com>
2026-01-28 09:45:18 +0000

    Add comment

--- tt_llk_blackhole/common/inc/ckernel_instr_params.h
+++ tt_llk_blackhole/common/inc/ckernel_instr_params.h
@@ -143,6 +143,8 @@ struct p_setadc
     constexpr static uint SET_Z = 2;
     constexpr static uint SET_W = 3;
 
+    // Added here to match Wormhole B0
+    // This allows reuse of Wormhole code in some optimizations
     constexpr static uint X  = 1;
     constexpr static uint Y  = 2;
     constexpr static uint XY = 3;
