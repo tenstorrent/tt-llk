0ee225019dc69d20bebee4795abe50ad09904f9e
njokovicTT <njokovic@tenstorrent.com>
2026-02-04 11:07:21 +0000

    Uint16 edge case

--- tt_llk_wormhole_b0/llk_lib/llk_math_eltwise_unary_datacopy.h
+++ tt_llk_wormhole_b0/llk_lib/llk_math_eltwise_unary_datacopy.h
@@ -263,7 +263,7 @@ inline void eltwise_unary_configure_mop(uint rows_per_inst, uint total_rows, con
         uint innerloop = (rows_per_inst == p_mova2d::MOV_1_ROW) ? total_rows : (total_rows >> 3);
         uint outerloop = num_faces;
 
-        if(unpack_to_dest && is_fp32_dest_acc_en && !is_32bit_input(dst_format, dst_format)){
+        if(unpack_to_dest && is_fp32_dest_acc_en && !is_32bit_input(dst_format, dst_format) && !(dst_format == (uint)DataFormat::UInt16)){
             ckernel_template tmp(outerloop, innerloop, TT_OP_ELWADD(0, 0, p_elwise::SRCB_NO_BCAST, ADDR_MOD_2, 0));
             tmp.set_end_op(TT_OP_SETRWC(p_setrwc::CLR_AB, 0, 0, 0, 0, p_setrwc::SET_AB));
             tmp.program();
