4b6111ffd20df759c58e5cf47857235daa960135
Ryan Zhu <ryanzhu@tenstorrent.com>
2026-02-05 20:56:17 +0000

    pass parameters down from above layer

--- tt_llk_wormhole_b0/llk_lib/llk_pack.h
+++ tt_llk_wormhole_b0/llk_lib/llk_pack.h
@@ -111,10 +111,12 @@ inline void _llk_pack_reconfig_data_format_(
     const std::uint32_t face_r_dim = FACE_R_DIM,
     const std::uint32_t num_faces  = 4,
     const bool partial_face        = false,
-    const bool narrow_tile         = false)
+    const bool narrow_tile         = false,
+    const uint relu_config         = 0)
 {
     LLK_ASSERT(num_faces == 1 || num_faces == 2 || num_faces == 4, "num_faces must be 1, 2, or 4");
-    reconfig_packer_data_format<is_fp32_dest_acc_en>(pack_src_format, pack_dst_format, tile_size, face_r_dim, num_faces, partial_face);
+    reconfig_packer_data_format<is_fp32_dest_acc_en>(
+        pack_src_format, pack_dst_format, tile_size, face_r_dim, num_faces, partial_face, narrow_tile, relu_config);
 
     if constexpr (is_tile_dim_reconfig_en)
     {
