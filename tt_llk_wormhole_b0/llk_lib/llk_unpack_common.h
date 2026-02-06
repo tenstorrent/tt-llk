a1f7c002718703bc19ad4779aad89bb1b20aadb1
Ryan Zhu <ryanzhu@tenstorrent.com>
2026-02-06 16:56:01 +0000

    explanatory comment for constants in z stride

--- tt_llk_wormhole_b0/llk_lib/llk_unpack_common.h
+++ tt_llk_wormhole_b0/llk_lib/llk_unpack_common.h
@@ -102,6 +102,8 @@ inline void _llk_unpack_reconfig_data_format_srca_impl_(
         uint unpack_ch1_x_stride = (uint)(unpack_dst_format & 0x3) == (uint)DataFormat::Float32   ? 4
                                    : (uint)(unpack_dst_format & 0x3) == (uint)DataFormat::Float16 ? 2
                                                                                                   : 1;
+        // FACE_R_DIM constant is used here because data is not stored densely in src/dest registers
+        // so we want to keep standard stride for one face
         uint unpack_ch1_z_stride = FACE_C_DIM * FACE_R_DIM * unpack_ch1_x_stride;
         cfg_reg_rmw_tensix<UNP0_ADDR_CTRL_ZW_REG_1_Zstride_RMW>(unpack_ch1_z_stride);
 
