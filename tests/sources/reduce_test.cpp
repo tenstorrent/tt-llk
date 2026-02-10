757c7f5f0957af137ad0b9b42e7be1fb76744718
Gagan Deep Singh <gagandeepsingh@tenstorrent.com>
2026-02-10 20:08:07 +0000

    addressed some pr comments

--- tests/sources/reduce_test.cpp
+++ tests/sources/reduce_test.cpp
@@ -30,9 +30,9 @@ void run_kernel(const volatile struct RuntimeParams *params)
         formats.unpack_dst,
         params->TEST_FACE_R_DIM,
         params->TEST_FACE_R_DIM,
-        params->num_faces /* num_faces */,
-        params->num_faces /* num_faces */);
-    _llk_unpack_AB_reduce_init_<POOL_TYPE, REDUCE_DIM>(params->TEST_FACE_R_DIM, params->num_faces /* num_faces */);
+        params->num_faces,
+        params->num_faces);
+    _llk_unpack_AB_reduce_init_<POOL_TYPE, REDUCE_DIM, false /* enforce_fp32_accumulation */>(params->TEST_FACE_R_DIM, params->num_faces);
     for (int i = 0; i < params->INPUT_TILE_CNT; ++i)
     {
         _llk_unpack_AB_reduce_<POOL_TYPE, REDUCE_DIM>(L1_ADDRESS(buffer_A[i]), L1_ADDRESS(buffer_B[0]));
@@ -55,7 +55,7 @@ void run_kernel(const volatile struct RuntimeParams *params)
 
     _llk_math_pack_sync_init_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
     _llk_math_hw_configure_<is_fp32_dest_acc_en>(formats.math, formats.math);
-    _llk_math_reduce_init_<POOL_TYPE, REDUCE_DIM, is_fp32_dest_acc_en, MATH_FIDELITY, false>();
+    _llk_math_reduce_init_<POOL_TYPE, REDUCE_DIM, is_fp32_dest_acc_en, MATH_FIDELITY, enforce_fp32_accumulation>();
 
     if (params->IS_REDUCE_TO_ONE)
     {
@@ -111,17 +111,17 @@ void run_kernel(const volatile struct RuntimeParams *params)
     _llk_pack_init_<false, false>(formats.pack_dst);
 
 #ifdef ARCH_BLACKHOLE
-    _llk_pack_hw_configure_<is_fp32_dest_acc_en, false, false>(formats.pack_src, formats.pack_dst, 16 * 16 * 4);
+    _llk_pack_hw_configure_<is_fp32_dest_acc_en, false /* untilize */, false /* tilize */>(formats.pack_src, formats.pack_dst, 16 * 16 * 4);
 #else
-    _llk_pack_hw_configure_<is_fp32_dest_acc_en, false>(formats.pack_src, formats.pack_dst, 16 * 16 * 4);
+    _llk_pack_hw_configure_<is_fp32_dest_acc_en, false /* untilize */>(formats.pack_src, formats.pack_dst, 16 * 16 * 4);
 #endif
 
-    _llk_pack_reduce_mask_config_<false, REDUCE_DIM>();
+    _llk_pack_reduce_mask_config_<false /* untilize */, REDUCE_DIM>();
 
 #ifdef ARCH_BLACKHOLE
     _llk_pack_dest_init_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
 #else
-    _llk_pack_dest_init_<DstSync::SyncHalf, is_fp32_dest_acc_en, false>();
+    _llk_pack_dest_init_<DstSync::SyncHalf, is_fp32_dest_acc_en, false /* untilize */>();
 #endif
     int remaining_tiles = params->OUTPUT_TILE_CNT;
     while (remaining_tiles)
@@ -130,7 +130,7 @@ void run_kernel(const volatile struct RuntimeParams *params)
         _llk_packer_wait_for_math_done_();
         for (int i = 0; i < tiles_from_dest; ++i)
         {
-            _llk_pack_<DstSync::SyncHalf, is_fp32_dest_acc_en, false>(i, L1_ADDRESS(buffer_Res[params->OUTPUT_TILE_CNT - remaining_tiles + i]));
+            _llk_pack_<DstSync::SyncHalf, is_fp32_dest_acc_en, false /* untilize */>(i, L1_ADDRESS(buffer_Res[params->OUTPUT_TILE_CNT - remaining_tiles + i]));
         }
         _llk_pack_dest_section_done_<DstSync::SyncHalf, is_fp32_dest_acc_en>();
         remaining_tiles -= tiles_from_dest;
