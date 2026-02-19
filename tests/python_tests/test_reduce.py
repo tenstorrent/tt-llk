95d298497cf440e934b7c09c17109bbf13383446
Gagan Deep Singh <gagandeepsingh@tenstorrent.com>
2026-02-19 17:15:15 +0000

    Lowered tensor size to mitigate timeout

--- tests/python_tests/test_reduce.py
+++ tests/python_tests/test_reduce.py
@@ -49,7 +49,7 @@ mathop_mapping = {
 
 
 @parametrize(
-    input_dimensions=[[512, 32]],
+    input_dimensions=[[256, 32]],
     tile_dimensions=[[1, 32], [2, 32], [4, 32], [8, 32], [16, 32], [32, 32]],
     formats=input_output_formats(
         [
