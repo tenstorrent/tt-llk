c42286194bb2a6a2b0496a77e901b561e2a267bc
Strahinja Stanisic <sstanisic@tenstorrent.com>
2026-02-28 18:11:02 +0000

    fixup :pencil2:: runtime formats

--- tests/sources/state/reconfig/math_reconfig_test.cpp
+++ tests/sources/state/reconfig/math_reconfig_test.cpp
@@ -38,10 +38,10 @@ void run_kernel(const volatile struct RuntimeParams *params)
 {
     (void)params; // ... unused variable warning ...
 
-    const std::uint32_t prev_a = (std::uint32_t)formats.unpack_A_src;
-    const std::uint32_t prev_b = (std::uint32_t)formats.unpack_A_dst;
-    const std::uint32_t next_a = (std::uint32_t)formats.pack_src;
-    const std::uint32_t next_b = (std::uint32_t)formats.pack_dst;
+    const std::uint32_t prev_a = (std::uint32_t)params->formats.unpack_A_src;
+    const std::uint32_t prev_b = (std::uint32_t)params->formats.unpack_A_dst;
+    const std::uint32_t next_a = (std::uint32_t)params->formats.pack_src;
+    const std::uint32_t next_b = (std::uint32_t)params->formats.pack_dst;
 
     _llk_math_hw_configure_<is_fp32_dest_acc_en>(
         /* srca_data_format */ next_a,
