9a83bf6b20690264e324b77449a0391e1e75145d
Gagan Deep Singh <gagandeepsingh@tenstorrent.com>
2026-03-16 16:23:45 +0000

    Updated stall_clr_cntrl

--- tests/python_tests/test_reduce.py
+++ tests/python_tests/test_reduce.py
@@ -47,7 +47,7 @@ mathop_mapping = {
 
 
 @parametrize(
-    tile_dimensions=[[1, 32], [2, 32], [4, 32], [8, 32], [16, 32], [32, 32], [32, 16]],
+    tile_dimensions=[[1, 32], [2, 32], [4, 32], [8, 32]],
     formats=input_output_formats(
         [
             DataFormat.Float32,
