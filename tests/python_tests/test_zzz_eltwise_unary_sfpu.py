927c348a1f2ea7f8e60f19c51497e0322c7d3960
ldjurovicTT <ldjurovic@tenstoreent.com>
2026-03-17 15:34:24 +0000

    Enable more tests on BH

--- tests/python_tests/test_zzz_eltwise_unary_sfpu.py
+++ tests/python_tests/test_zzz_eltwise_unary_sfpu.py
@@ -6,7 +6,7 @@ from itertools import chain, product
 
 import pytest
 import torch
-from conftest import skip_for_blackhole, skip_for_coverage
+from conftest import skip_for_coverage
 from helpers.chip_architecture import ChipArchitecture
 from helpers.format_config import DataFormat, InputOutputFormat
 from helpers.golden_generators import (
@@ -91,6 +91,7 @@ FORMATS_INCLUDE_BFP4_B = input_output_formats(
     [
         DataFormat.Float16_b,
         DataFormat.Bfp8_b,
+        DataFormat.Float16,
         DataFormat.Bfp4_b,
     ]
 )
@@ -106,7 +107,7 @@ MATHOPS_INCLUDE_BFP4_B = [
     # MathOperation.Reciprocal,
     # MathOperation.Sin,
     # MathOperation.Sqrt,
-    # MathOperation.Rsqrt,
+    MathOperation.Rsqrt,
     MathOperation.Square,
     MathOperation.Tanh,
     MathOperation.Celu,
@@ -267,7 +268,7 @@ FLOAT_TEST_PARAMS_BFP4_B = list(
 
 
 # No BH testing done for now
-@skip_for_blackhole
+# @skip_for_blackhole
 # Skipped because of: https://github.com/tenstorrent/tt-llk/issues/1435
 @skip_for_coverage
 @pytest.mark.nightly
