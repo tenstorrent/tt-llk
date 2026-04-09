f4cd234314ebce447e726a72eec080707ed6f290
Ana Mokan <amokan@tenstorrent.com>
2026-04-09 15:24:46 +0000

    Add skip for quasar for perf and test fused

--- tests/python_tests/test_fused.py
+++ tests/python_tests/test_fused.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import pytest
-from conftest import skip_for_coverage
+from conftest import skip_for_coverage, skip_for_quasar
 from fuser.fuser_config_parser import FUSER_CONFIG_DIR, FuserConfigSchema
 
 yaml_files = sorted(FUSER_CONFIG_DIR.glob("*.yaml"))
@@ -11,6 +11,7 @@ test_names = [f.stem for f in yaml_files]
 
 
 @skip_for_coverage
+@skip_for_quasar
 @pytest.mark.parametrize("test_name", test_names, ids=test_names)
 def test_fuser(
     test_name,
